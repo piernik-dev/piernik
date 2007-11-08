41493b31f9ccd633ae7b991e69002b5abbc1b4e7
Michał Hanasz <mhanasz@astri.umk.pl>
2007-11-08 19:33:25 +0000

    *** empty log message ***

--- src/shear/shear.F90
+++ src/shear/shear.F90
@@ -1,19 +1,20 @@
+#include "mhd.def"
 module shear
-   real    :: ts, dely, eps
-   integer :: delj
+  real    :: ts, dely, eps
+  integer :: delj
 
-   contains
+  contains
 
-   subroutine yshift(ts)
-     use start, only : qshear,omega,xmin,xmax,nyd
-     use grid, only : dy
-     implicit none
-     real, intent(in) :: ts
+  subroutine yshift(ts)
+    use start, only : qshear,omega,xmin,xmax,nyd
+    use grid, only : dy
+    implicit none
+    real, intent(in) :: ts
 
-     dely  = ts*qshear*omega*(xmax-xmin)
-     delj  = mod(int(dely/dy),nyd)
-     eps   = mod(dely,dy)/dy
+    dely  = ts*qshear*omega*(xmax-xmin)
+    delj  = mod(int(dely/dy),nyd)
+    eps   = mod(dely,dy)/dy
 !     write(*,*) ts,delj,eps
-   end subroutine yshift
+  end subroutine yshift
 end module shear
 
