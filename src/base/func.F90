44395372a54e01d885235dbe39142ef95237f129
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2007-11-09 16:49:02 +0000

    New module with inline functions

---
+++ src/base/func.F90
@@ -0,0 +1,31 @@
+module func
+contains
+function tvdb_emf(vh,vg,b,dt)
+  implicit none
+  real, dimension(:), intent(in) :: vh,vg,b
+  real, intent(in)               :: dt
+  real, dimension(size(vh)) :: tvdb_emf
+  integer :: i,ip,ipp,im
+  real    :: w,wp,wm,dw,v
+
+  do i = lbound(vh,1)+2, ubound(vh,1)-3
+    ip  = i  + 1
+    ipp = ip + 1
+    im  = i  - 1
+    v   = vh(i)
+    if (v .gt. 0.) then
+      w=vg(i)*b(i)
+      wp=(vg(ip)*b(ip)-w)*0.5
+      wm=(w-vg(im)*b(im))*0.5
+    else
+      w=vg(ip)*b(ip)
+      wp=(w-vg(ipp)*b(ipp))*0.5
+      wm=(vg(i)*b(i)-w)*0.5
+    end if
+    dw=0.0
+    if(wm*wp > 0.0) dw=2.0*wm*wp/(wm+wp)
+    tvdb_emf(i)=(w+dw)*dt
+  enddo
+  return
+end function tvdb_emf
+end module func
