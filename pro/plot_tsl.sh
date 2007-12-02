76971703173bd055c95a010f48a7ae53bd4ddf8b
Michał Hanasz <mhanasz@astri.umk.pl>
2007-12-02 11:46:31 +0000

    *** empty log message ***

---
+++ pro/plot_tsl.sh
@@ -0,0 +1,15 @@
+#!/bin/tcsh
+
+setenv IDL_PATH "/home/mhanasz/piernik0.1/pro:<IDL_DEFAULT>"
+
+echo " "
+echo "WORKING DIRECTORY:"
+pwd
+
+if (! -f "./plot_tsl.var") then
+  cp ~/piernik0.1/pro/plot_tsl.var .
+endif
+
+idl ~/piernik0.1/pro/plot_tsl.idl
+
+echo " "
