61f5e64b0d7a26dec50809acead6232b01e6e44e
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2008-01-22 14:14:47 +0000

    MAJOR BUG - ill defined directive SUM_SQR resulted in erroneous pressure bef: 0.5 * vx**2 + vy**2 + vz**2 / dens aft: 0.5 * (vx**2 + vy**2 + vz**2) / dens

--- src/base/fluxes.F90
+++ src/base/fluxes.F90
@@ -1,6 +1,6 @@
 #include "mhd.def"
 #define SQR(var) var*var
-#define SUM_SQR(x,y,z) SQR(x)+SQR(y)+SQR(z)  
+#define SUM_SQR(x,y,z) ( SQR(x)+SQR(y)+SQR(z) ) 
 #define RNG 2:n-1
 module fluxes
   implicit none
