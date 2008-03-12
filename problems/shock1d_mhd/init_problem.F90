f09fc4679d6e88830be8f1178aacb4d3d35ac909
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2008-03-12 16:27:39 +0000

    Ryu & Jones shock tube problem

--- problems/shock1d_mhd/init_problem.F90
+++ problems/shock1d_mhd/init_problem.F90
@@ -4,8 +4,8 @@
 module init_problem
 ! ----------------------------------------- ! 
 ! Initial condition for a 1D MHD shock tube !
-! Written by: K. Kowalik, Feb 2008          !
-! See: Stone, Hawley, Evans ApJ 388:415-437 !
+! Written by: K. Kowalik, Mar 2008          !
+! See: Ryu, Jones, ApJ 442:228-258, (1995)  !
 !    and reference therein                  !
 ! ----------------------------------------- !
 
@@ -18,10 +18,13 @@ module init_problem
       rbuff, cbuff, ibuff
   use mpi_setup
 
-  real :: d0,r0,bx0,by0,bz0
+  real :: dl,vxl,vyl,vzl,bxl,byl,bzl,el
+  real :: dr,vxr,vyr,vzr,bxr,byr,bzr,er
   character ::  problem_name*32,run_id*3,dir*1
 
-  namelist /PROBLEM_CONTROL/  problem_name, run_id, d0, r0,bx0,by0,bz0
+  namelist /PROBLEM_CONTROL/  problem_name, &
+     run_id,dl,vxl,vyl,vzl,bxl,byl,bzl,el,  &
+     dr,vxr,vyr,vzr,bxr,byr,bzr,er
 
 
 contains
@@ -35,8 +38,6 @@ contains
 
     problem_name = 'shock'
     run_id  = 'tst'
-    d0      = 1.0
-    r0      = 0.25
     
     if(proc .eq. 0) then
       open(1,file='problem.par')
@@ -55,8 +56,22 @@ contains
       cbuff(1) =  problem_name
       cbuff(2) =  run_id
 
-      rbuff(1) = d0
-      rbuff(2) = r0
+      rbuff(1) = dl
+      rbuff(2) = vxl
+      rbuff(3) = vyl
+      rbuff(4) = vzl
+      rbuff(5) = bxl
+      rbuff(6) = byl
+      rbuff(7) = bzl
+      rbuff(8) = el
+      rbuff(9) = dr
+      rbuff(10) = vxr
+      rbuff(11) = vyr
+      rbuff(12) = vzr
+      rbuff(13) = bxr
+      rbuff(14) = byr
+      rbuff(15) = bzr
+      rbuff(16) = er
     
       call MPI_BCAST(cbuff, 32*buffer_dim, MPI_CHARACTER,        0, comm, ierr)
       call MPI_BCAST(ibuff,    buffer_dim, MPI_INTEGER,          0, comm, ierr)
@@ -71,8 +86,22 @@ contains
       problem_name = cbuff(1)   
       run_id       = cbuff(2)   
 
-      d0           = rbuff(1)  
-      r0           = rbuff(2)
+      dl  = rbuff(1)
+      vxl = rbuff(2)
+      vyl = rbuff(3)
+      vzl = rbuff(4)
+      bxl = rbuff(5)
+      byl = rbuff(6)
+      bzl = rbuff(7)
+      el  = rbuff(8)
+      dr  = rbuff(9)
+      vxr = rbuff(10)
+      vyr = rbuff(11)
+      vzr = rbuff(12)
+      bxr = rbuff(13)
+      byr = rbuff(14)
+      bzr = rbuff(15)
+      er  = rbuff(16) 
     
     endif
 
@@ -101,24 +130,26 @@ contains
         do k = 1,nz
           zk = z(k)
 
-          if ((xi <= 50)) then
-             rho = 1.0
-             pre = 1.0
-             vx  = 0.0
-             vy  = 0.0
-             bx  = 0.75
-             by  = 1.0
+          if ((xi <= 0.5)) then
+             rho = dl
+             pre = el
+             vx  = vxl
+             vy  = vyl
+             vz  = vzl
+             bx  = bxl
+             by  = byl
+             bz  = bzl
           else
-             rho = 0.125
-             pre = 0.1
-             vx  = 0.0
-             vy  = 0.0
-             bx  = 0.75
-             by  = -1.0
+             rho = dr
+             pre = er
+             vx  = vxr
+             vy  = vyr
+             vz  = vzr
+             bx  = bxr
+             by  = byr
+             bz  = bzr
           endif
           
-          vz = 0.0
-          bz = 0.0
         
           
           u(idna,i,j,k) = rho
@@ -127,7 +158,7 @@ contains
           u(imya,i,j,k) = vy*u(idna,i,j,k)
           u(imza,i,j,k) = vz*u(idna,i,j,k)
 #ifndef ISO	  
-          u(iena,i,j,k) = pre/(gamma-1.0)
+          u(iena,i,j,k) = pre ! pre here meand eint
           u(iena,i,j,k) = max(u(iena,i,j,k), smallei)
           u(iena,i,j,k) = u(iena,i,j,k) +0.5*(vx**2+vy**2+vz**2)*u(idna,i,j,k)
 #endif
