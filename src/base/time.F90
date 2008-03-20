b35c2b72072302e17e9d2149e55c3dcd47dcd369
Dominik Wóltański <minikwolt@astri.umk.pl>
2008-03-20 13:30:06 +0000

    moving multiplying by cfl to the last value of dt_mhd_all +adding missing precompilator remarks

--- src/base/time.F90
+++ src/base/time.F90
@@ -18,7 +18,7 @@ module time
 
 #ifdef COSM_RAYS
   use cr_diffusion
-#endif 
+#endif /* COSM_RAYS */
   
   implicit none
   real c
@@ -31,7 +31,7 @@ contains
          dt_visc, dt_cr
 #ifdef RESIST
     use resistivity, only : dt_resist, timestep_resist
-#endif
+#endif /* RESIST */
     implicit none
 ! Timestep computation
 
@@ -41,23 +41,23 @@ contains
 #ifdef RESIST
     call timestep_resist
     dt = min(dt,dt_resist)
-#endif 
+#endif /* RESIST */
 #ifdef COOL_HEAT
     call timestep_coolheat
     dt = min(dt,dt_coolheat)
-#endif 
+#endif /* COOL_HEAT */
 #ifdef HEAT_COND
     dt_heatcond = cfl_heatcond * 0.5*dxmn**2/(K_heatcond+small)
     dt = min(dt,dt_heatcond)
-#endif 
+#endif /* HEAT_COND */
 #ifdef VISC
     dt_visc = cfl_visc * 0.5*dxmn**2/(nu_bulk+small)
     dt = min(dt,dt_visc)
-#endif 
+#endif /* VISC */
 #ifdef COSM_RAYS
     dt_cr = cfl_cr * 0.5*dxmn**2/(K_cr_paral+K_cr_perp+small)
     dt = min(dt,dt_cr)
-#endif 
+#endif /* COSM_RAYS */
 
   end subroutine timestep
 
@@ -105,13 +105,11 @@ contains
           p = csi2*u(idna,i,j,k)
           ps =p+pmag
           cf = sqrt(abs(  (2.*pmag+p)/u(idna,i,j,k)) )
-! <> ISO
-#else 
+#else /* ISO */
           ps=(u(iena,i,j,k)-sum(u(imxa:imza,i,j,k)**2,1)/u(idna,i,j,k)/2.)*(gamma-1.)+(2.-gamma)*pmag
           p=ps-pmag          
           cf = sqrt(abs(  (2.*pmag+gamma*p)/u(idna,i,j,k)) )
-!--> ISO
-#endif 
+#endif /* ISO */
 
           cx=max(cx,vx+cf)
           cy=max(cy,vy+cf)
@@ -122,9 +120,9 @@ contains
       end do
     end do
 
-    dt_mhd_proc_x = cfl*dx/cx
-    dt_mhd_proc_y = cfl*dy/cy
-    dt_mhd_proc_z = cfl*dz/cz
+    dt_mhd_proc_x = dx/cx
+    dt_mhd_proc_y = dy/cy
+    dt_mhd_proc_z = dz/cz
     dt_mhd_proc   = min(dt_mhd_proc_x, dt_mhd_proc_y, dt_mhd_proc_z)
 
     call MPI_REDUCE(c, c_max_all, 1, MPI_DOUBLE_PRECISION, MPI_MAX, 0, comm, ierr)
@@ -134,7 +132,7 @@ contains
     
     call MPI_REDUCE(dt_mhd_proc, dt_mhd_all, 1, MPI_DOUBLE_PRECISION, MPI_MIN, 0, comm, ierr)
     call MPI_BCAST(dt_mhd_all, 1, MPI_DOUBLE_PRECISION, 0, comm, ierr)
-    dt_mhd = dt_mhd_all
+    dt_mhd = cfl*dt_mhd_all
 
   end subroutine timestep_mhd
 
