2e3cda59983e2cd13cf1e599dbf1e948c975ef66
Dominik Wóltański <minikwolt@astri.umk.pl>
2008-03-27 11:29:54 +0000

    cleaning

--- src/unitsystems/psm/constants.F90
+++ src/unitsystems/psm/constants.F90
@@ -3,23 +3,6 @@
 module constants     ! module containg numerical and physical constants !!!
 		     ! 	    actual units system is pc, Msun, Myr	!!!
 
-! units systems:
-!
-! cm, g, s
-!
-! g ---> mean particle mass pm:= 1.36 * mprot = 1.36 * 1.672614e-24 g = 2.2748e-24 g
-!
-! pc, Msun, myr: pc = 3.0856e18 cm, Msun = 1.989e33 g, myr = 3.154e13 s
-! density:    		Msun/pc3 = 	6.7677922e-23 g/cm3
-!					29.763 pm/cm3
-! column density: 	Msun/pc2 = 	2.0891e-4
-!					0.91838e20 pm/cm3
-! velocity: 		pc/myr = 	0.97775e5 cm/s = 0.97775 km/s
-! energy: 		Msun*pc2/myr2 = 1.9015e43 erg
-!
-! G_one, pc, myr -> [m] = 1.9713302e21 g
-!-----------------> [m]/pc3 = 6.7076688e-35 g/cm3
-!-----------------> [energy] = 1.8817e31 erg
 
   real, parameter :: one  = 1.0
   real, parameter :: half = 0.5
@@ -29,9 +12,6 @@ module constants     ! module containg numerical and physical constants !!!
   real, parameter :: thrq = 3./4.
   real, parameter :: small= 1.e-50
   real, parameter :: big  = 1.e+50
-!  real, dimension(2,3)  :: cn
-!  data                     cn  /one,thrq,onet, &
-!                                one,oneq,twot/
 
   real, parameter :: pi = 3.141592653589793238
   real, parameter :: dpi = 2.*pi
@@ -40,29 +20,29 @@ module constants     ! module containg numerical and physical constants !!!
 
 ! uses: length --> pc, mass --> Msun, time --> myr, temperature --> kelvin
 ! length units:
-	real, parameter :: cm = 	1.0/3.0856e18
-	real, parameter :: metr = 	1.0e2/3.0856e18
-	real, parameter :: km =		1.0e5/3.0856e18
-	real, parameter :: au =		1.49597892e13/3.0856e18
-	real, parameter :: pc =		1.0
+	real, parameter :: cm = 	1.0/3.0856e18			! psm
+	real, parameter :: metr = 	1.0e2*cm			! psm
+	real, parameter :: km =		1.0e5*cm
+	real, parameter :: au =		1.49597892e13*cm
+	real, parameter :: pc =		1.0				! psm
 	real, parameter :: kpc =	1000.0*pc
-	real, parameter :: ly =		9.4605e17/3.0856e18
-! mass units:
-	real, parameter :: gram =	1.0/1.989e33
-	real, parameter :: kg =		1.0e3/1.989e33
-	real, parameter :: Msun =	1.0
-	real, parameter :: gmu =	2.32e7*Msun
-	real, parameter :: me = 	9.109558e-28/1.989e33
-	real, parameter :: mp = 	1.672614e-24/1.989e33
-	real, parameter :: mH = 	1.673559e-24/1.989e33
-	real, parameter :: amu =	1.660531e-24/1.989e33
+	real, parameter :: ly =		9.4605e17*cm
 ! time units:
-	real, parameter :: sek =	1.0e-6/365.2652/24.0/3600.0
-	real, parameter :: minute =	1.0e-6/365.2652/24.0/60.0
-	real, parameter :: hour =	1.0e-6/365.2652/24.0
-	real, parameter :: day =	1.0e-6/365.2652
-	real, parameter :: year =	1.0e-6
-	real, parameter :: myr =	1.0
+	real, parameter :: sek =	1.0e-6/365.2652/24.0/3600.0	! psm
+	real, parameter :: minute =	60.0*sek
+	real, parameter :: hour =	3600.0*sek
+	real, parameter :: day =	86400.0*sek
+	real, parameter :: year =	1.0e-6				! psm
+	real, parameter :: myr =	1.0				! psm
+! mass units:
+	real, parameter :: gram =	1.0/1.989e33			! psm
+	real, parameter :: kg =		1.0e3*gram			! psm
+	real, parameter :: Msun =	1.0				! psm
+	real, parameter :: gmu =	2.32e7*Msun
+	real, parameter :: me = 	9.109558e-28*gram
+	real, parameter :: mp = 	1.672614e-24*gram
+	real, parameter :: mH = 	1.673559e-24*gram
+	real, parameter :: amu =	1.660531e-24*gram
 ! force units:
 	real, parameter :: newton =	kg*metr/sek**2
 	real, parameter :: dyna =	gram*cm/sek**2
@@ -88,8 +68,8 @@ module constants     ! module containg numerical and physical constants !!!
 	real, parameter :: Mearth =	5.977e27*gram
 	real, parameter :: earthradius=	6378.17*km
 ! utilities (to remove in the future):
-	real, parameter :: k_B = 1.38066e-16*erg/kelvin ! erg/K ---> kboltz
-	real, parameter :: m_H = 1.66053e-24*gram ! g     ---> amu
+	real, parameter :: k_B = 1.38066e-16*erg/kelvin ! ---> kboltz
+	real, parameter :: m_H = 1.66053e-24*gram       ! ---> amu
 	real, parameter :: hydro_mass = mH
 	real, parameter :: chcf = hydro_mass / myr
 	real, parameter :: fpiG = fpi*newtong
