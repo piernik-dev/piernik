7052481e90797e5fd6c1d4b45dee179af9f749e6
Dominik Wóltański <minikwolt@astri.umk.pl>
2008-04-22 09:24:18 +0000

    cleaning in tv for simpler comparison and first-step-unification between orig and ssp

--- src/scheme/split/orig/tv.F90
+++ src/scheme/split/orig/tv.F90
@@ -1,7 +1,7 @@
-#include "mhd.def" 
+#include "mhd.def"
 module tv ! split orig
   contains
-  
+
   subroutine tvdb(vibj,b,vg,n,dt,di)
     use constants, only : big
     use func, only      : tvdb_emf
@@ -25,16 +25,19 @@ module tv ! split orig
       vibj1=eoshift(b*vg,1,boundary=big)
     end where
     b1(2:n) = b(2:n) -(vibj1(2:n)-vibj1(1:n-1))*dti*0.5;    b1(1) = b(2)
-    
+
     vibj = tvdb_emf(vh,vg,b1,dt)
 
   end subroutine tvdb
 
   subroutine relaxing_tvd(u,bb,sweep,i1,i2,dx,n,dt)
-    use start,  only : smallei, smalld, smallecr, integration_order,cn
-    use arrays, only : idna,imxa,imya,imza,iecr,ibx,iby,ibz,nu,iena,wa
+    use start,  only : smalld, integration_order,cn
+    use arrays, only : idna,imxa,imya,imza,nu
     use fluxes
-
+#ifndef ISO
+    use start,  only : smallei
+    use arrays, only : iena,ibx,iby,ibz
+#endif /* ISO */
 #ifdef GRAV
     use gravity, only :grav_pot2accel
 #endif /* GRAV */
@@ -43,12 +46,13 @@ module tv ! split orig
     use start,  only : qshear, omega
 #endif /* SHEAR */
 #ifdef COSM_RAYS
-    use start, only : gamma_cr, cr_active
+    use start,  only : gamma_cr, cr_active, smallecr
+    use arrays, only : wa, iecr
 #endif /* COSM_RAYS */
 #ifdef VZ_LIMITS
     use arrays, only : z
     use start, only : floor_vz, ceil_vz
-#endif /* VZ_LIMITS */    
+#endif /* VZ_LIMITS */
 
 
     implicit none
@@ -58,15 +62,15 @@ module tv ! split orig
     real, dimension(3,n)  :: bb
     real, dimension(n)    :: gravl,gravr,rotfr,vx,vxr
     real, dimension(n)    :: dgrp,dgrm,dglp,dglm
-           
-    character sweep*6
-          
-!locals    
-    real, dimension(nu,n) :: w,fr,fl,flux,dfrp,dfrm,dflm,dflp, &
-                             dulf,durf
-    real, dimension(nu,n) :: ul0,ur0,u1,ul1,ur1
-    real, dimension(n)    :: ekin,eint,emag
 
+    character sweep*6
+
+!locals
+    real, dimension(nu,n) :: w,fr,fl,flux,dfrp,dfrm,dflm,dflp,dulf,durf
+    real, dimension(nu,n) :: ul0,ur0,u1,ul1,ur1
+#ifndef ISO
+    real, dimension(n)    :: ekin,eint,emag
+#endif /* ISO */
 #ifdef GRAV
     real, dimension(nu,n) :: duls,durs
 #endif /* GRAV */
@@ -74,16 +78,16 @@ module tv ! split orig
     real, dimension(n)    :: divv,decr,gpcr,ecr,tmp
 #endif /* COSM_RAYS */
 
-                            
+
     w         = 0.0
     cfr       = 0.0
     dtx       = dt / dx
 
 #ifdef GRAV
-    duls = 0.0 
+    duls = 0.0
     durs = 0.0
 #endif /* GRAV */
-    
+
     u1 = u
 
   do istep=1,integration_order
@@ -97,31 +101,31 @@ module tv ! split orig
       ul0 = fl/cfr
     endif
 
-    fl(:,1:n-1) = fl(:,2:n)                         ; fl(:,n)   = fl(:,n-1)  
+    fl(:,1:n-1) = fl(:,2:n)                         ; fl(:,n)   = fl(:,n-1)
 
     if(istep == 2) then
-       dfrp(:,1:n-1) = 0.5*(fr(:,2:n) - fr(:,1:n-1)); dfrp(:,n) = dfrp(:,n-1)         
-       dfrm(:,2:n)   = dfrp(:,1:n-1);                 dfrm(:,1) = dfrm(:,2)               
+       dfrp(:,1:n-1) = 0.5*(fr(:,2:n) - fr(:,1:n-1)); dfrp(:,n) = dfrp(:,n-1)
+       dfrm(:,2:n)   = dfrp(:,1:n-1);                 dfrm(:,1) = dfrm(:,2)
       call flimiter(fr,dfrm,dfrp,nu,n)
 
-       dflp(:,1:n-1) = 0.5*(fl(:,1:n-1) - fl(:,2:n)); dflp(:,n) = dflp(:,n-1)         
-       dflm(:,2:n)   = dflp(:,1:n-1);                 dflm(:,1) = dflm(:,2)                  
+       dflp(:,1:n-1) = 0.5*(fl(:,1:n-1) - fl(:,2:n)); dflp(:,n) = dflp(:,n-1)
+       dflm(:,2:n)   = dflp(:,1:n-1);                 dflm(:,1) = dflm(:,2)
       call flimiter(fl,dflm,dflp,nu,n)
     endif
 
-    durf(:,2:n) = dtx*(fr(:,2:n) - fr(:,1:n-1));     durf(:,1) = durf(:,2)        
-    dulf(:,2:n) = dtx*(fl(:,2:n) - fl(:,1:n-1));     dulf(:,1) = dulf(:,2)       
-    
+    durf(:,2:n) = dtx*(fr(:,2:n) - fr(:,1:n-1));     durf(:,1) = durf(:,2)
+    dulf(:,2:n) = dtx*(fl(:,2:n) - fl(:,1:n-1));     dulf(:,1) = dulf(:,2)
+
     ur1(:,:) = ur0 - cn(istep)*durf
     ul1(:,:) = ul0 + cn(istep)*dulf
 
-    ur1(1,:) = max(ur1(1,:), 0.5*smalld)
-    ul1(1,:) = max(ul1(1,:), 0.5*smalld)
+    ur1(idna,:) = max(ur1(idna,:), 0.5*smalld)
+    ul1(idna,:) = max(ul1(idna,:), 0.5*smalld)
 
 #ifdef SHEAR
     vxr(1:n-1) = 0.5*(u1(imya,2:n)/u1(idna,2:n) + u1(imya,1:n-1)/u1(idna,1:n-1))
     if(sweep .eq. 'xsweep') then
-      rotfr(:) =   2.0*omega*(vxr(:) + qshear*omega*xr(:))   
+      rotfr(:) =   2.0*omega*(vxr(:) + qshear*omega*xr(:))
     else if(sweep .eq. 'ysweep')  then
       rotfr(:) = - 2.0*omega*vxr(:)
     else
@@ -135,20 +139,20 @@ module tv ! split orig
 #ifdef GRAV
     call grav_pot2accel(sweep,i1,i2, n, gravr)
     gravr      = gravr + rotfr                     ;  gravr(n)   = gravr(n-1)
-    gravl(2:n) = gravr(1:n-1)                      ;  gravl(1)   = gravl(2)                                                   
- 
+    gravl(2:n) = gravr(1:n-1)                      ;  gravl(1)   = gravl(2)
+
     if(istep == 2) then
-      dgrp(1:n-1) = 0.5*(gravr(1:n-1) - gravr(2:n));  dgrp(n) = dgrp(n-1)      
-      dgrm(2:n) = dgrp(1:n-1)                      ;  dgrm(1) = dgrm(2)                  
+      dgrp(1:n-1) = 0.5*(gravr(1:n-1) - gravr(2:n));  dgrp(n) = dgrp(n-1)
+      dgrm(2:n) = dgrp(1:n-1)                      ;  dgrm(1) = dgrm(2)
       call flimiter(gravr,dgrp,dgrm,1,n)
 
-      dglp(1:n-1) = 0.5*(gravl(2:n) - gravl(1:n-1));  dglp(n) = dglp(n-1)       
-      dglm(2:n)   = dglp(1:n-1)                    ;  dglm(1) = dglm(2)                         
+      dglp(1:n-1) = 0.5*(gravl(2:n) - gravl(1:n-1));  dglp(n) = dglp(n-1)
+      dglm(2:n)   = dglp(1:n-1)                    ;  dglm(1) = dglm(2)
       call flimiter(gravl,dglp,dglm,1,n)
     endif
 
 #ifndef ISO
-    duls(iena,:)  = gravr*ul0(imxa,:)*dt 
+    duls(iena,:)  = gravr*ul0(imxa,:)*dt
     durs(iena,:)  = gravl*ur0(imxa,:)*dt
 #endif /* ISO */
     duls(imxa,:)  = gravr*ul0(idna,:)*dt
@@ -160,36 +164,34 @@ module tv ! split orig
     u1 = ul1 + ur1
     u1(idna,:) = max(u1(idna,:), smalld)
 
-
 #ifdef VZ_LIMITS
     if(sweep .eq. 'zsweep') then
-      where((z(:) .gt. 0.0) .and. (u1(imxa,:) .lt.  floor_vz*u(idna,:)))      
+      where((z(:) .gt. 0.0) .and. (u1(imxa,:) .lt.  floor_vz*u(idna,:)))
         u1(imxa,:) =  floor_vz*u(idna,:)
-      endwhere       
-      where((z(:) .lt. 0.0) .and. (u1(imxa,:) .gt. -floor_vz*u(idna,:)))
-        u1(imxa,:) = -floor_vz*u(idna,:) 
       endwhere
-    endif  
-#endif /* VZ_LIMITS */  
-          
+      where((z(:) .lt. 0.0) .and. (u1(imxa,:) .gt. -floor_vz*u(idna,:)))
+        u1(imxa,:) = -floor_vz*u(idna,:)
+      endwhere
+    endif
+#endif /* VZ_LIMITS */
+
 #ifdef COSM_RAYS
     select case (sweep)
-      case('xsweep') 
+      case('xsweep')
         divv = wa(:,i1,i2)
-      case('ysweep') 
+      case('ysweep')
         divv = wa(i2,:,i1)
-      case('zsweep') 
+      case('zsweep')
         divv = wa(i1,i2,:)
     end select
 
-    decr(:)  = -(gamma_cr-1.)*u1(iecr,:)*divv(:)*dt 
+    decr(:)  = -(gamma_cr-1.)*u1(iecr,:)*divv(:)*dt
     u1(iecr,:) = u1(iecr,:) + cn(istep)*decr(:)
-    u1(iecr,:) = max(smallecr,u1(iecr,:)) 
-    
+    u1(iecr,:) = max(smallecr,u1(iecr,:))
 
     vx = u1(imxa,:)/u1(idna,:)
     ecr = u1(iecr,:)
-    
+
     gpcr(2:n-1) = cr_active*(gamma_cr -1.)*(ecr(3:n)-ecr(1:n-2))/(2.*dx) ; gpcr(1:2)=0.0 ; gpcr(n-1:n) = 0.0
 
 #ifndef ISO
@@ -200,24 +202,24 @@ module tv ! split orig
 #endif /* COSM_RAYS */
 #ifndef ISO
     ekin = 0.5*(u1(imxa,:)*u1(imxa,:)+u1(imya,:)*u1(imya,:)+u1(imza,:)*u1(imza,:))/u1(idna,:)
-    emag = 0.5*(bb(ibx,:)*bb(ibx,:) + bb(iby,:)*bb(iby,:) + bb(ibz,:)*bb(ibz,:)) 
-    eint = u1(5,:)-ekin-emag
+    emag = 0.5*(bb(ibx,:)*bb(ibx,:) + bb(iby,:)*bb(iby,:) + bb(ibz,:)*bb(ibz,:))
+    eint = u1(iena,:)-ekin-emag
     eint = max(eint,smallei)
-    u1(5,:) = eint+ekin+emag
+    u1(iena,:) = eint+ekin+emag
 #endif /* ISO */
 
 #ifdef VZ_LIMITS
 ! Dwukrotne ograniczanie vz moze byc zbedne (patrz wyzej)
 ! Ten fragment bedzie mozna ewentualnie wykasowac po testach
     if(sweep .eq. 'zsweep') then
-      where((z(:) .gt. 0.0) .and. (u1(imxa,:) .lt.  floor_vz*u(idna,:)))      
+      where((z(:) .gt. 0.0) .and. (u1(imxa,:) .lt.  floor_vz*u(idna,:)))
         u1(imxa,:) =  floor_vz*u(idna,:)
-      endwhere       
-      where((z(:) .lt. 0.0) .and. (u1(imxa,:) .gt. -floor_vz*u(idna,:)))
-        u1(imxa,:) = -floor_vz*u(idna,:) 
       endwhere
-    endif  
-#endif /* VZ_LIMITS */    
+      where((z(:) .lt. 0.0) .and. (u1(imxa,:) .gt. -floor_vz*u(idna,:)))
+        u1(imxa,:) = -floor_vz*u(idna,:)
+      endwhere
+    endif
+#endif /* VZ_LIMITS */
 
       u(:,:) = u1(:,:)
   enddo
@@ -228,5 +230,3 @@ module tv ! split orig
 
 !==========================================================================================
 end module tv
-
-
--- src/scheme/split/ssp/tv.F90
+++ src/scheme/split/ssp/tv.F90
@@ -1,69 +1,82 @@
 #include "mhd.def"
-module tv   ! split ssp
-  use constants
-  use start
-  use arrays, only : idna,imxa,imya,imza,iecr,ibx,iby,ibz,nu,iena,wa
+module tv ! split ssp
   contains
+
   subroutine tvdb(vibj,b,vg,n,dt)
-    use func, only : tvdb_emf
+    use func, only      : tvdb_emf
     implicit none
-    integer i,n,ip,ipp,im
-    real dt
-    real, dimension(n) :: vibj,b,vg
+    integer, intent(in) :: n
+    real, intent(in)    :: dt
+    real, dimension(n)  :: vibj,b,vg
 ! locals
-    real, dimension(n) :: vh
-    real w,wp,wm,dw,v
+    real, dimension(n)  :: vh
 
   ! unlike the B field, the vibj lives on the right cell boundary
     vh = 0.0
     vh(1:n-1) =(vg(1:n-1)+ vg(2:n))*0.5;     vh(n) = vh(n-1)
-    
+
     vibj = tvdb_emf(vh,vg,b,dt)
 
   end subroutine tvdb
 
   subroutine relaxing_tvd(u,bb,sweep,i1,i2,dx,n,dt)
+    use start,  only : smalld, integration_order,cn
+    use arrays, only : idna,imxa,imya,imza,nu
     use fluxes
+#ifndef ISO
+    use start,  only : smallei
+    use arrays, only : iena,ibx,iby,ibz
+#endif /* ISO */
 #ifdef GRAV
     use gravity, only :grav_pot2accel
 #endif /* GRAV */
+#ifdef SHEAR
     use arrays, only : xr
+    use start,  only : qshear, omega
+#endif /* SHEAR */
 #ifdef COSM_RAYS
-    use start, only : gamma_cr, cr_active
+    use start,  only : gamma_cr, cr_active, smallecr
+    use arrays, only : wa, iecr
 #endif /* COSM_RAYS */
+#ifdef VZ_LIMITS
+    use arrays, only : z
+    use start, only : floor_vz, ceil_vz
+#endif /* VZ_LIMITS */
+
 
     implicit none
-    integer i1,i2, n
-    real :: dt,dx,dtx
+    integer :: i1,i2, n, istep
+    real    :: dt,dx,dtx
     real, dimension(nu,n) :: u,cfr,ul,ur
     real, dimension(3,n)  :: bb
     real, dimension(n)    :: gravl,gravr,rotfr,vx,vxr
     real, dimension(n)    :: dgrp,dgrm,dglp,dglm
-           
-    character sweep*6
-          
-!locals    
-    real, dimension(nu,n) :: w,fr,fl,flux,dfrp,dfrm,dflm,dflp, &
-                             dulf,durf
-    real, dimension(nu,n) :: ul0,ur0,u1,ul1,ur1
-    real, dimension(n)    :: ekin,eint,emag
 
+    character sweep*6
+
+!locals
+    real, dimension(nu,n) :: w,fr,fl,flux,dfrp,dfrm,dflm,dflp,dulf,durf
+    real, dimension(nu,n) :: ul0,ur0,u1,ul1,ur1
+#ifndef ISO
+    real, dimension(n)    :: ekin,eint,emag
+#endif /* ISO */
 #ifdef GRAV
     real, dimension(nu,n) :: duls,durs
 #endif /* GRAV */
 #ifdef COSM_RAYS
     real, dimension(n)    :: divv,decr,gpcr,ecr,tmp
 #endif /* COSM_RAYS */
-                            
-    w    = 0.0
-    cfr  = 0.0
-    dtx  = dt / dx
+
+
+    w         = 0.0
+    cfr       = 0.0
+    dtx       = dt / dx
 
 #ifdef GRAV
-    duls = 0.0 
+    duls = 0.0
     durs = 0.0
 #endif /* GRAV */
-    
+
     u1 = u
 
    ur1 = 0.0
@@ -78,34 +91,36 @@ module tv   ! split ssp
     ur = fr/cfr
     ul = fl/cfr
 
-    if(istep .eq. 1) then
+    if(istep == 1) then
       ur0 = ur
       ul0 = ul
     endif
 
-    fl(:,1:n-1) = fl(:,2:n)                      ; fl(:,n) = fl(:,n-1)        ! fl=cshift(fl,shift=1,dim=2)                 
+    fl(:,1:n-1) = fl(:,2:n)                         ; fl(:,n)   = fl(:,n-1)
 
-    dfrp(:,1:n-1) = 0.5*(fr(:,2:n) - fr(:,1:n-1)); dfrp(:,n) = dfrp(:,n-1)    ! dfrp=(cshift(fr,shift=1,dim=2)-fr)*0.5    
-    dfrm(:,2:n)   = dfrp(:,1:n-1);                 dfrm(:,1) = dfrm(:,2)    ! dfrm=cshift(dfrp,shift=-1,dim=2)          
-    call flimiter(fr,dfrm,dfrp,nu,n)
-    
-    dflp(:,1:n-1) = 0.5*(fl(:,1:n-1) - fl(:,2:n)); dflp(:,n) = dflp(:,n-1)    ! dflp=(fl-cshift(fl,shift=1,dim=2))*0.5    
-    dflm(:,2:n)   = dflp(:,1:n-1);                 dflm(:,1) = dflm(:,2)    ! dflm=cshift(dflp,shift=-1,dim=2)          
-    call flimiter(fl,dflm,dflp,nu,n)
 
-    durf(:,2:n) = dtx*(fr(:,2:n) - fr(:,1:n-1));     durf(:,1) = durf(:,2)  ! durf = (fr-cshift(fr,shift=-1,dim=2))/dx*dt 
-    dulf(:,2:n) = dtx*(fl(:,2:n) - fl(:,1:n-1));     dulf(:,1) = dulf(:,2)  ! dulf = (fl-cshift(fl,shift=-1,dim=2))/dx*dt
-    
-    ur1= cn(1,istep)*ur0 + cn(2,istep)*(ur1 - durf)      
-    ul1= cn(1,istep)*ul0 + cn(2,istep)*(ul1 + dulf)
+       dfrp(:,1:n-1) = 0.5*(fr(:,2:n) - fr(:,1:n-1)); dfrp(:,n) = dfrp(:,n-1)
+       dfrm(:,2:n)   = dfrp(:,1:n-1);                 dfrm(:,1) = dfrm(:,2)
+      call flimiter(fr,dfrm,dfrp,nu,n)
 
-    ur1(1,:) = max(ur1(1,:), smalld)
-    ul1(1,:) = max(ul1(1,:), smalld)
-    
+       dflp(:,1:n-1) = 0.5*(fl(:,1:n-1) - fl(:,2:n)); dflp(:,n) = dflp(:,n-1)
+       dflm(:,2:n)   = dflp(:,1:n-1);                 dflm(:,1) = dflm(:,2)
+      call flimiter(fl,dflm,dflp,nu,n)
+
+
+    durf(:,2:n) = dtx*(fr(:,2:n) - fr(:,1:n-1));     durf(:,1) = durf(:,2)
+    dulf(:,2:n) = dtx*(fl(:,2:n) - fl(:,1:n-1));     dulf(:,1) = dulf(:,2)
+
+    ur1(:,:) = cn(1,istep)*ur0 + cn(2,istep)*(ur1 - durf)
+    ul1(:,:) = cn(1,istep)*ul0 + cn(2,istep)*(ul1 + dulf)
+
+    ur1(idna,:) = max(ur1(idna,:), smalld)
+    ul1(idna,:) = max(ul1(idna,:), smalld)
+
 #ifdef SHEAR
     vxr(1:n-1) = 0.5*(u1(imya,2:n)/u1(idna,2:n) + u1(imya,1:n-1)/u1(idna,1:n-1))
     if(sweep .eq. 'xsweep') then
-      rotfr(:) =   2.0*omega*(vxr(:) + qshear*omega*xr(:))   
+      rotfr(:) =   2.0*omega*(vxr(:) + qshear*omega*xr(:))
     else if(sweep .eq. 'ysweep')  then
       rotfr(:) = - 2.0*omega*vxr(:)
     else
@@ -118,66 +133,94 @@ module tv   ! split ssp
 ! Gravity source terms -------------------------------------
 #ifdef GRAV
     call grav_pot2accel(sweep,i1,i2, n, gravr)
-    gravr      = gravr + rotfr                   ;  gravr(n)   = gravr(n-1)
-    gravl(2:n) = gravr(1:n-1)                    ;  gravl(1)   = gravl(2)   ! gravl      = cshift(gravr,-1) 
+    gravr      = gravr + rotfr                     ;  gravr(n)   = gravr(n-1)
+    gravl(2:n) = gravr(1:n-1)                      ;  gravl(1)   = gravl(2)
 
-    dgrp(1:n-1) = 0.5*(gravr(1:n-1) - gravr(2:n));  dgrp(n) = dgrp(n-1)     ! dgrp = (gravr-cshift(gravr,shift=1))*0.5
-    dgrm(2:n) = dgrp(1:n-1)                      ;  dgrm(1) = dgrm(2)       ! dgrm = (cshift(gravr,shift=-1)-gravr)*0.5
-    call flimiter(gravr,dgrp,dgrm,1,n)
+      dgrp(1:n-1) = 0.5*(gravr(1:n-1) - gravr(2:n));  dgrp(n) = dgrp(n-1)
+      dgrm(2:n) = dgrp(1:n-1)                      ;  dgrm(1) = dgrm(2)
+      call flimiter(gravr,dgrp,dgrm,1,n)
 
-    dglp(1:n-1) = 0.5*(gravl(2:n) - gravl(1:n-1));  dglp(n) = dglp(n-1)     ! dglp = (cshift(gravl,shift=1)-gravl)*0.5
-    dglm(2:n)   = dglp(1:n-1)                    ;  dglm(1) = dglm(2)       ! dglm = (gravl-cshift(gravl,shift=-1))*0.5
-    call flimiter(gravl,dglp,dglm,1,n)
- 
+      dglp(1:n-1) = 0.5*(gravl(2:n) - gravl(1:n-1));  dglp(n) = dglp(n-1)
+      dglm(2:n)   = dglp(1:n-1)                    ;  dglm(1) = dglm(2)
+      call flimiter(gravl,dglp,dglm,1,n)
+
+
 #ifndef ISO
-    duls(iena,:)  = gravr*ul(imxa,:)*dt 
+    duls(iena,:)  = gravr*ul(imxa,:)*dt
     durs(iena,:)  = gravl*ur(imxa,:)*dt
 #endif /* ISO */
     duls(imxa,:)  = gravr*ul(idna,:)*dt
     durs(imxa,:)  = gravl*ur(idna,:)*dt
-    ur1= ur1 + cn(2,istep)*durs                 
-    ul1= ul1 + cn(2,istep)*duls             
+    ur1= ur1 + cn(2,istep)*durs
+    ul1= ul1 + cn(2,istep)*duls
 #endif /* GRAV */
 
     u1 = ul1 + ur1
-    u1(1,:) = max(u1(1,:), smalld)
-          
+    u1(idna,:) = max(u1(idna,:), smalld)
+
+#ifdef VZ_LIMITS
+    if(sweep .eq. 'zsweep') then
+      where((z(:) .gt. 0.0) .and. (u1(imxa,:) .lt.  floor_vz*u(idna,:)))
+        u1(imxa,:) =  floor_vz*u(idna,:)
+      endwhere
+      where((z(:) .lt. 0.0) .and. (u1(imxa,:) .gt. -floor_vz*u(idna,:)))
+        u1(imxa,:) = -floor_vz*u(idna,:)
+      endwhere
+    endif
+#endif /* VZ_LIMITS */
+
 #ifdef COSM_RAYS
     select case (sweep)
-      case('xsweep') 
+      case('xsweep')
         divv = wa(:,i1,i2)
-      case('ysweep') 
+      case('ysweep')
         divv = wa(i2,:,i1)
-      case('zsweep') 
+      case('zsweep')
         divv = wa(i1,i2,:)
     end select
 
-    decr(:)  = -(gamma_cr-1.)*u1(iecr,:)*divv(:)*dt 
+    decr(:)  = -(gamma_cr-1.)*u1(iecr,:)*divv(:)*dt
     u1(iecr,:) = u1(iecr,:) + cn(2,istep)*decr(:)
+    u1(iecr,:) = max(smallecr,u1(iecr,:))
 
     vx = u1(imxa,:)/u1(idna,:)
     ecr = u1(iecr,:)
-    
+
     gpcr(2:n-1) = cr_active*(gamma_cr -1.)*(ecr(3:n)-ecr(1:n-2))/(2.*dx) ; gpcr(1:2)=0.0 ; gpcr(n-1:n) = 0.0
 
 #ifndef ISO
     u1(iena,:) = u1(iena,:) - cn(2,istep)*u1(imxa,:)/u1(idna,:)*gpcr*dt
 #endif /* ISO */
     u1(imxa,:) = u1(imxa,:) - cn(2,istep)*gpcr*dt
+
 #endif /* COSM_RAYS */
 #ifndef ISO
     ekin = 0.5*(u1(imxa,:)*u1(imxa,:)+u1(imya,:)*u1(imya,:)+u1(imza,:)*u1(imza,:))/u1(idna,:)
-    emag = 0.5*(bb(ibx,:)*bb(ibx,:) + bb(iby,:)*bb(iby,:) + bb(ibz,:)*bb(ibz,:)) 
+    emag = 0.5*(bb(ibx,:)*bb(ibx,:) + bb(iby,:)*bb(iby,:) + bb(ibz,:)*bb(ibz,:))
     eint = u1(iena,:)-ekin-emag
     eint = max(eint,smallei)
     u1(iena,:) = eint+ekin+emag
 #endif /* ISO */
-    
-    u = u1
-  
+
+#ifdef VZ_LIMITS
+! Dwukrotne ograniczanie vz moze byc zbedne (patrz wyzej)
+! Ten fragment bedzie mozna ewentualnie wykasowac po testach
+    if(sweep .eq. 'zsweep') then
+      where((z(:) .gt. 0.0) .and. (u1(imxa,:) .lt.  floor_vz*u(idna,:)))
+        u1(imxa,:) =  floor_vz*u(idna,:)
+      endwhere
+      where((z(:) .lt. 0.0) .and. (u1(imxa,:) .gt. -floor_vz*u(idna,:)))
+        u1(imxa,:) = -floor_vz*u(idna,:)
+      endwhere
+    endif
+#endif /* VZ_LIMITS */
+
+      u(:,:) = u1(:,:)
   enddo
 
-  return
+    return
+
   end subroutine relaxing_tvd
 
+!==========================================================================================
 end module tv
