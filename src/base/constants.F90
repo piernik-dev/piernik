6f5e65f14a82881a1dce91df4a9516a6c9f9aa90
Dominik Wóltański <minikwolt@astri.umk.pl>
2008-11-14 16:33:56 +0000

    Gaus, microgaus and Tesla units added

--- src/base/constants.F90
+++ src/base/constants.F90
@@ -142,6 +142,10 @@ module constants     ! module containg numerical and physical constants !!!
         real, parameter :: kboltz =     1.380622e-16*erg/kelvin
         real, parameter :: gasRconst =  8.31434e7*erg/kelvin !/mol
         real, parameter :: clight =     2.997924562e10*cm/sek
+		  
+		  real, parameter :: Gs     =     4.*pi*sek*sqrt(cm/gram)
+		  real, parameter :: mGs	 =		 Gs*1.e-6
+		  real, parameter :: Tesla  =     1.e4*Gs
 #ifdef PGM
         real, parameter :: newtong =    G_one				! pgm
 #else /* PGM */
