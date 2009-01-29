85351a72d4fc686f9692f87cfdef8ef01ce90759
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2009-01-29 12:27:33 +0000

    GPL

--- src/magnetic/magboundaries.F90
+++ src/magnetic/magboundaries.F90
@@ -1,13 +1,34 @@
 ! $Id$
+!
+! PIERNIK Code Copyright (C) 2006 Michal Hanasz
+!
+!    This file is part of PIERNIK code.
+!
+!    PIERNIK is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    PIERNIK is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with PIERNIK.  If not, see <http://www.gnu.org/licenses/>.
+!
+!    Initial implemetation of PIERNIK code was based on TVD split MHD code by
+!    Ue-Li Pen 
+!        see: Pen, Arras & Wong (2003) for algorithm and
+!             http://www.cita.utoronto.ca/~pen/MHD 
+!             for original source code "mhd.f90" 
+!   
+!    For full list of developers see $PIERNIK_HOME/license/pdt.txt
+!
 #include "piernik.def"
 
 module magboundaries
 
-! Written  by M. Hanasz - January/February 2006
-! Modified by M. Hanasz - MPI comunication in "z"   - April 2006
-! Modified by M. Hanasz - MPI comunication in "xyz" - November 2006
-! Modified by M. Hanasz - MPI corner-periodic bcs   - December 2006
-! Modified by M. Hanasz - MPI shearing-periodic in "x" - November 2007
   use mpisetup
   use fluidindex, only : ibx, iby, ibz  
   use grid, only : nb,nx,ny,nz,nxb,nyb,nzb,nzd
