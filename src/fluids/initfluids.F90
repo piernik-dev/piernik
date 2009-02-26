1173b998e2f2b0abb4d0409748f43b586728759e
Michał Hanasz <mhanasz@astri.umk.pl>
2009-02-26 20:40:45 +0000

    Moving ''gamma'' and ''cs'' to fluid dependend modules

--- src/fluids/initfluids.F90
+++ src/fluids/initfluids.F90
@@ -30,11 +30,13 @@
 module initfluids
 
 #ifdef IONIZED
-  use initionized, only : init_ionized
+  use initionized, only : init_ionized, gamma_ion, cs_iso_ion2
+  use fluidindex,  only : i_ion
 #endif /* IONIZED */
 
 #ifdef NEUTRAL
-  use initneutral, only : init_neutral
+  use initneutral, only : init_neutral, gamma_neu, cs_iso_neu2
+  use fluidindex,  only : i_neu
 #endif /* NEUTRAL */
 
 #ifdef DUST
@@ -44,6 +46,13 @@ module initfluids
 #ifdef COSM_RAYS
   use initcosmicrays, only : init_cosmicrays
 #endif /* COSM_RAYS */
+
+  use fluidindex,   only   : nfluid
+  
+  implicit none
+  
+  real, allocatable :: gamma(:)
+  real              :: cs_iso, cs_iso2
 
   contains
 
@@ -66,6 +75,23 @@ module initfluids
   call init_cosmicrays
 #endif /* COSM_RAYS */
 
+  allocate(gamma(nfluid))
+  
+#if defined NEUTRAL && defined IONIZED
+  if(cs_iso_neu /= cs_iso_ion) then
+      write(*,*) "WARNING: 'cs_iso_neu' and 'cs_iso_ion' should be equal"
+#endif /* defined NEUTRAL && defined IONIZED  */
+
+#ifdef IONIZED
+  gamma(i_ion) = gamma_ion
+  cs_iso   = cs_iso_ion
+  cs_iso2  = cs_iso_ion2
+#endif /* IONIZED */
+#ifdef NEUTRAL  
+  gamma(i_neu) = gamma_neu
+  cs_iso  = cs_iso_neu 
+  cs_iso2 = cs_iso_neu2
+#endif /* NEUTRAL  */ 
 
   end subroutine init_fluids
 
