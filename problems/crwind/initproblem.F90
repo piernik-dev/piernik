49e1e6748afeda496b3be376724602cf9383ecd5
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2009-05-27 10:31:38 +0000

    public: reverting changes to  crwind problem

--- problems/crwind/initproblem.F90
+++ problems/crwind/initproblem.F90
@@ -38,18 +38,23 @@ module initproblem
    use mpisetup
    use grid
    use initionized
+   use initcosmicrays
    use fluidindex, only : ibx,iby,ibz
    use hydrostatic, only : hydrostatic_zeq
 #ifdef SHEAR
    use shear, only : qshear, omega
 #endif /* SHEAR */
 
-   real :: d0, bxn,byn,bzn, alpha
+   real :: d0, bxn,byn,bzn, h_sn, f_sn_kpc2, amp_cr, beta_cr, r_sn
+   real :: ethu, f_sn, amp_ecr_sn, alpha
    character(len=32) :: problem_name
    character(len=3)  :: run_id
 
    namelist /PROBLEM_CONTROL/  problem_name, run_id, &
-                               d0, bxn,byn,bzn, alpha
+                               d0, &
+                               bxn,byn,bzn, &
+                               x0, y0, z0, alpha, &
+                               r_sn, h_sn, f_sn_kpc2, amp_cr, beta_cr
    contains
 
 !-----------------------------------------------------------------------------
@@ -66,6 +71,9 @@ module initproblem
       bxn    = 0.0
       byn    = 1.0
       bzn    = 0.0
+      x0     = 0.0
+      y0     = 0.0
+      z0     = 0.0
 
       if(proc == 0) then
          open(1,file='problem.par')
@@ -87,6 +95,14 @@ module initproblem
          rbuff(2)  = bxn
          rbuff(3)  = byn
          rbuff(4)  = bzn
+         rbuff(5)  = x0
+         rbuff(6)  = y0
+         rbuff(7)  = z0
+         rbuff(8)  = amp_cr
+         rbuff(9)  = beta_cr
+         rbuff(10) = r_sn
+         rbuff(11) = h_sn
+         rbuff(12) = f_sn_kpc2
          rbuff(13) = alpha
 
          call MPI_BCAST(cbuff, 32*buffer_dim, MPI_CHARACTER,        0, comm, ierr)
@@ -106,6 +122,14 @@ module initproblem
          bxn          = rbuff(2)
          byn          = rbuff(3)
          bzn          = rbuff(4)
+         x0           = rbuff(5)
+         y0           = rbuff(6)
+         z0           = rbuff(7)
+         amp_cr       = rbuff(8)
+         beta_cr      = rbuff(9)
+         r_sn         = rbuff(10)
+         h_sn         = rbuff(11)
+         f_sn_kpc2    = rbuff(12)
          alpha        = rbuff(13)
 
 
@@ -122,6 +146,32 @@ module initproblem
 
       integer :: i,j,k
       real :: b0, csim2
+
+      ethu = 7.0**2/(5.0/3.0-1.0) * 1.0    ! thermal energy unit=0.76eV/cm**3
+                                           ! for c_si= 7km/s, n=1/cm^3
+                                           ! gamma=5/3
+
+      amp_ecr_sn = 4.96e6*cr_eff/r_sn**3   ! cosmic ray explosion amplitude
+                                           ! in units:
+                                           ! e_0 = 1/(5/3-1)*rho_0*c_s0**2                                           
+                                           ! rho_0=1.67e-24g/cm**3,
+                                           ! c_s0 = 7km/s
+
+      f_sn = f_sn_kpc2                     ! SN freq
+      
+      if(nxd /=1) then 
+         f_sn = f_sn * (xmax-xmin)/1000.0
+      else 
+         f_sn = f_sn * 2.0*r_sn/1000.0
+      endif
+      
+      if(nyd /=1) then 
+         f_sn = f_sn * (ymax-ymin)/1000.0  
+      else 
+         f_sn = f_sn * 2.0*r_sn/1000.0
+      endif
+
+      write(*,*) proc, csim2
 !   Secondary parameters
 
       b0 = sqrt(2.*alpha*d0*cs_iso2)
@@ -129,7 +179,6 @@ module initproblem
       csim2 = cs_iso2*(1.0+alpha)
  
       call hydrostatic_zeq(1, 1, csim2, d0, dprof)
-      
 
       do k = 1,nz
          do j = 1,ny
@@ -148,7 +197,17 @@ module initproblem
                                + 0.5*(u(imxi,i,j,k)**2 + u(imyi,i,j,k)**2 + &
                                       u(imzi,i,j,k)**2 ) / u(idni,i,j,k)
 #endif /* ISO */
-
+#ifdef COSM_RAYS
+               u(iecr,i,j,k)   =  beta_cr*cs_iso2 * u(idni,i,j,k)/( gamma_cr - 1.0 )
+#ifdef GALAXY
+! Single SN explosion in x0,y0,z0 at t = 0 if amp_cr /= 0
+               u(iecr,i,j,k)= u(iecr,i,j,k) &
+                     + amp_cr*exp(-((x(i)- x0    )**2 + (y(j)- y0    )**2 + (z(k)-z0)**2)/r_sn**2) &
+                     + amp_cr*exp(-((x(i)-(x0+Lx))**2 + (y(j)- y0    )**2 + (z(k)-z0)**2)/r_sn**2) &
+                     + amp_cr*exp(-((x(i)- x0    )**2 + (y(j)-(y0+Ly))**2 + (z(k)-z0)**2)/r_sn**2) &
+                     + amp_cr*exp(-((x(i)-(x0+Lx))**2 + (y(j)-(y0+Ly))**2 + (z(k)-z0)**2)/r_sn**2)
+#endif /* GALAXY */
+#endif /* COSM_RAYS */
             enddo
          enddo
       enddo
@@ -169,6 +228,77 @@ module initproblem
       return
    end subroutine init_prob
 
+!-----------------------------------------------------------------------------
+!
+!   subroutine mass_loss_compensate
+!
+!      implicit none
+!
+!      real tot_mass, dmass
+!      integer i
+!
+!      call total_mass(tot_mass)
+!
+!      mass_loss = max(0.0,init_mass - tot_mass)
+!
+!      dmass = mass_loss/init_mass
+
+!      do i=1,nx
+!         u(idni,:,:,:) = u(idni,:,:,:) + dmass * dinit(:,:,:)
+#ifdef SHEAR
+!         u(imyi,:,:,:) = u(imyi,:,:,:) - dmass * qshear*omega*x(i) * dinit(:,:,:)
+#endif /* SHEAR */
+#ifndef ISO
+!         u(ieni,:,:,:) = u(ieni,:,:,:) + dmass * (c_si**2/(gamma_ion-1.0) * dinit(:,:,:)
+#ifdef SHEAR
+!         u(ieni,:,:,:) = u(ieni,:,:,:) + dmass * 0.5 * (qshear*omega*x(i))**2) * dinit(:,:,:)
+#endif /* SHEAR */
+#endif /* ISO */
+!      enddo
+!
+!   end subroutine mass_loss_compensate
+!
+!=============================================================================
+! Te procedury powinny sie znalezc docelowo w jakims innym module.
+
+!   subroutine save_init_dens
+!
+!      implicit none
+!
+!      real mass
+!
+!      dinit(:,:,:) = u(idni,:,:,:)
+!      mass = sum(dinit(is:ie,js:je,ks:ke)) * dvol
+!      call MPI_ALLREDUCE(mass, init_mass, 1, mpi_real8, mpi_sum, comm3d, ierr)
+!
+!   end subroutine save_init_dens
+
+!-----------------------------------------------------------------------------
+
+!   subroutine get_init_mass
+!
+!      implicit none
+!
+!      real mass
+!
+!      mass = sum(dinit(is:ie,js:je,ks:ke)) * dvol
+!      call MPI_ALLREDUCE(mass, init_mass, 1, mpi_real8, mpi_sum, comm3d, ierr)
+!
+!   end subroutine get_init_mass
+
+!-----------------------------------------------------------------------------
+
+
+!   subroutine total_mass(tmass)
+!
+!      implicit none
+!
+!      real mass, tmass
+!
+!      mass = sum(u(idni,is:ie,js:je,ks:ke)) * dvol
+!      call MPI_ALLREDUCE(mass, tmass, 1, mpi_real8, mpi_sum, comm3d, ierr)
+!
+!   end subroutine total_mass
 
 end module initproblem
 
