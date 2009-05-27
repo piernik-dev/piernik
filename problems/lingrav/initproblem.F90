58cc9ff14f81ba96688d845f8c9dcff281d2d2cd
Michał Hanasz <mhanasz@astri.umk.pl>
2009-05-27 09:27:16 +0000

    public: New problem  lingrav

---
+++ problems/lingrav/initproblem.F90
@@ -0,0 +1,174 @@
+! $Id: initproblem.F90 1036 2009-05-27 09:25:17Z mhanasz $
+!
+! PIERNIK Code Copyright (C) 2006 Michal Hanasz
+!
+!    This file is part of PIERNIK code.
+!
+!    PIERNIK is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    PIERNIK is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with PIERNIK.  If not, see <http://www.gnu.org/licenses/>.
+!
+!    Initial implemetation of PIERNIK code was based on TVD split MHD code by
+!    Ue-Li Pen
+!        see: Pen, Arras & Wong (2003) for algorithm and
+!             http://www.cita.utoronto.ca/~pen/MHD
+!             for original source code "mhd.f90"
+!
+!    For full list of developers see $PIERNIK_HOME/license/pdt.txt
+!
+#include "piernik.def"
+
+module initproblem
+
+! Initial condition for the cosmic ray driven dynamo
+! Based on Parker instability setup
+! Written by: M. Hanasz, February 2006
+! Modified by M.Hanasz for CR-driven dynamo
+
+   use arrays
+   use mpisetup
+   use grid
+   use initionized
+   use fluidindex, only : ibx,iby,ibz
+   use hydrostatic, only : hydrostatic_zeq
+#ifdef SHEAR
+   use shear, only : qshear, omega
+#endif /* SHEAR */
+
+   real :: d0, bxn,byn,bzn, alpha
+   character(len=32) :: problem_name
+   character(len=3)  :: run_id
+
+   namelist /PROBLEM_CONTROL/  problem_name, run_id, &
+                               d0, bxn,byn,bzn, alpha
+   contains
+
+!-----------------------------------------------------------------------------
+
+   subroutine read_problem_par
+      use mpisetup
+      use errh, only : namelist_errh
+      implicit none
+      integer :: ierrh
+
+      problem_name = 'xxx'
+      run_id  = 'aaa'
+      d0     = 1.0
+      bxn    = 0.0
+      byn    = 1.0
+      bzn    = 0.0
+
+      if(proc == 0) then
+         open(1,file='problem.par')
+            read(unit=1,nml=PROBLEM_CONTROL,iostat=ierrh)
+            call namelist_errh(ierrh,'PROBLEM_CONTROL')
+         close(1)
+         open(3, file='tmp.log', position='append')
+         write(3,nml=PROBLEM_CONTROL)
+         write(3,*)
+         close(3)
+      endif
+
+      if(proc == 0) then
+
+         cbuff(1) =  problem_name
+         cbuff(2) =  run_id
+
+         rbuff(1)  = d0
+         rbuff(2)  = bxn
+         rbuff(3)  = byn
+         rbuff(4)  = bzn
+         rbuff(13) = alpha
+
+         call MPI_BCAST(cbuff, 32*buffer_dim, MPI_CHARACTER,        0, comm, ierr)
+         call MPI_BCAST(ibuff,    buffer_dim, MPI_INTEGER,          0, comm, ierr)
+         call MPI_BCAST(rbuff,    buffer_dim, MPI_DOUBLE_PRECISION, 0, comm, ierr)
+
+      else
+
+         call MPI_BCAST(cbuff, 32*buffer_dim, MPI_CHARACTER,        0, comm, ierr)
+         call MPI_BCAST(ibuff,    buffer_dim, MPI_INTEGER,          0, comm, ierr)
+         call MPI_BCAST(rbuff,    buffer_dim, MPI_DOUBLE_PRECISION, 0, comm, ierr)
+
+         problem_name = cbuff(1)
+         run_id       = cbuff(2)
+
+         d0           = rbuff(1)
+         bxn          = rbuff(2)
+         byn          = rbuff(3)
+         bzn          = rbuff(4)
+         alpha        = rbuff(13)
+
+
+      endif
+
+   end subroutine read_problem_par
+
+!-----------------------------------------------------------------------------
+
+   subroutine init_prob
+      use initfluids, only : cs_iso2
+
+      implicit none
+
+      integer :: i,j,k
+      real :: b0, csim2
+!   Secondary parameters
+
+      b0 = sqrt(2.*alpha*d0*cs_iso2)
+
+      csim2 = cs_iso2*(1.0+alpha)
+ 
+      call hydrostatic_zeq(1, 1, csim2, d0, dprof)
+      
+
+      do k = 1,nz
+         do j = 1,ny
+            do i = 1,nx
+               u(idni,i,j,k)   = max(smalld,dprof(k))
+
+               u(imxi,i,j,k) = 0.0
+               u(imyi,i,j,k) = 0.0
+               u(imzi,i,j,k) = 0.0
+#ifdef SHEAR
+               u(imyi,i,j,k) = -qshear*omega*x(i)*u(idni,i,j,k)
+#endif /* SHEAR */
+
+#ifndef ISO
+               u(ieni,i,j,k)   = cs_iso2/(gamma_ion-1.0) * u(idni,i,j,k) &
+                               + 0.5*(u(imxi,i,j,k)**2 + u(imyi,i,j,k)**2 + &
+                                      u(imzi,i,j,k)**2 ) / u(idni,i,j,k)
+#endif /* ISO */
+
+            enddo
+         enddo
+      enddo
+
+      do k = 1,nz
+         do j = 1,ny
+            do i = 1,nx
+               b(ibx,i,j,k)   = b0*sqrt(u(idni,i,j,k)/d0)* bxn/sqrt(bxn**2+byn**2+bzn**2)
+               b(iby,i,j,k)   = b0*sqrt(u(idni,i,j,k)/d0)* byn/sqrt(bxn**2+byn**2+bzn**2)
+               b(ibz,i,j,k)   = b0*sqrt(u(idni,i,j,k)/d0)* bzn/sqrt(bxn**2+byn**2+bzn**2)
+#ifndef ISO
+               u(ieni,i,j,k)   = u(ieni,i,j,k) +0.5*sum(b(:,i,j,k)**2,1)
+#endif /* ISO */
+            enddo
+         enddo
+      enddo
+
+      return
+   end subroutine init_prob
+
+
+end module initproblem
+
