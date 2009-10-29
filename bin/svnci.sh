dbc455a7be88c118bff3582a020f8b022be0936f
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2009-10-29 12:10:11 +0000

    public: adding tool for automated commit taging

---
+++ bin/svnci.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# copyright A. Gawryszczak 2009
+
+url=$( svn info | grep URL | awk '{print $NF}' )
+root=$( LC_ALL=C LANG=C LANGUAGE=C svn info | grep "Repository Root: " | awk '{print $NF}' )
+relpath=$( echo $url | sed "s^$root/^^" )
+last=$( basename $relpath )
+
+#echo $url $root $relpath $last
+
+cmdb='svn ci -m '"'"'[source:'"$relpath"' '"$last"']: '
+cmde="'"' .'
+
+if [ $# -lt 1 ] ; then
+	echo "Usage: $0 comment"
+	echo ${cmdb}${cmde}
+	exit 1
+fi
+
+eval "${cmdb}$*${cmde}"
