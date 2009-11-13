7127725c1a92cef356103802fedb10e13c767150
Dominik Wóltański <minikwolt@astri.umk.pl>
2009-11-13 13:58:10 +0000

    public: doxy table in  initneutral module

--- src/fluids/neutral/initneutral.F90
+++ src/fluids/neutral/initneutral.F90
@@ -18,19 +18,31 @@
 !    along with PIERNIK.  If not, see <http://www.gnu.org/licenses/>.
 !
 !    Initial implemetation of PIERNIK code was based on TVD split MHD code by
-!    Ue-Li Pen 
+!    Ue-Li Pen
 !        see: Pen, Arras & Wong (2003) for algorithm and
-!             http://www.cita.utoronto.ca/~pen/MHD 
-!             for original source code "mhd.f90" 
-!   
+!             http://www.cita.utoronto.ca/~pen/MHD
+!             for original source code "mhd.f90"
+!
 !    For full list of developers see $PIERNIK_HOME/license/pdt.txt
 !
 #include "piernik.def"
 
-!> 
+!>
 !! \brief (MH) Initialization of the neutral fluid
 !!
 !!
+!!
+!! In this module following namelist of parameters is specified:
+!!
+!! @b FLUID_NEUTRAL
+!!
+!! \f[ \begin{tabular}{ | p{3cm} | p{3cm} | p{4cm} | p{8cm} | } \hline &&&\\
+!! {\bf parameter} & {\bf default value} & {\bf possible values} & {\bf description} \\ &&&\\ \hline \hline &&&\\
+!! gamma\_neu   & 1.66666666 & real value &  \\ &&&\\ \hline &&&\\
+!! cs\_iso\_neu & 1.0        & real value &  \\ &&&\\ \hline &&&\\
+!! cs\_gas\_neu &            & real value &  \\ &&&\\ \hline &&&\\
+!! csvk         &            & real value &  \\ &&&\\ \hline
+!! \end{tabular} \f]
 !<
 
 module initneutral
@@ -42,9 +54,9 @@ module initneutral
     integer               :: idnn, imxn, imyn, imzn
 #ifndef ISO
     integer               :: ienn
-#endif /* ISO */    
+#endif /* ISO */
 
-    integer, allocatable, dimension(:)  :: iarr_neu 
+    integer, allocatable, dimension(:)  :: iarr_neu
     integer, allocatable, dimension(:)  :: iarr_neu_swpx, iarr_neu_swpy, iarr_neu_swpz
 
   contains
@@ -62,7 +74,7 @@ module initneutral
     character par_file*(100), tmp_log_file*(100)
 
     namelist /FLUID_NEUTRAL/ gamma_neu, cs_iso_neu, eta_gas_neu, csvk
-    
+
       gamma_neu  = 1.66666666
       cs_iso_neu = 1.0
 
@@ -79,53 +91,53 @@ module initneutral
          close(3)
       endif
 
- 
+
     if(proc .eq. 0) then
 
       rbuff(1)   = gamma_neu
       rbuff(2)   = cs_iso_neu
       rbuff(3)   = eta_gas_neu
       rbuff(4)   = csvk
-    
+
       call MPI_BCAST(cbuff, 32*buffer_dim, MPI_CHARACTER,        0, comm, ierr)
       call MPI_BCAST(ibuff,    buffer_dim, MPI_INTEGER,          0, comm, ierr)
       call MPI_BCAST(rbuff,    buffer_dim, MPI_DOUBLE_PRECISION, 0, comm, ierr)
 
     else
-    
+
       call MPI_BCAST(cbuff, 32*buffer_dim, MPI_CHARACTER,        0, comm, ierr)
       call MPI_BCAST(ibuff,    buffer_dim, MPI_INTEGER,          0, comm, ierr)
       call MPI_BCAST(rbuff,    buffer_dim, MPI_DOUBLE_PRECISION, 0, comm, ierr)
-      
-      gamma_neu   = rbuff(1)  
-      cs_iso_neu  = rbuff(2)  
+
+      gamma_neu   = rbuff(1)
+      cs_iso_neu  = rbuff(2)
       eta_gas_neu = rbuff(3)
       csvk        = rbuff(4)
 
     endif
 
-    cs_iso_neu2      = cs_iso_neu**2 
+    cs_iso_neu2      = cs_iso_neu**2
 #ifdef SHEAR
     global_gradP_neu = 2.0*omega*eta_gas_neu * cs_iso_neu / csvk
 #else
     global_gradP_neu = 0.0
 #endif /* SHEAR */
-   
+
   end subroutine init_neutral
 
 
 
   subroutine neutral_index(nvar,nvar_neu)
-  
+
     implicit none
     integer :: nvar, nvar_neu
 
-   
+
       idnn = nvar + 1
       imxn = nvar + 2
       imyn = nvar + 3
       imzn = nvar + 4
-           
+
 #ifdef ISO
       nvar_neu      = 4
       nvar          = imzn
@@ -136,25 +148,25 @@ module initneutral
       write(*,*) 'iarr_neu_swpz = ', allocated(iarr_neu_swpz)
       allocate(iarr_neu(nvar_neu),iarr_neu_swpx(nvar_neu), iarr_neu_swpy(nvar_neu), iarr_neu_swpz(nvar_neu))
 
-      iarr_neu      = [idnn,imxn,imyn,imzn] 
+      iarr_neu      = [idnn,imxn,imyn,imzn]
       iarr_neu_swpx = [idnn,imxn,imyn,imzn]
       iarr_neu_swpy = [idnn,imyn,imxn,imzn]
       iarr_neu_swpz = [idnn,imzn,imyn,imxn]
 #else
       ienn          = nvar + 5
       nvar_neu      = 5
-      nvar          = ienn 
+      nvar          = ienn
 
-      allocate(iarr_neu(nvar_neu),iarr_neu_swpx(nvar_neu), iarr_neu_swpy(nvar_neu), iarr_neu_swpz(nvar_neu))     
+      allocate(iarr_neu(nvar_neu),iarr_neu_swpx(nvar_neu), iarr_neu_swpy(nvar_neu), iarr_neu_swpz(nvar_neu))
 
-      iarr_neu      = [idnn,imxn,imyn,imzn,ienn] 
+      iarr_neu      = [idnn,imxn,imyn,imzn,ienn]
       iarr_neu_swpx = [idnn,imxn,imyn,imzn,ienn]
       iarr_neu_swpy = [idnn,imyn,imxn,imzn,ienn]
       iarr_neu_swpz = [idnn,imzn,imyn,imxn,ienn]
-#endif /* ISO */   
+#endif /* ISO */
 
 !      write(*,*) 'neutral_index', iarr_neu_swpx
-      
+
    end subroutine neutral_index
 
 end module initneutral
