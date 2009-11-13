4a4f4642ac33bbcca3fcc2e502b41e2247903d70
Dominik Wóltański <minikwolt@astri.umk.pl>
2009-11-13 13:54:50 +0000

    public: doxy table in  initdust module

--- src/fluids/dust/initdust.F90
+++ src/fluids/dust/initdust.F90
@@ -18,18 +18,28 @@
 !    along with PIERNIK.  If not, see <http://www.gnu.org/licenses/>.
 !
 !    Initial implemetation of PIERNIK code was based on TVD split MHD code by
-!    Ue-Li Pen 
+!    Ue-Li Pen
 !        see: Pen, Arras & Wong (2003) for algorithm and
-!             http://www.cita.utoronto.ca/~pen/MHD 
-!             for original source code "mhd.f90" 
-!   
+!             http://www.cita.utoronto.ca/~pen/MHD
+!             for original source code "mhd.f90"
+!
 !    For full list of developers see $PIERNIK_HOME/license/pdt.txt
 !
 #include "piernik.def"
-!> 
+!>
 !! \brief (MH) Initialization of the dust fluid
 !!
 !!
+!!
+!! In this module following namelist of parameters is specified:
+!!
+!! @b FLUID_DUST
+!!
+!! \f[ \begin{tabular}{ | p{3cm} | p{3cm} | p{4cm} | p{8cm} | } \hline &&&\\
+!! {\bf parameter} & {\bf default value} & {\bf possible values} & {\bf description} \\ &&&\\ \hline \hline &&&\\
+!! dragc\_gas\_dust & 1.0   & real value &  \\ &&&\\ \hline &&&\\
+!! dalpha           & 1.0   & real value &  \\ &&&\\ \hline
+!! \end{tabular} \f]
 !<
 
 module initdust
@@ -39,7 +49,7 @@ module initdust
     integer               :: idnd, imxd, imyd, imzd
     real                  :: dragc_gas_dust, taus, dalpha
 
-    integer, allocatable, dimension(:)  :: iarr_dst 
+    integer, allocatable, dimension(:)  :: iarr_dst
     integer, allocatable, dimension(:)  :: iarr_dst_swpx, iarr_dst_swpy, iarr_dst_swpz
 
   contains
@@ -50,11 +60,11 @@ module initdust
     use mpisetup
     use errh, only : namelist_errh
     implicit none
-    integer :: ierrh      
+    integer :: ierrh
     character(LEN=100) :: par_file, tmp_log_file
 
     namelist /FLUID_DUST/ dragc_gas_dust, dalpha
-    
+
       dragc_gas_dust  = 1.0
       dalpha = 1.0
 
@@ -71,19 +81,19 @@ module initdust
          close(3)
       endif
 
- 
+
     if(proc .eq. 0) then
 
       rbuff(1)   = dragc_gas_dust
       rbuff(2)   = dalpha
-    
+
       call MPI_BCAST(rbuff,    buffer_dim, MPI_DOUBLE_PRECISION, 0, comm, ierr)
 
     else
-    
+
       call MPI_BCAST(rbuff,    buffer_dim, MPI_DOUBLE_PRECISION, 0, comm, ierr)
-      
-      dragc_gas_dust  = rbuff(1)  
+
+      dragc_gas_dust  = rbuff(1)
       dalpha          = rbuff(2)
 
     endif
@@ -94,27 +104,27 @@ module initdust
 
 
   subroutine dust_index(nvar,nvar_dst)
-  
+
     implicit none
     integer :: nvar, nvar_dst
 
-   
+
       idnd = nvar + 1
       imxd = nvar + 2
       imyd = nvar + 3
       imzd = nvar + 4
-           
+
       nvar_dst      = 4
       nvar          = imzd
 
       allocate(iarr_dst(nvar_dst),iarr_dst_swpx(nvar_dst), iarr_dst_swpy(nvar_dst), iarr_dst_swpz(nvar_dst))
 
-      iarr_dst      = [idnd,imxd,imyd,imzd] 
+      iarr_dst      = [idnd,imxd,imyd,imzd]
       iarr_dst_swpx = [idnd,imxd,imyd,imzd]
       iarr_dst_swpy = [idnd,imyd,imxd,imzd]
       iarr_dst_swpz = [idnd,imzd,imyd,imxd]
 
-      
+
    end subroutine dust_index
 
 end module initdust
