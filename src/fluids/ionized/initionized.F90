4f56797b2745c3d119b6b4809dddbd46f907fdaf
Dominik Wóltański <minikwolt@astri.umk.pl>
2009-11-13 13:56:08 +0000

    public: doxy table in  initionized module

--- src/fluids/ionized/initionized.F90
+++ src/fluids/ionized/initionized.F90
@@ -18,19 +18,30 @@
 !    along with PIERNIK.  If not, see <http://www.gnu.org/licenses/>.
 !
 !    Initial implemetation of PIERNIK code was based on TVD split MHD code by
-!    Ue-Li Pen 
+!    Ue-Li Pen
 !        see: Pen, Arras & Wong (2003) for algorithm and
-!             http://www.cita.utoronto.ca/~pen/MHD 
-!             for original source code "mhd.f90" 
-!   
+!             http://www.cita.utoronto.ca/~pen/MHD
+!             for original source code "mhd.f90"
+!
 !    For full list of developers see $PIERNIK_HOME/license/pdt.txt
 !
 #include "piernik.def"
 
-!> 
+!>
 !! \brief (MH) Initialization of the ionized fluid
 !!
 !!
+!!
+!! In this module following namelist of parameters is specified:
+!!
+!! @b FLUID_IONIZED
+!!
+!! \f[ \begin{tabular}{ | p{3cm} | p{3cm} | p{4cm} | p{8cm} | } \hline &&&\\
+!! {\bf parameter} & {\bf default value} & {\bf possible values} & {\bf description} \\ &&&\\ \hline \hline &&&\\
+!! gamma\_ion   & 1.66666666 & real value &  \\ &&&\\ \hline &&&\\
+!! cs\_iso\_ion & 1.0        & real value &  \\ &&&\\ \hline &&&\\
+!! cs\_ion      &            & real value &  \\ &&&\\ \hline
+!! \end{tabular} \f]
 !<
 
 module initionized
@@ -42,9 +53,9 @@ module initionized
     integer               :: idni, imxi, imyi, imzi
 #ifndef ISO
     integer               :: ieni
-#endif /* ISO */    
+#endif /* ISO */
 
-    integer, allocatable, dimension(:)  :: iarr_ion 
+    integer, allocatable, dimension(:)  :: iarr_ion
     integer, allocatable, dimension(:)  :: iarr_ion_swpx, iarr_ion_swpy, iarr_ion_swpz
 
  contains
@@ -59,7 +70,7 @@ module initionized
     character par_file*(100), tmp_log_file*(100)
 
     namelist /FLUID_IONIZED/ gamma_ion, cs_iso_ion, cs_ion
-    
+
       gamma_ion  = 1.66666666
       cs_iso_ion = 1.0
 
@@ -74,31 +85,31 @@ module initionized
            write(3,nml=FLUID_IONIZED)
            write(3,*)
          close(3)
-      endif 
- 
- 
+      endif
+
+
     if(proc .eq. 0) then
 
       rbuff(1)   = gamma_ion
       rbuff(2)   = cs_iso_ion
       rbuff(3)   = cs_ion
-    
+
       call MPI_BCAST(cbuff, 32*buffer_dim, MPI_CHARACTER,        0, comm, ierr)
       call MPI_BCAST(ibuff,    buffer_dim, MPI_INTEGER,          0, comm, ierr)
       call MPI_BCAST(rbuff,    buffer_dim, MPI_DOUBLE_PRECISION, 0, comm, ierr)
 
     else
-    
+
       call MPI_BCAST(cbuff, 32*buffer_dim, MPI_CHARACTER,        0, comm, ierr)
       call MPI_BCAST(ibuff,    buffer_dim, MPI_INTEGER,          0, comm, ierr)
       call MPI_BCAST(rbuff,    buffer_dim, MPI_DOUBLE_PRECISION, 0, comm, ierr)
-      
-      gamma_ion  = rbuff(1)  
-      cs_iso_ion = rbuff(2)  
-      cs_ion     = rbuff(3)  
+
+      gamma_ion  = rbuff(1)
+      cs_iso_ion = rbuff(2)
+      cs_ion     = rbuff(3)
 
     endif
-    
+
     cs_iso_ion2  = cs_iso_ion**2
 
   end subroutine init_ionized
@@ -106,41 +117,41 @@ module initionized
 
 
   subroutine ionized_index(nvar,nvar_ion)
-  
+
     implicit none
     integer :: nvar, nvar_ion
 
-   
+
       idni = nvar + 1
       imxi = nvar + 2
       imyi = nvar + 3
       imzi = nvar + 4
-           
+
 #ifdef ISO
       nvar_ion      = 4
       nvar          = imzi
 
       allocate(iarr_ion(nvar_ion),iarr_ion_swpx(nvar_ion), iarr_ion_swpy(nvar_ion), iarr_ion_swpz(nvar_ion))
 
-      iarr_ion      = [idni,imxi,imyi,imzi] 
+      iarr_ion      = [idni,imxi,imyi,imzi]
       iarr_ion_swpx = [idni,imxi,imyi,imzi]
       iarr_ion_swpy = [idni,imyi,imxi,imzi]
       iarr_ion_swpz = [idni,imzi,imyi,imxi]
 #else
       ieni          = nvar + 5
       nvar_ion      = 5
-      nvar          = ieni 
+      nvar          = ieni
 
-      allocate(iarr_ion(nvar_ion),iarr_ion_swpx(nvar_ion), iarr_ion_swpy(nvar_ion), iarr_ion_swpz(nvar_ion))     
+      allocate(iarr_ion(nvar_ion),iarr_ion_swpx(nvar_ion), iarr_ion_swpy(nvar_ion), iarr_ion_swpz(nvar_ion))
 
-      iarr_ion      = [idni,imxi,imyi,imzi,ieni] 
+      iarr_ion      = [idni,imxi,imyi,imzi,ieni]
       iarr_ion_swpx = [idni,imxi,imyi,imzi,ieni]
       iarr_ion_swpy = [idni,imyi,imxi,imzi,ieni]
       iarr_ion_swpz = [idni,imzi,imyi,imxi,ieni]
-#endif /* ISO */   
+#endif /* ISO */
 
 !      write(*,*) 'ionized_index', iarr_ion_swpx
-      
+
    end subroutine ionized_index
 
 end module initionized
