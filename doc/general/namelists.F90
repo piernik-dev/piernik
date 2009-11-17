a566f928b03b6859b689f564d6bce12096ed39ae
Dominik Wóltański <minikwolt@astri.umk.pl>
2009-11-17 14:40:27 +0000

    public: adding new doxy page with all namelists parameters description

---
+++ doc/general/namelists.F90
@@ -0,0 +1,23 @@
+!>
+!! \page namelists Namelists descriptions
+!!
+!! \par All about namelists of parameters in Piernik MHD.
+!!
+!! To change values of namelists parameters you should edit problem.par file.
+!!
+!! In the following section all namelists and its parameters descriptions are gathered.
+!!
+!! \copydetails dataio::init_dataio
+!! \copydetails gravity::init_grav
+!! \copydetails grid::init_grid
+!! \copydetails initcosmicrays::init_cosmicrays
+!! \copydetails inidust::init_dust
+!! \copydetails initionized::init_ionized
+!! \copydetails initneutral::init_neutral
+!! \copydetails interactions::init_interactions
+!! \copydetails mpisetup::mpistart
+!! \copydetails resistivity::init_resistivity
+!! \copydetails shear::init_shear
+!<
+
+
