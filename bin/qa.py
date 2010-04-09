6ec933c006b3390fd10a242ab3acb52446cc2149
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2010-04-09 14:39:09 +0000

    public: Updating qa.py

--- bin/qa.py
+++ bin/qa.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import re
 import subprocess as sp
 
 class bcolors:
@@ -29,11 +30,35 @@ def remove_binaries(files):
          list.append(file)
    return list
 
+def select_sources(files):
+   list = []
+   for file in files:
+      if( re.search("F90$",file)):
+         list.append(file)
+   return list
+
 def qa_checks(files,options):
    print b.OKBLUE + '"I am the purifier, the light that clears all shadows." - seal of cleansing inscription' + b.ENDC
    files = remove_binaries(files)
+   f90files = select_sources(files)
    qa_trailing_spaces(files,options)
-   qa_nonconforming_tabs(files,options)
+   nt = qa_nonconforming_tabs(files,options)
+   wc = qa_depreciated_syntax(f90files,options)
+   if (wc):
+      s =  b.WARNING + "%i warnings detected. " % wc + b.ENDC + "Do you wish to proceed? (y/N) "
+      if( raw_input(s) != 'y' ):
+         print "See you later!"
+         exit()
+   else:
+      print b.OKGREEN + "No warnings detected. " + b.ENDC + "If everyone were like you, I'd be out of business!"
+   if (len(nt)):
+      print b.FAIL + "%i error(s) detected! " % len(nt) + b.ENDC + "I will not let you commit unless you force me!!!"
+      if options.force:
+         print "Damn! You are determined to make low quality commit :-("
+      else:
+         exit()
+   else:
+      print b.OKGREEN + "Yay! No errors!!! " + b.ENDC
 
 def qa_nonconforming_tabs(files,options):
    print b.OKGREEN + "QA: " + b.ENDC + "Checking for tabs"
@@ -42,12 +67,7 @@ def qa_nonconforming_tabs(files,options):
       if (len(sp.Popen('grep "	" '+file, shell=True, executable="/bin/bash",stdout=sp.PIPE).communicate()[0])):
          print b.FAIL + "QA:  " + b.ENDC + "non conforming tab detected in " + file
          wrong_files.append(file)
-   if (len(wrong_files)):
-      print b.FAIL + "Error detected! " + b.ENDC + "I will not let you commit unless you force me!!!"
-      if options.force:
-         print "Damn! You are determined to make low quality commit :-("
-      else:
-         exit()
+   return wrong_files
 
 def qa_trailing_spaces(files,options):
    print b.OKGREEN + "QA: " + b.ENDC + "Removing trailing whitespaces"
@@ -55,6 +75,27 @@ def qa_trailing_spaces(files,options):
       sp.Popen('sed -i -e "s/\s\+$//" '+file, shell=True, executable="/bin/bash")
       if options.verbose:
          print b.OKGREEN + "QA: " + b.ENDC + " done cleansing in " + file.rstrip('\n')
+
+def qa_depreciated_syntax(files,options):
+   print b.OKGREEN + "QA: " + b.ENDC + "Checking for depreciated syntax"
+   warning = 0
+   for name in files:
+      file = open(name)
+      n = 1
+      for line in file.readlines():
+         if( re.search("^\s{1,12}(?i)(?:real(?:\s|,)|integer(?:\s|,)|logical(?:\s|,|\()|character(?:\s|,))(?!.*::)",line) and
+            not re.search("(?i)\sfunction\s",line)):
+               print b.WARNING + "!! lacking ::   " + b.ENDC + name + " @ L%i => " % n + line.strip()
+               warning=warning+1
+         if( re.search("^\s{1,12}(?i)use[\s](?!.*only)",line)):
+               print b.WARNING + "!! greedy use   " + b.ENDC + name + " @ L%i => " % n + line.strip()
+               warning=warning+1
+         if( re.search("^\s{1,12}(?i)character(?![(])",line)):
+               print b.WARNING + "!! wrong syntax " + b.ENDC + name + " @ L%i => " % n + line.strip()
+               warning=warning+1
+         n = n + 1
+      file.close()
+   return warning
 
 if __name__ == "__main__":
    from optparse import OptionParser
