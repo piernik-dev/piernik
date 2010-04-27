808bbf2b9821644214fb13e33fe2ab9ef13d12be
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2010-04-27 10:34:03 +0000

    public: Merging with multigrid

--- problems/jeans/initproblem.F90
+++ problems/jeans/initproblem.F90
@@ -66,7 +66,6 @@ contains
          open(1,file=par_file)
             read(unit=1,nml=PROBLEM_CONTROL,iostat=ierrh)
             call namelist_errh(ierrh,'PROBLEM_CONTROL')
-            write(*,nml=PROBLEM_CONTROL)
          close(1)
          open(3, file=tmp_log_file, position='append')
             write(3,nml=PROBLEM_CONTROL)
@@ -130,24 +129,23 @@ contains
       ky = dpi/lamy
       kz = dpi/lamz
 
-      cs0 = sqrt(gamma_ion*p0/d0)
-      kn  = sqrt(kx**2+ky**2+kz**2)
-      omg = sqrt(cs0**2 * kn**2 - fpiG*d0)
-      kJ  = sqrt(fpiG*d0)/cs0
-      Lbox = 0.5*sqrt(pi*gamma_ion*p0/newtong / d0**2)
-      Tamp = (d0*amp**2*omg**2*Lbox**2)/(8.0*kn**2)
-
-      write(*,*) 'Unperturbed adiabatic sound speed = ', cs0
-      write(*,*) 'Gravitational constant * 4pi      = ', fpiG
-      write(*,*) 'Lbox                              = ', Lbox
-      write(*,*) 'gamma                             = ', gamma_ion
-      write(*,*) 'Perturbation wavenumber           = ', kn
-      write(*,*) 'Jeans wavenumber                  = ', kJ
-      write(*,*) 'characteristic frequency          = ', omg
-      write(*,*) '------------------------------------'
-      write(*,*) 'T(t) = ',Tamp,'*[1 - cos(',2.0*omg,'t)]'
-      write(*,*) '------------------------------------'
-      write(*,*) 'Divide T(t) for .tsl by L to get proper amplitude !'
+      if (proc == 0) then
+         write(*,*) 'Unperturbed adiabatic sound speed = ', cs0
+         write(*,*) 'Gravitational constant * 4pi      = ', fpiG
+         write(*,*) 'Lbox                              = ', Lbox
+         write(*,*) 'gamma                             = ', gamma_ion
+         write(*,*) 'Perturbation wavenumber           = ', kn
+         write(*,*) 'Jeans wavenumber                  = ', kJ
+         if (omg2>0) then
+            write(*,*) 'characteristic frequency          = ', omg
+            write(*,*)
+            write(*,*) 'T(t) = ',Tamp,'*[1 - cos(',2.0*omg,'t)]'
+         else
+            write(*,*) 'characteristic timescale          = ', omg
+            !write(*,*) 'Something like T(t) = ',Tamp,'* exp(',omg,'t)]' !BEWARE the formula is not completed
+         end if
+         write(*,'(/,a)') 'Divide T(t) for .tsl by L to get proper amplitude !'
+      end if
 ! Uniform equilibrium state
 
       do k = 1,nz
@@ -159,10 +157,9 @@ contains
                u(idni,i,j,k)   = d0 * (1.d0 + amp* dcos(kx*xi+ky*yj+kz*zk))
                pres            = p0 * (1.d0 + gamma_ion*amp*dcos(kx*xi+ky*yj+kz*zk))
 
-               u(imxi:imzi,i,j,k)   = 0.0
+               u(imxi:imzi,i,j,k) = 0.0
 #ifndef ISO
-               u(ieni,i,j,k)   = pres/(gamma_ion-1.0) + &
-                            0.5*sum(u(imxi:imzi,i,j,k)**2,1)/u(idni,i,j,k)
+               u(ieni,i,j,k)      = pres/(gamma_ion-1.0) + 0.5*sum(u(imxi:imzi,i,j,k)**2,1) / u(idni,i,j,k)
 
                b(:,i,j,k)      = 0.0
                u(ieni,i,j,k)   = u(ieni,i,j,k) + 0.5*sum(b(:,i,j,k)**2,1)
@@ -173,12 +170,11 @@ contains
 
     Tamp_aux = 10**int(log(Tamp)/log(10.))
     Tamp_rounded = (int(Tamp/Tamp_aux)+1)*Tamp_aux
-    if(proc == 0) then
-      write(*,*) '======================================'
-      write(*,*) 'Run:'
-      write(*,*) ' % gnuplot verify.gpl; display jeans-*.png'
-      write(*,*) 'to verify results'
-      write(*,*) '======================================'
+    if (proc == 0) then
+      write(*,'(/,a)') 'Run:'
+      write(*,'(a)')   ' % gnuplot verify.gpl; display jeans-*.png'
+      write(*,'(a,/)') 'to verify results'
+
       open(137,file="verify.gpl",status="unknown")
          write(137,*) "set sample 1000"
          write(137,*) "set term png #font luximr"
