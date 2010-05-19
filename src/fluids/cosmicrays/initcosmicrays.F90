9f3945f4210d26b2c29119076ed0b42ac787375a
Artur Gawryszczak <gawrysz@gmail.com>
2010-05-19 13:20:06 +0000

    trunk: fixed use of uninitialized value in initcosmicrays

--- src/fluids/cosmicrays/initcosmicrays.F90
+++ src/fluids/cosmicrays/initcosmicrays.F90
@@ -37,183 +37,186 @@
 !<
 module initcosmicrays
 
-  implicit none
+   implicit none
 
-    integer,parameter                   :: ncr_max = 9 !< maximum number of CR nuclear and electron components
-    integer                             :: ncrn         !< number of CR nuclear  components
-    integer                             :: ncre         !< number of CR electron components
+   integer, parameter                  :: ncr_max = 9  !< maximum number of CR nuclear and electron components
+   integer                             :: ncrn         !< number of CR nuclear  components
+   integer                             :: ncre         !< number of CR electron components
 
-    real                                :: cfl_cr       !< CFL number for diffusive CR transport
-    real                                :: smallecr     !< floor value for CR energy density
-    real                                :: cr_active    !< parameter specifying whether CR pressure gradient is (when =1.) or isn't (when =0.) included in the gas equation of motion
-    real                                :: cr_eff       !< conversion rate of SN explosion energy to CR energy (default = 0.1)
+   real                                :: cfl_cr       !< CFL number for diffusive CR transport
+   real                                :: smallecr     !< floor value for CR energy density
+   real                                :: cr_active    !< parameter specifying whether CR pressure gradient is (when =1.) or isn't (when =0.) included in the gas equation of motion
+   real                                :: cr_eff       !< conversion rate of SN explosion energy to CR energy (default = 0.1)
 
-    real, dimension(ncr_max)            :: gamma_crn    !< array containing adiabatic indexes of all CR nuclear components
-    real, dimension(ncr_max)            :: K_crn_paral  !< array containing parallel diffusion coefficients of all CR nuclear components
-    real, dimension(ncr_max)            :: K_crn_perp   !< array containing perpendicular diffusion coefficients of all CR nuclear components
-    real, dimension(ncr_max)            :: gamma_cre    !< array containing adiabatic indexes of all CR nuclear components
-    real, dimension(ncr_max)            :: K_cre_paral  !< array containing parallel diffusion coefficients of all CR electron components
-    real, dimension(ncr_max)            :: K_cre_perp   !< array containing perpendicular diffusion coefficients of all CR electron components
+   real, dimension(ncr_max)            :: gamma_crn    !< array containing adiabatic indexes of all CR nuclear components
+   real, dimension(ncr_max)            :: K_crn_paral  !< array containing parallel diffusion coefficients of all CR nuclear components
+   real, dimension(ncr_max)            :: K_crn_perp   !< array containing perpendicular diffusion coefficients of all CR nuclear components
+   real, dimension(ncr_max)            :: gamma_cre    !< array containing adiabatic indexes of all CR nuclear components
+   real, dimension(ncr_max)            :: K_cre_paral  !< array containing parallel diffusion coefficients of all CR electron components
+   real, dimension(ncr_max)            :: K_cre_perp   !< array containing perpendicular diffusion coefficients of all CR electron components
 
-    integer, allocatable, dimension(:)  :: iarr_crn     !< array of indexes pointing to all CR nuclear components
-    integer, allocatable, dimension(:)  :: iarr_cre     !< array of indexes pointing to all CR electron components
-    integer, allocatable, dimension(:)  :: iarr_crs     !< array of indexes pointing to all CR components
+   integer, allocatable, dimension(:)  :: iarr_crn     !< array of indexes pointing to all CR nuclear components
+   integer, allocatable, dimension(:)  :: iarr_cre     !< array of indexes pointing to all CR electron components
+   integer, allocatable, dimension(:)  :: iarr_crs     !< array of indexes pointing to all CR components
 
-    real,    allocatable, dimension(:)  :: gamma_crs    !< array containing adiabatic indexes of all CR components
-    real,    allocatable, dimension(:)  :: K_crs_paral  !< array containing parallel diffusion coefficients of all CR components
-    real,    allocatable, dimension(:)  :: K_crs_perp   !< array containing perpendicular diffusion coefficients of all CR components
+   real,    allocatable, dimension(:)  :: gamma_crs    !< array containing adiabatic indexes of all CR components
+   real,    allocatable, dimension(:)  :: K_crs_paral  !< array containing parallel diffusion coefficients of all CR components
+   real,    allocatable, dimension(:)  :: K_crs_perp   !< array containing perpendicular diffusion coefficients of all CR components
 
- contains
+contains
 
-!>
-!! \brief Routine to set parameters values from namelist COSMIC_RAYS
-!!
-!! \n \n
-!! @b COSMIC_RAYS
-!! \n \n
-!! <table border="+1">
-!! <tr><td width="150pt"><b>parameter</b></td><td width="135pt"><b>default value</b></td><td width="200pt"><b>possible values</b></td><td width="315pt"> <b>description</b></td></tr>
-!! <tr><td>cfl_cr    </td><td>0.9  </td><td>real value</td><td>\copydoc initcosmicrays::cfl_cr</td></tr>
-!! <tr><td>smallecr  </td><td>0.0  </td><td>real value</td><td>\copydoc initcosmicrays::smallecr</td></tr>
-!! <tr><td>cr_active </td><td>1.0  </td><td>real value</td><td>\copydoc initcosmicrays::cr_active</td></tr>
-!! <tr><td>gamma_cr  </td><td>4/3  </td><td>real value</td><td>\copydoc initcosmicrays::gamma_cr</td></tr>
-!! <tr><td>cr_eff    </td><td>0.1  </td><td>real value</td><td>\copydoc initcosmicrays::cr_eff</td></tr>
-!! <tr><td>K_cr_paral</td><td>0.0  </td><td>real value</td><td>\copydoc initcosmicrays::K_cr_paral</td></tr>
-!! <tr><td>K_cr_perp </td><td>0.0  </td><td>real value</td><td>\copydoc initcosmicrays::K_cr_perp</td></tr>
-!! </table>
-!! \n \n
-!<
- subroutine init_cosmicrays
-    use errh,     only: namelist_errh
-    use mpisetup, only: proc, ibuff, rbuff, comm, ierr, MPI_DOUBLE_PRECISION, MPI_INTEGER, cwd, buffer_dim
+   !>
+   !! \brief Routine to set parameters values from namelist COSMIC_RAYS
+   !!
+   !! \n \n
+   !! @b COSMIC_RAYS
+   !! \n \n
+   !! <table border="+1">
+   !! <tr><td width="150pt"><b>parameter</b></td><td width="135pt"><b>default value</b></td><td width="200pt"><b>possible values</b></td><td width="315pt"> <b>description</b></td></tr>
+   !! <tr><td>cfl_cr    </td><td>0.9  </td><td>real value</td><td>\copydoc initcosmicrays::cfl_cr</td></tr>
+   !! <tr><td>smallecr  </td><td>0.0  </td><td>real value</td><td>\copydoc initcosmicrays::smallecr</td></tr>
+   !! <tr><td>cr_active </td><td>1.0  </td><td>real value</td><td>\copydoc initcosmicrays::cr_active</td></tr>
+   !! <tr><td>gamma_cr  </td><td>4/3  </td><td>real value</td><td>\copydoc initcosmicrays::gamma_cr</td></tr>
+   !! <tr><td>cr_eff    </td><td>0.1  </td><td>real value</td><td>\copydoc initcosmicrays::cr_eff</td></tr>
+   !! <tr><td>K_cr_paral</td><td>0.0  </td><td>real value</td><td>\copydoc initcosmicrays::K_cr_paral</td></tr>
+   !! <tr><td>K_cr_perp </td><td>0.0  </td><td>real value</td><td>\copydoc initcosmicrays::K_cr_perp</td></tr>
+   !! </table>
+   !! \n \n
+   !<
+   subroutine init_cosmicrays
 
-    implicit none
-    integer            :: ierrh
-    integer            :: nn
-    integer            :: ne
-    character(LEN=100) :: par_file
-    character(LEN=100) :: tmp_log_file
+      use errh,     only: namelist_errh, die
+      use mpisetup, only: proc, ibuff, rbuff, comm, ierr, MPI_DOUBLE_PRECISION, MPI_INTEGER, cwd, buffer_dim
 
+      implicit none
 
-    namelist /COSMIC_RAYS/ cfl_cr, smallecr, cr_active, cr_eff &
-                         , ncrn, gamma_crn, K_crn_paral, K_crn_perp &
-                         , ncre, gamma_cre, K_cre_paral, K_cre_perp
+      integer            :: ierrh
+      integer            :: nn
+      integer            :: ne
+      character(LEN=100) :: par_file
+      character(LEN=100) :: tmp_log_file
 
-    cfl_cr     = 0.9
-    smallecr   = 0.0
-    cr_active  = 1.0
-    cr_eff     = 0.1       !  canonical conversion rate of SN en.-> CR
-                           !  we fix E_SN=10**51 erg
-    ncrn       = 0
-    ncre       = 0
+      namelist /COSMIC_RAYS/ cfl_cr, smallecr, cr_active, cr_eff, &
+           &                 ncrn, gamma_crn, K_crn_paral, K_crn_perp, &
+           &                 ncre, gamma_cre, K_cre_paral, K_cre_perp
 
-    gamma_crn(:)   = 4./3.
-    K_crn_paral(:) = 0.0
-    K_crn_perp(:)  = 0.0
-    gamma_cre(:)   = 4./3.
-    K_cre_paral(:) = 0.0
-    K_cre_perp(:)  = 0.0
+      cfl_cr     = 0.9
+      smallecr   = 0.0
+      cr_active  = 1.0
+      cr_eff     = 0.1       !  canonical conversion rate of SN en.-> CR
+      !  we fix E_SN=10**51 erg
+      ncrn       = 0
+      ncre       = 0
 
-      if(proc .eq. 0) then
+      gamma_crn(:)   = 4./3.
+      K_crn_paral(:) = 0.0
+      K_crn_perp(:)  = 0.0
+      gamma_cre(:)   = 4./3.
+      K_cre_paral(:) = 0.0
+      K_cre_perp(:)  = 0.0
+
+      if (proc == 0) then
          par_file = trim(cwd)//'/problem.par'
          tmp_log_file = trim(cwd)//'/tmp.log'
          open(1,file=par_file)
-            read(unit=1,nml=COSMIC_RAYS,iostat=ierrh)
-            call namelist_errh(ierrh,'COSMIC_RAYS')
+         read(unit=1,nml=COSMIC_RAYS,iostat=ierrh)
+         call namelist_errh(ierrh,'COSMIC_RAYS')
          close(1)
          open(3, file='tmp.log', position='append')
-           write(3,nml=COSMIC_RAYS)
-           write(3,*)
+         write(3,nml=COSMIC_RAYS)
+         write(3,*)
          close(3)
       endif
 
-      nn          = 10
+      if (proc == 0) then
 
-    if(proc .eq. 0) then
+         ibuff(1)   = ncrn
+         ibuff(2)   = ncre
 
-       ibuff(1)   = ncrn
-       ibuff(2)   = ncre
+         rbuff(1)   = cfl_cr
+         rbuff(2)   = smallecr
+         rbuff(3)   = cr_active
+         rbuff(4)   = cr_eff
 
-       rbuff(1)   = cfl_cr
-       rbuff(2)   = smallecr
-       rbuff(3)   = cr_active
-       rbuff(4)   = cr_eff
+         nn         = 4         ! WARNING: this must match the last rbuff() index above
+         ne         = nn + 3 * ncrn
+         if (ne + 3 * ncre > ubound(rbuff, 1)) call die("[initcosmicrays:init_cosmicrays] rbuff size exceeded.")
 
+         if(ncrn > 0) then
+            rbuff(nn+1       :nn+  ncrn) = gamma_crn  (1:ncrn)
+            rbuff(nn+1+  ncrn:nn+2*ncrn) = K_crn_paral(1:ncrn)
+            rbuff(nn+1+2*ncrn:nn+3*ncrn) = K_crn_perp (1:ncrn)
+         endif
 
-       if(ncrn > 0) then
-          rbuff(nn+1:nn+ncrn)          = gamma_crn(1:ncrn)
-          rbuff(nn+1+ncrn:nn+2*ncrn)   = K_crn_paral(1:ncrn)
-          rbuff(nn+1+2*ncrn:nn+3*ncrn) = K_crn_perp(1:ncrn)
-       endif
+         if(ncre > 0) then
+            rbuff(ne+1       :ne+  ncre) = gamma_cre  (1:ncre)
+            rbuff(ne+1+  ncre:ne+2*ncre) = K_cre_paral(1:ncre)
+            rbuff(ne+1+2*ncre:ne+3*ncre) = K_cre_perp (1:ncre)
+         endif
 
-       if(ncre > 0) then
-          rbuff(ne+1:ne+ncre)          = gamma_cre(1:ncre)
-          rbuff(ne+1+ncre:ne+2*ncre)   = K_cre_paral(1:ncre)
-          rbuff(ne+1+2*ncre:ne+3*ncre) = K_cre_perp(1:ncre)
-       endif
+      end if
 
-       call MPI_BCAST(ibuff,    buffer_dim, MPI_INTEGER,          0, comm, ierr)
-       call MPI_BCAST(rbuff,    buffer_dim, MPI_DOUBLE_PRECISION, 0, comm, ierr)
+      call MPI_BCAST(ibuff,    buffer_dim, MPI_INTEGER,          0, comm, ierr)
+      call MPI_BCAST(rbuff,    buffer_dim, MPI_DOUBLE_PRECISION, 0, comm, ierr)
 
-    else
+      if (proc /= 0) then
 
-       call MPI_BCAST(ibuff,    buffer_dim, MPI_INTEGER,          0, comm, ierr)
-       call MPI_BCAST(rbuff,    buffer_dim, MPI_DOUBLE_PRECISION, 0, comm, ierr)
+         ncrn       = ibuff(1)
+         ncre       = ibuff(2)
 
-       ncrn       = ibuff(1)
-       ncre       = ibuff(2)
+         cfl_cr     = rbuff(1)
+         smallecr   = rbuff(2)
+         cr_active  = rbuff(3)
+         cr_eff     = rbuff(4)
 
-       cfl_cr     = rbuff(1)
-       smallecr   = rbuff(2)
-       cr_active  = rbuff(3)
-       cr_eff     = rbuff(4)
+         nn         = 4         ! WARNING: this must match the last rbuff() index above
+         ne         = nn + 3 * ncrn
 
-       ne         = nn + 3*ncrn
+         if(ncrn > 0) then
+            gamma_crn  (1:ncrn) = rbuff(nn+1       :nn+  ncrn)
+            K_crn_paral(1:ncrn) = rbuff(nn+1+  ncrn:nn+2*ncrn)
+            K_crn_perp (1:ncrn) = rbuff(nn+1+2*ncrn:nn+3*ncrn)
+         endif
 
-       if(ncrn > 0) then
-          gamma_crn(1:ncrn)  = rbuff(nn+1:nn+ncrn)
-          K_crn_paral(1:ncrn)= rbuff(nn+1+ncrn:nn+2*ncrn)
-          K_crn_perp(1:ncrn) = rbuff(nn+1+2*ncrn:nn+3*ncrn)
-       endif
+         if(ncre > 0) then
+            gamma_cre  (1:ncre) = rbuff(ne+1       :ne+  ncre)
+            K_cre_paral(1:ncre) = rbuff(ne+1+  ncre:ne+2*ncre)
+            K_cre_perp (1:ncre) = rbuff(ne+1+2*ncre:ne+3*ncre)
+         endif
 
-       if(ncre > 0) then
-          gamma_cre(1:ncre)  = rbuff(ne+1:ne+ncre)
-          K_cre_paral(1:ncre)= rbuff(ne+1+ncre:ne+2*ncre)
-          K_cre_perp(1:ncre) = rbuff(ne+1+2*ncre:ne+3*ncre)
-       endif
+      endif
 
-    endif
+      if (ncrn > ncr_max) call die("[initcosmicrays:init_cosmicrays] ncrn > ncr_max")
+      if (ncre > ncr_max) call die("[initcosmicrays:init_cosmicrays] ncre > ncr_max")
 
-!    if(ncrn > ncr_max) call die("ncrn > ncr_max")
-!    if(ncre > ncr_max) call die("ncre > ncr_max")
+      allocate(gamma_crs(ncrn+ncre),K_crs_paral(ncrn+ncre),K_crs_perp(ncrn+ncre))
 
-    allocate(gamma_crs(ncrn+ncre),K_crs_paral(ncrn+ncre),K_crs_perp(ncrn+ncre))
+      if(ncrn > 0) then
+         gamma_crs  (1:ncrn) = gamma_crn  (1:ncrn)
+         K_crs_paral(1:ncrn) = K_crn_paral(1:ncrn)
+         K_crs_perp (1:ncrn) = K_crn_perp (1:ncrn)
+      endif
 
-    if(ncrn > 0) then
-       gamma_crs(1:ncrn)   = gamma_crn(1:ncrn)
-       K_crs_paral(1:ncrn) = K_crn_paral(1:ncrn)
-       K_crs_perp(1:ncrn)  = K_crn_perp(1:ncrn)
-    endif
-
-    if(ncre > 0) then
-       gamma_crs(ncrn+1:ncrn+ncre)   = gamma_cre(1:ncre)
-       K_crs_paral(ncrn+1:ncrn+ncre) = K_crs_paral(1:ncre)
-       K_crs_perp(ncrn+1:ncrn+ncre)  = K_crs_perp(1:ncre)
-    endif
+      if(ncre > 0) then
+         gamma_crs  (ncrn+1:ncrn+ncre) = gamma_cre  (1:ncre)
+         K_crs_paral(ncrn+1:ncrn+ncre) = K_crs_paral(1:ncre)
+         K_crs_perp (ncrn+1:ncrn+ncre) = K_crs_perp (1:ncre)
+      endif
 
 #ifdef NEW_HDF5
-    call cr_add_hdf5(ncrn+ncre)
+      call cr_add_hdf5(ncrn+ncre)
 #endif /* NEW_HDF5 */
 
- end subroutine init_cosmicrays
+   end subroutine init_cosmicrays
 
- subroutine cosmicray_species
+   subroutine cosmicray_species
 
- end subroutine cosmicray_species
+   end subroutine cosmicray_species
 
- subroutine cosmicray_index(nvar, nvar_crn, nvar_cre, nvar_crs)
+   subroutine cosmicray_index(nvar, nvar_crn, nvar_cre, nvar_crs)
 
       implicit none
+
       integer :: nvar
       integer :: nvar_crn
       integer :: nvar_cre
@@ -240,20 +243,24 @@ module initcosmicrays
       enddo
       nvar = nvar + nvar_cre
 
-  end subroutine cosmicray_index
+   end subroutine cosmicray_index
 
 #ifdef NEW_HDF5
    subroutine cr_add_hdf5(nvar_crs)
-      use arrays, only : u
-      use grid, only : nxb,nyb,nzb,is,ie,js,je,ks,ke
+
+      use arrays,    only : u
+      use grid,      only : nxb, nyb, nzb, is, ie, js, je, ks, ke
       use list_hdf5, only : add_lhdf5, lhdf5_info
+
       implicit none
+
       integer, intent(in) :: nvar_crs
+
       type(lhdf5_info) :: item
-      integer :: i
+      integer          :: i
 
       item%p    => get_cr
-      item%sz   = (/nxb,nyb,nzb/)
+      item%sz   = (/nxb, nyb, nzb/)
       item%ind  = (/is, ie, js, je, ks, ke/)
 
       do i = 1, nvar_crs
@@ -264,20 +271,22 @@ module initcosmicrays
 
       enddo
 
-      contains
-         function get_cr(i,sz,opti) result (outtab)
-            implicit none
-            integer, dimension(6), intent(in)    :: i
-            integer, dimension(3), intent(in)    :: sz
-            integer, dimension(5), intent(in)    :: opti
-            real, dimension(sz(1),sz(2),sz(3))   :: outtab
-            outtab(:,:,:) =  &
-               u(opti(1),i(1):i(2),i(3):i(4),i(5):i(6))
-         end function get_cr
+   contains
+
+      function get_cr(i,sz,opti) result (outtab)
+
+         implicit none
+
+         integer, dimension(6), intent(in)    :: i
+         integer, dimension(3), intent(in)    :: sz
+         integer, dimension(5), intent(in)    :: opti
+         real, dimension(sz(1),sz(2),sz(3))   :: outtab
+
+         outtab(:,:,:) = u(opti(1),i(1):i(2),i(3):i(4),i(5):i(6))
+
+      end function get_cr
 
    end subroutine cr_add_hdf5
 #endif /* NEW_HDF5 */
 
-
 end module initcosmicrays
-
