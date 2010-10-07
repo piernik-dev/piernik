a20fe206708d5e3a293560d6d799f591681fa9b7
Dominik Wóltański <minikwolt@astri.umk.pl>
2010-10-07 12:54:48 +0000

    trunk: [interactions] supplementary to revs.[2570]&[2571]

--- src/fluids/interactions/interactions.F90
+++ src/fluids/interactions/interactions.F90
@@ -62,13 +62,14 @@ module interactions
 !! \n \n
 !<
    subroutine init_interactions
-      use mpisetup,     only : proc, rbuff, MPI_DOUBLE_PRECISION, buffer_dim, ierr, comm
-      use fluidindex,   only : nvar
-      use dataio_public, only: par_file, cwd
-      use func,        only : compare_namelist
+      use mpisetup,      only: proc, rbuff, MPI_DOUBLE_PRECISION, buffer_dim, ierr, comm
+      use fluidindex,    only: nvar
+      use dataio_public, only: par_file, cwd, ierrh
+      use errh,          only: namelist_errh
+      use func,          only: compare_namelist
 
 #ifdef DUST
-      use initdust,     only : dragc_gas_dust
+      use initdust,      only: dragc_gas_dust
 #endif /* DUST */
 
       implicit none
