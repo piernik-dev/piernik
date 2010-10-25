e79d8fde019906d12f446c49866bfbd7b87dad4d
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2010-10-25 08:05:24 +0000

    trunk: [fluxneutral] cosmetics, simplify dependencies.

--- src/fluids/neutral/fluxneutral.F90
+++ src/fluids/neutral/fluxneutral.F90
@@ -64,94 +64,89 @@
 !<
 !*/
 module fluxneutral
-  implicit none
+   implicit none
+   private
+   public :: flux_neu
 
-  contains
+contains
 !==========================================================================================
 
-  subroutine flux_neu(fluxn,cfrn,uun,n)
+   subroutine flux_neu(fluxn,cfrn,uun,n)
 
-    use constants,       only: small
-    use fluidindex,      only: idn, imx, imy, imz, ien, nvar
-    use initneutral,     only: gamma_neu, cs_iso_neu2
-    use mpisetup,        only: cfr_smooth
-    use timestep,        only: c_all
+      use constants,       only: small
+      use fluidindex,      only: idn, imx, imy, imz, ien, nvar
+      use mpisetup,        only: cfr_smooth, smallp
+      use timestep,        only: c_all
 
-    implicit none
-    integer,intent(in) :: n                     !< number of cells in the current sweep
+      implicit none
+      integer, intent(in)                          :: n         !< number of cells in the current sweep
+      real, dimension(nvar%neu%all,n), intent(out) :: fluxn     !< flux of neutral fluid
+      real, dimension(nvar%neu%all,n), intent(in)  :: uun       !< part of u for neutral fluid
+      real, dimension(nvar%neu%all,n), intent(out) :: cfrn      !< freezing speed for neutral fluid
 
-! locals
-    real, dimension(nvar%neu%all,n):: fluxn     !< flux of neutral fluid
-    real, dimension(nvar%neu%all,n):: uun       !< part of u for neutral fluid
-    real, dimension(nvar%neu%all,n):: cfrn      !< freezing speed for neutral fluid
-    real, dimension(n) :: vx                    !< velocity of neutral fluid for current sweep
-    real, dimension(n) :: p                     !< pressure of neutral fluid
+      ! locals
+      real, dimension(n) :: vx        !< velocity of neutral fluid for current sweep
+      real, dimension(n) :: p         !< pressure of neutral fluid
 #ifdef LOCAL_FR_SPEED
-    real :: minvx                               !<
-    real :: maxvx                               !<
-    real :: amp                                 !<
-!    real, dimension(n) :: c_fr                  !< temporary array for freezing speed
-!    integer :: i
+      real               :: minvx     !<
+      real               :: maxvx     !<
+      real               :: amp       !<
 #endif /* LOCAL_FR_SPEED */
 
-    fluxn   = 0.0
-    cfrn    = 0.0
-    vx      = 0.0
-    p       = 0.0
+      fluxn   = 0.0; cfrn    = 0.0; vx      = 0.0; p       = 0.0
 
-    vx(RNG)=uun(imx,RNG)/uun(idn,RNG)
+      vx(RNG) = uun(imx,RNG)/uun(idn,RNG)
 #ifdef ISO
-    p(RNG) = cs_iso_neu2*uun(idn,RNG)
+      p(RNG)  = nvar%neu%cs2*uun(idn,RNG)
 #else /* ISO */
-    p(RNG)=(uun(ien,RNG)  &
+      p(RNG)  = (uun(ien,RNG)  &
       - 0.5*( uun(imx,RNG)**2 + uun(imy,RNG)**2 + uun(imz,RNG)**2 ) &
-          / uun(idn,RNG))*(gamma_neu-1.0)
-    !!!! BEWARE: constant smallp !!!!
-    p(RNG) = max(p(RNG),1.e-5)
+          / uun(idn,RNG))*(nvar%neu%gam-1.0)
+      p(RNG)  = max(p(RNG),smallp)
 #endif /* ISO */
 
-    fluxn(idn,RNG)=uun(imx,RNG)
-    fluxn(imx,RNG)=uun(imx,RNG)*vx(RNG)+p(RNG)
-    fluxn(imy,RNG)=uun(imy,RNG)*vx(RNG)
-    fluxn(imz,RNG)=uun(imz,RNG)*vx(RNG)
+      fluxn(idn,RNG)=uun(imx,RNG)
+      fluxn(imx,RNG)=uun(imx,RNG)*vx(RNG)+p(RNG)
+      fluxn(imy,RNG)=uun(imy,RNG)*vx(RNG)
+      fluxn(imz,RNG)=uun(imz,RNG)*vx(RNG)
 #ifndef ISO
-    fluxn(ien,RNG)=(uun(ien,RNG)+p(RNG))*vx(RNG)
+      fluxn(ien,RNG)=(uun(ien,RNG)+p(RNG))*vx(RNG)
 #endif /* !ISO */
 
 #ifdef LOCAL_FR_SPEED
 
-!       The freezing speed is now computed locally (in each cell)
-!       as in Trac & Pen (2003). This ensures much sharper shocks,
-!       but sometimes may lead to numerical instabilities
-    minvx = minval(vx(RNG))
-    maxvx = maxval(vx(RNG))
-    amp   = 0.5*(maxvx-minvx)
-!    c_fr  = 0.0
+      !       The freezing speed is now computed locally (in each cell)
+      !       as in Trac & Pen (2003). This ensures much sharper shocks,
+      !       but sometimes may lead to numerical instabilities
+      minvx = minval(vx(RNG))
+      maxvx = maxval(vx(RNG))
+      amp   = 0.5*(maxvx-minvx)
+      !    c_fr  = 0.0
 #ifdef ISO
-    cfrn(1,RNG) = sqrt(vx(RNG)**2+cfr_smooth*amp) + max(sqrt( abs(          p(RNG))/uun(idn,RNG)),small)
+      cfrn(1,RNG) = sqrt(vx(RNG)**2+cfr_smooth*amp) + max(sqrt( abs(             p(RNG))/uun(idn,RNG)),small)
 #else /* ISO */
-    cfrn(1,RNG) = sqrt(vx(RNG)**2+cfr_smooth*amp) + max(sqrt( abs(gamma_neu*p(RNG))/uun(idn,RNG)),small)
+      cfrn(1,RNG) = sqrt(vx(RNG)**2+cfr_smooth*amp) + max(sqrt( abs(nvar%neu%gam*p(RNG))/uun(idn,RNG)),small)
 #endif /* ISO */
-!BEWARE: that is the cause of fast decreasing of timestep in galactic disk problem
-!TODO: find why is it so
-!if such a treatment is OK then should be applied also in both cases of neutral and ionized gas
-!    do i = 2,n-1
-!       cfrn(1,i) = maxval( [c_fr(i-1), c_fr(i), c_fr(i+1)] )
-!    enddo
+      !BEWARE: that is the cause of fast decreasing of timestep in galactic disk problem
+      !TODO: find why is it so
+      !if such a treatment is OK then should be applied also in both cases of neutral and ionized gas
+      !    do i = 2,n-1
+      !       cfrn(1,i) = maxval( [c_fr(i-1), c_fr(i), c_fr(i+1)] )
+      !    enddo
 
-    cfrn(1,1) = cfrn(1,2)
-    cfrn(1,n) = cfrn(1,n-1)
-    cfrn = spread(cfrn(1,:),1,nvar%neu%all)
+      cfrn(1,1) = cfrn(1,2)
+      cfrn(1,n) = cfrn(1,n-1)
+      cfrn = spread(cfrn(1,:),1,nvar%neu%all)
 #endif /* LOCAL_FR_SPEED */
 
 #ifdef GLOBAL_FR_SPEED
-!       The freezing speed is now computed globally
-!       (c=const for the whole domain) in sobroutine 'timestep'
+      !       The freezing speed is now computed globally
+      !       (c=const for the whole domain) in sobroutine 'timestep'
 
-!    cfrn(:,:) = c_neu       ! check which c_xxx is better
-    cfrn(:,:) = c_all
+      !    cfrn(:,:) = nvar%neu%snap%c   ! check which c_xxx is better
+      cfrn(:,:) = c_all
 #endif /* GLOBAL_FR_SPEED */
 
-  end subroutine flux_neu
-
+   end subroutine flux_neu
+      
 end module fluxneutral
