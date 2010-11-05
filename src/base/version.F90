6194ba46774e0f1a32f22d440bf4ed5a1a582b4a
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2010-11-05 19:02:44 +0000

    trunk: Revert r3089 as it renders trunk unusable

---
+++ src/base/version.F90
@@ -0,0 +1,9 @@
+! $Id$
+!
+! This file is just a template to be completed during make process
+!
+! Do not try to fix it to make it a valid Fortran source file!
+!
+module version
+   implicit none
+   public         ! QA_WARN nothing to hide here
