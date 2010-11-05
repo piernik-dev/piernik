cf34cf3738d07c40f1ec4531e29dd2c75f922da0
Dominik Wóltański <minikwolt@astri.umk.pl>
2010-11-05 12:51:12 +0000

    trunk: [piernik.h] further dependencies between precompiler directives

--- src/base/piernik.h
+++ src/base/piernik.h
@@ -2,5 +2,13 @@
 #include "piernik.def"
 
 #if defined(MULTIGRID) || defined(POISSON_FFT)
+#define SELF_GRAV
+#endif
+
+#if defined(SELF_GRAV) || defined(VARIABLE_USER_GP)
 #define VARIABLE_GP
+#endif
+
+#if defined (GRAV_PTMASSPURE) || defined (GRAV_PTMASS) || defined (GRAV_PTFLAT) || defined (GRAV_PTMASSSTIFF)
+#define GRAV_PTMTYPE
 #endif
