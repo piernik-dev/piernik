9d63e99e8461890568b2ede19b5face2e4ce265a
Artur Gawryszczak <gawrysz@gmail.com>
2010-11-05 16:02:11 +0000

    trunk: fixed issues with parallel creation and compilation of version.F90

--- src/base/version.F90
+++
@@ -1,9 +0,0 @@
-! $Id$
-!
-! This file is just a template to be completed during make process
-!
-! Do not try to fix it to make it a valid Fortran source file!
-!
-module version
-   implicit none
-   public         ! QA_WARN nothing to hide here
