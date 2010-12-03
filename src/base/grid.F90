13d39fb1dfd94045a57f919cfe65f3a090b5fbb2
Dominik Wóltański <minikwolt@astri.umk.pl>
2010-12-03 13:56:35 +0000

    trunk: [grid] [x,y,z][min,max]b also necessary outside the module

--- src/base/grid.F90
+++ src/base/grid.F90
@@ -42,7 +42,8 @@ module grid
    private
    public :: &
         & Lx, Ly, Lz, cleanup_grid, dl, dvol, dx, dxmn, dy, dz, has_dir, idl, idx, idy, idz, ie, init_grid, is, je, js, ke, ks, maxxyz, nb, nx, &
-        & nxb, nxt, ny, nyb, nz, nzb, nzt, total_ncells, x, xdim, xl, xmax, xmin, xr, y, ydim, yl, ymax, ymin, yr, z, zdim, zl, zmax, zmin, zr
+        & nxb, nxt, ny, nyb, nyt, nz, nzb, nzt, total_ncells, x, xdim, xl, xmax, xmaxb, xmin, xminb, xr, y, ydim, yl, ymax, ymaxb, ymin, yminb, &
+        & yr, z, zdim, zl, zmax, zmaxb, zmin, zminb, zr
 
    real    :: dx                             !< length of the %grid cell in x-direction
    real    :: dy                             !< length of the %grid cell in y-direction
