7b8d12473ef443eb52414d30d1c782b9cbcf4486
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2011-03-07 19:25:55 +0000

    trunk: [rtvd] add implicit solver for dust-gas drag, HIGHLY EXPERIMENTAL

--- src/scheme/rtvd_split/rtvd.F90
+++ src/scheme/rtvd_split/rtvd.F90
@@ -304,6 +304,10 @@ contains
       real, dimension(n)             :: cs_iso2            !< square of local isothermal sound speed (optional for ISO_LOCAL)
 #endif /* ISO_LOCAL */
 
+#ifdef BALSARA
+      real, dimension(flind%fluids,n) :: vprim, psi
+      real, dimension(n) :: delta, drag
+#endif /* BALSARA */
 #ifdef COSM_RAYS
       integer                       :: icr
 #ifdef COSM_RAYS_SOURCES
@@ -411,8 +415,42 @@ contains
          u1(iarr_all_mx,:) = u1(iarr_all_mx,:) + rk2coef(integration_order,istep)*geosrc(:,:)*dt
 
          acc = 0.0
-
+#ifndef BALSARA
          acc = acc + fluid_interactions(dens, vx)
+#else /* !BALSARA */
+         ! BEWARE: this bit assumes that we have 2 fluids && u1 == u0 - \grad F
+         ! \todo:
+         !  1) Calculate drag
+         !  2) half-time step should be \le \frac{1}{2}\frac{c_s}{drag * \rho\prim_? |v'_d - v'_g|}
+         !  3) what if not isothermal?
+         !  4) remove hardcoded integers
+
+         drag(:) = 10.0 ! BEWARE: CHEAT!
+
+         drag(:) = drag(:)*dt*0.5
+
+         delta(:) = 1.0 + drag(:) * (u1(iarr_all_dn(1),:) + u1(iarr_all_dn(2),:))
+         delta(:) = 1./delta(:)
+
+         if (istep == 1) then
+            vprim(1,:) =  delta(:)*( (1./u1(iarr_all_dn(1),:) + drag(:))*u1(iarr_all_mx(1),:) + drag(:)*u1(iarr_all_mx(2),:) )
+            vprim(2,:) =  delta(:)*( (1./u1(iarr_all_dn(2),:) + drag(:))*u1(iarr_all_mx(2),:) + drag(:)*u1(iarr_all_mx(1),:) )
+         else
+!            psi(1,:) = ( u1(iarr_all_mx(1),:) - drag(:)*(u0(iarr_all_dn(2),:)*u0(iarr_all_mx(1),:) - u0(iarr_all_dn(1),:)*u0(iarr_all_mx(2),:)) )
+!            psi(2,:) = ( u1(iarr_all_mx(2),:) - drag(:)*(u0(iarr_all_dn(1),:)*u0(iarr_all_mx(2),:) - u0(iarr_all_dn(2),:)*u0(iarr_all_mx(1),:)) )
+
+            vprim(2,:) =  delta(:)*(  &
+                 drag(:)*( u0(iarr_all_dn(2),:)/u1(iarr_all_dn(2),:)*u0(iarr_all_mx(1),:) - u0(iarr_all_dn(1),:)/u1(iarr_all_dn(2),:)*u0(iarr_all_mx(2),:) + u1(iarr_all_mx(1),:) ) + &
+                 u1(iarr_all_mx(2),:) * ( 1./u1(iarr_all_dn(2),:) + drag(:) ) &
+               )
+            vprim(1,:) =  delta(:)*(  &
+                 drag(:)*( u0(iarr_all_dn(1),:)/u1(iarr_all_dn(1),:)*u0(iarr_all_mx(2),:) - u0(iarr_all_dn(2),:)/u1(iarr_all_dn(1),:)*u0(iarr_all_mx(1),:) + u1(iarr_all_mx(2),:) ) + &
+                 u1(iarr_all_mx(1),:) * ( 1./u1(iarr_all_dn(1),:) + drag(:) ) &
+               )
+            !vprim(2,:) =  delta(:)*( (1./u1(iarr_all_dn(2),:) + drag(:))*psi(2,:) + drag(:)*psi(1,:) )
+         endif
+         u1(iarr_all_mx,:) = u1(iarr_all_dn,:) * vprim(:,:)
+#endif /* BALSARA */
 #ifdef SHEAR
          acc = acc + shear_acc(sweep,u)
 #endif /* SHEAR */
