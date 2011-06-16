1a4cd015c1adbac2c8bc6845b82fd7fc64dd8a54
Dominik Wóltański <minikwolt@astri.umk.pl>
2011-06-16 11:15:55 +0000

    trunk: [doc/initproblem] warning & annotation related to pointers assignment

--- doc/general/initproblem.F90
+++ doc/general/initproblem.F90
@@ -5,16 +5,20 @@
 !!
 !! \par problem_pointers
 !!
-!! This routine iscalled very early, just after init_mpi and is supposed to set up procedure pointers, if necessary.
+!! This routine is called very early, just after init_mpi and is supposed to set up procedure pointers, if necessary.
 !! Such approach allows implementation of problem-specific hacks also during the initialization of the Piernik code.
 !! In extreme cases it is possible to change behavior of the Piernik code quite radically, without precompiler directives.
 !!
 !! It assumes that all procedure pointers are initialized to Null().
 !!
+!! \warning Pointers assignment may be dependent on parameters from problem.par file (unrecommended). In such a case they must
+!! be assigned not before read_problem_par routine is called.
+!!
 !! \par read_problem_par
 !!
 !! This routine reads the PROBLEM_CONTROL namelist by master process and communicates read parameters to other processes.
 !! It can also calculate things that might be needed by the problem, except the initial conditions.
+!! \annotate Pointers assignment dependent on parameters from problem.par file should be placed at the end of this routine.
 !!
 !! This routine is called before it is decided whether to read restart file or start from scratch.
 !!
