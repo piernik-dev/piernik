ade9d49fe2e6c2cd9918b74abce549c02f251ad8
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2011-08-17 07:49:28 +0000

    trunk: [plot.py] plot 1d data

--- python/plot.py
+++ python/plot.py
@@ -23,22 +23,36 @@ def read_hdf5(options,filename):
 def get_frame(filename,options):
    tab, attrs = read_hdf5(options,filename)
    shape = list(tab.shape)
+   i1 = options.i1
+   i2 = options.i2
 
    if options.plane == "xy":
       p = 0
       extent = [attrs.xmin[0], attrs.xmax[0], attrs.ymin[0], attrs.ymax[0]]
-      s = s_[:,:,options.cell]
+      s = s_[:,:,i1]
    elif options.plane == "xz":
       p = 1
       extent = [attrs.xmin[0], attrs.xmax[0], attrs.zmin[0], attrs.zmax[0]]
-      s = s_[:,options.cell,:]
-   else:
+      s = s_[:,i1,:]
+   elif options.plane == "yz":
       p = 2
       extent = [attrs.ymin[0], attrs.ymax[0], attrs.zmin[0], attrs.zmax[0]]
-      s = s_[:,:,options.cell]
+      s = s_[:,:,i1]
+   elif options.plane == "x":
+      p = 0
+      extent = [attrs.xmin[0], attrs.xmax[0]]
+      s = s_[i2,i1,:]
+   elif options.plane == "y":
+      p = 1
+      extent = [attrs.ymin[0], attrs.ymax[0]]
+      s = s_[i1,:,i2]
+   elif options.plane == "z":
+      p = 2
+      extent = [attrs.zmin[0], attrs.zmax[0]]
+      s = s_[:,i2,i1]
 
-   if options.cell >= shape[p] or options.cell < 0:
-      print("You gotta be kiddin' me... %d is _not_ in [0,%d]" % (options.cell, shape[p]-1))
+   if i1 >= shape[p] or i1 < 0:
+      print("You gotta be kiddin' me... %d is _not_ in [0,%d]" % (i1, shape[p]-1))
 
    arr = tab[s]
    if options.dolog:
@@ -58,13 +72,15 @@ parser.add_option("-d", "--dataset", dest="dset", default="",
       help="datasets, use comma-separated list")
 parser.add_option("-p", "--plane", dest="plane", default="xy",
       help="plane, possible values {xy,yz,xz}")
-parser.add_option("-c", "--cell", dest="cell", default=0, type="int",
-      help="plane, possible values {xy,yz,xz}")
+parser.add_option("-c", "--cell", "--i1", dest="i1", default=0, type="int",
+      help="")
+parser.add_option("--i2", dest="i2", default=0, type="int",
+      help="")
 parser.add_option("--log", dest="dolog", default=False, action="store_true",
       help="use logarithmic scal")
 (options, args) = parser.parse_args(sys.argv[1:])
 
-if not options.plane in ["xy","yz","xz"]:
+if not options.plane in ["xy","yz","xz","x","y","z"]:
    print '\033[91m' + "wrong plane: " + '\033[0m' + options.plane
    exit(-1)
 
@@ -72,18 +88,24 @@ if len(args) > 1:
    ion()
 
 arr, title_str, extent = get_frame(args[0], options)
-plt = imshow(arr,extent=extent,interpolation='nearest')
+if len(extent) == 2:
+   plt = plot(linspace(extent[0],extent[1],num=len(arr)), arr)
+else:
+   plt = imshow(arr,extent=extent,interpolation='nearest')
+   col=colorbar()
 title(title_str)
-col=colorbar()
 draw()
 show()
 
 if len(args) > 1:
    for arg in args[1:]:
       arr, title_str, extent = get_frame(arg,options)
-      plt = imshow(arr,extent=extent,interpolation='nearest')
+      if len(extent) == 2:
+         plt = plot(linspace(extent[0],extent[1],num=len(arr)), arr)
+      else:
+         plt = imshow(arr,extent=extent,interpolation='nearest')
+         col.update_bruteforce(plt)
       title(title_str)
-      col.update_bruteforce(plt)
       draw()
    print "Press ENTER to exit"
    e = sys.stdin.readline()  # wait for ENTER due to ion()
