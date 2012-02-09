9e6a7db71d9059d946fb5d41b0b5eb5ae68c938f
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2012-02-09 12:36:08 +0000

    trunk: selectively merge with AMR

--- bin/qa.py
+++ bin/qa.py
@@ -9,7 +9,7 @@ debug = False
 typ1 = np.dtype([('name','a50'),('beg','i'),('end','i'),('type','a4')])
 
 test_for_routines  = re.compile('''
-      ^\s{0,12}(|end|pure|elemental)\s           # starts with spaces or spaces and one of { 'end', 'pure', ... }
+      ^\s{0,12}(|end|pure|elemental|recursive)\s # starts with spaces or spaces and one of { 'end', 'pure', ... }
       (|real|logical|integer)(|\s)               # if function it can have a type
       (subroutine|function|type(,|\s))           # next goes subroutine or function or type
    ''',re.VERBOSE)
