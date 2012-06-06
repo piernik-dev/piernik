131283e2f65e7857abccbdd10262a1ee501873ab
Dominik Wóltański <minikwolt@astri.umk.pl>
2012-06-06 09:11:42 +0000

    trunk: [doc] update precompiler flags info

--- doc/general/precompiler.F90
+++ doc/general/precompiler.F90
@@ -9,8 +9,9 @@
 !! <li>instruct precompiler on which parts of selected routines should be retained for compilation</li>
 !! </ul>
 !!
-!! \anchor isothermal
-!! \b \#define \b "ISO" - the choice between adiabatic and isothermal fluids (if defined then isothermal fluid is set, otherwise adiabatic)
+!! @n @n
+!!
+!! \b FLUID \b COMPONENTS:
 !!
 !! \b \#define \b "IONIZED" - to include parts of the code related to ionized fluid
 !!
@@ -20,11 +21,27 @@
 !!
 !! \b \#define \b "COSM_RAYS" - to include parts of the code related to cosmic ray component
 !!
+!! \b \#define \b "COSM_RAYS_SOURCES" - to take into account several different species of cosmic rays particles
+!!
+!! \b \#define \b "TRACER" - to include parts of the code related to tracer
+!! @n @n
+!!
+!! \b PHYSICS \b INCLUDED \b (i.e. \b FIELDS, \b INTERACTIONS):
+!!
+!! \anchor isothermal
+!! \b \#define \b "ISO" - the choice between adiabatic and isothermal fluids (if defined then isothermal fluid is set, otherwise adiabatic)
+!!
+!! \b \#define \b "ISO_LOCAL" - to choose isothermal fluids with locally specified soundspeed and temperature
+!!
 !! \b \#define \b "MAGNETIC" - to include parts of the code related to magnetic field (otherwise magnetic field is switched off even if ionized fluid is simulated)
+!!
+!! \b \#define \b "RESISTIVE" - to include resistive dissipation of magnetic field
 !!
 !! \b \#define \b "FLUID_INTERACTIONS" - to take into account interactions between different fluids (eg. drag force)
 !!
-!! \b \#define \b "RESIST" - to include resistive dissipation of magnetic field
+!! \b \#define \b "FLUID_INTERACTIONS_DW" - older type of fluids interaction scheme
+!!
+!! \b \#define \b "BALSARA"
 !!
 !! \b \#define \b "GRAV" - to include gravitational forces
 !!
@@ -37,10 +54,65 @@
 !! where \f$r_{gc}\f$ is galactocentric radius and \f$r_{gcSun}\f$ is the galactocentric radius of Sun.
 !! </td></tr></table>
 !! \n \n
+!!
+!! \b \#define \b "SELF_GRAV"
+!!
+!! \b \#define \b "VARIABLE_GP"
+!!
+!! \b \#define \b "CORIOLIS"
+!!
+!! \b \#define \b "SN_SRC"
+!! @n @n
+!!
+!! \b NUMERICAL \b SCHEMES \b AND \b SOLUTIONS:
+!!
+!! \b \#define \b "RTVD"  - to choose Relaxing TVD scheme (then HLLC must be undefined)
+!!
+!! \b \#define \b "HLLC" - to choose HLLC scheme (without magnetic fields; RTVD must be undefined)
+!!
+!! \b \#define \b "LOCAL_FR_SPEED"
+!!
+!! \b \#define \b "GLOBAL_FR_SPEED"
+!!
 !! \b \#define \b "MULTIGRID" - to include self–gravity (multigrid solver, uses FFT when possible, recommended)
 !!
 !! \b \#define \b "POISSON_FFT" - to include self–gravity (pure FFT solver)
 !!
+!! \b \#define \b "FFTW"
+!!
 !! \b \#define \b "SHEAR" - to include Coriolis and tidal forces in gas equation of motion and use  <a href="http://cdsads.u-strasbg.fr/abs/1995ApJ...440..742H">Hawley, Gammie and Balbus (1995)</a> type approach for shearing BCS
+!!
+!! \b \#define \b "SHEAR_BND"
+!!
+!! \b \#define \b "NEW_HYDROSTATIC"
+!!
+!! \b \#define \b "ZERO_BND_EMF"
 !! @n @n
+!!
+!! \b OTHERS:
+!!
+!! \b \#define \b "DEBUG"
+!!
+!! \b \#define \b "VERBOSE"
+!!
+!! \b \#define \b "PGPLOT"
+!!
+!! \b \#define \b "JEANS_PROBLEM"
+!!
+!! \b \#define \b "INDEPENDENT_ATOUTB"
+!!
+!! \b \#define \b "PERFMON"
+!!
+!! \b \#define \b "PIERNIK_OPENCL"
+!!
+!! \b \#define \b "USER_RULES"
+!!
+!! \b \#define \b "__IFORT__"
+!!
+!! \b \#define \b "__INTEL_COMPILER"
+!!
+!! \b \#define \b "__PGI"
+!!
 !<
+
+
