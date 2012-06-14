a69a46f1efc1eac6d70766af73c412558b26b696
Dominik Wóltański <minikwolt@astri.umk.pl>
2012-06-14 08:55:10 +0000

    trunk: [doc] initproblem description reorganized in different order

--- doc/general/initproblem.F90
+++ doc/general/initproblem.F90
@@ -3,6 +3,20 @@
 !!
 !! Each problem should provide a initproblem.F90 file with module initproblem and three subroutines:
 !!
+!! \par init_prob
+!!
+!! This routine fills the grid with initial conditions, according to the parameters read in read_problem_par.
+!!
+!! It is not called when the code starts from the restart file.
+!!
+!! \par read_problem_par
+!!
+!! This routine reads the PROBLEM_CONTROL namelist by master process and communicates read parameters to other processes.
+!! It can also calculate things that might be needed by the problem, except the initial conditions.
+!! \attention Pointers assignment dependent on parameters from problem.par file should be placed at the end of this routine.
+!!
+!! This routine is called before it is decided whether to read restart file or start from scratch.
+!!
 !! \par problem_pointers
 !!
 !! This routine is called very early, just after init_mpi and is supposed to set up procedure pointers, if necessary.
@@ -38,18 +52,4 @@
 !!
 !! \warning Pointers assignment may be dependent on parameters from problem.par file (unrecommended). In such a case they must
 !! be assigned not before read_problem_par routine is called.
-!!
-!! \par read_problem_par
-!!
-!! This routine reads the PROBLEM_CONTROL namelist by master process and communicates read parameters to other processes.
-!! It can also calculate things that might be needed by the problem, except the initial conditions.
-!! \attention Pointers assignment dependent on parameters from problem.par file should be placed at the end of this routine.
-!!
-!! This routine is called before it is decided whether to read restart file or start from scratch.
-!!
-!! \par init_prob
-!!
-!! This routine fills the grid with initial conditions, according to the parameters read in read_problem_par.
-!!
-!! It is not called when the code starts from the restart file.
 !<
