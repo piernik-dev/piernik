80a54205e73056dbd3f58965754a30c4ea8a376a
Michał Hanasz <mhanasz@astri.umk.pl>
2012-07-06 14:51:08 +0000

    trunk: Reverting [6176]

--- src/fluids/cosmicrays/crhelpers.F90
+++ src/fluids/cosmicrays/crhelpers.F90
@@ -60,21 +60,12 @@ contains
 !>
 !! \brief Compute divergence of velocity
 !!
-!! The divergence of velocity computed with the aid of 6-th order finite
-!! differencing based on the Lagendre Polynomial interpolation.
-!!
-!! \details This routine requires a three layers of valid guardcells uin cg%u arrays
+!! \details This routine requires a single layer of valid guardcells uin cg%u arrays
 !<
-
-! Should be moved in future to a dedicated module containing general purpose interpolation
-! and derivation routines, and placed together with the other useful scheems described
-! in particular in http://turbulence.pha.jhu.edu/Database-functions.pdf
-!
-
 
    subroutine div_v(ifluid, cg)
 
-      use constants,   only: xdim, ydim, zdim, big
+      use constants,   only: xdim, ydim, zdim, half
       use dataio_pub,  only: die
       use domain,      only: dom
       use fluidindex,  only: iarr_all_dn, iarr_all_mx, iarr_all_my, iarr_all_mz
@@ -90,9 +81,6 @@ contains
       integer                         :: i, j, k
       integer                         :: idnf, imxf, imyf, imzf
       real, dimension(:,:,:), pointer :: divvel
-      integer                         :: nx, ny, nz
-      real                            :: idx, idy, idz
-      real, parameter                 :: p3_4 = 3./4., m3_20 = -3./20., p1_60 = 1./60.
 
       divvel => cg%q(qna%ind(divv_n))%arr
       if (.not. associated(divvel)) call die("[crhelpers:div_v] cannot get divvel")
@@ -104,9 +92,6 @@ contains
       imxf = iarr_all_mx(ifluid)
       imyf = iarr_all_my(ifluid)
       imzf = iarr_all_mz(ifluid)
-      nx   = cg%n_(xdim)  ; idx = cg%idx
-      ny   = cg%n_(ydim)  ; idy = cg%idy
-      nz   = cg%n_(zdim)  ; idy = cg%idy
 
       divvel(:,:,:) = 0.0
 
@@ -114,36 +99,30 @@ contains
          do k = 1, cg%n_(zdim)
             do j = 1, cg%n_(ydim)
                vx = cg%u(imxf,:,j,k) / cg%u(idnf,:,j,k)
-               divvel(4:nx-3,j,k) = divvel(4:nx-3,j,k) + ( vx(5:nx) - vx(3:nx-4) )  * (p3_4 *idx)
-               divvel(4:nx-3,j,k) = divvel(4:nx-3,j,k) + ( vx(6:nx) - vx(2:nx-5) )  * (m3_20*idx)
-               divvel(4:nx-3,j,k) = divvel(4:nx-3,j,k) + ( vx(7:nx) - vx(1:nx-6) )  * (p1_60*idx)
+               divvel(2:cg%n_(xdim)-1,j,k) = ( vx(3:cg%n_(xdim)) - vx(1:cg%n_(xdim)-2) )  * (half*cg%idx)
             enddo
          enddo
-         divvel(1:3,:,:) = big; divvel(nx-2:nx,:,:) = big ! for sanity
+         divvel(1,:,:) = divvel(2,:,:); divvel(cg%n_(xdim),:,:) = divvel(cg%n_(xdim)-1,:,:) ! for sanity
       endif
 
       if (dom%has_dir(ydim)) then
          do k = 1, cg%n_(zdim)
             do i = 1, cg%n_(xdim)
                vy = cg%u(imyf,i,:,k) / cg%u(idnf,i,:,k)
-               divvel(i,4:ny-3,k) = divvel(i,4:ny-3,k) + ( vy(5:ny) - vy(3:ny-4) )  * (p3_4 *idy)
-               divvel(i,4:ny-3,k) = divvel(i,4:ny-3,k) + ( vy(6:ny) - vy(2:ny-5) )  * (m3_20*idy)
-               divvel(i,4:ny-3,k) = divvel(i,4:ny-3,k) + ( vy(7:ny) - vy(1:ny-6) )  * (p1_60*idy)
+               divvel(i,2:cg%n_(ydim)-1,k) = divvel(i,2:cg%n_(ydim)-1,k)+( vy(3:cg%n_(ydim)) - vy(1:cg%n_(ydim)-2) )  * (half*cg%idy)
             enddo
          enddo
-         divvel(:,1:3,:) = big; divvel(:,ny-2:ny,:) = big ! for sanity
+         divvel(:,1,:) = divvel(:,2,:); divvel(:, cg%n_(ydim),:) = divvel(:, cg%n_(ydim)-1,:) ! for sanity
       endif
 
       if (dom%has_dir(zdim)) then
          do j = 1, cg%n_(ydim)
             do i = 1, cg%n_(xdim)
                vz = cg%u(imzf,i,j,:) / cg%u(idnf,i,j,:)
-               divvel(i,j,4:nz-3) = divvel(i,j,4:nz-3) + ( vz(5:nz) - vz(3:nz-4) )  * (p3_4 *idz)
-               divvel(i,j,4:nz-3) = divvel(i,j,4:nz-3) + ( vz(6:nz) - vz(2:nz-5) )  * (m3_20*idz)
-               divvel(i,j,4:nz-3) = divvel(i,j,4:nz-3) + ( vz(7:nz) - vz(1:nz-6) )  * (p1_60*idz)
+               divvel(i,j,2:cg%n_(zdim)-1) = divvel(i,j,2:cg%n_(zdim)-1)+( vz(3:cg%n_(zdim)) - vz(1:cg%n_(zdim)-2) )  * (half*cg%idz)
             enddo
          enddo
-         divvel(:,:,1:3) = big; divvel(:,:,nz-2:nz) = big ! for sanity
+         divvel(:,:,1) = divvel(:,:,2); divvel(:,:, cg%n_(zdim)) = divvel(:,:, cg%n_(zdim)-1) ! for sanity
       endif
 
       deallocate(vx, vy, vz)
