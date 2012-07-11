8c9e497049515e882685824560e3603485dce04d
Dominik Wóltański <minikwolt@astri.umk.pl>
2012-07-11 13:19:07 +0000

    trunk: [crhelpers] coiling up

--- src/fluids/cosmicrays/crhelpers.F90
+++ src/fluids/cosmicrays/crhelpers.F90
@@ -85,79 +85,43 @@ contains
 !! in particular in http://turbulence.pha.jhu.edu/Database-functions.pdf
 !<
    subroutine div_v_6th_lp(ifluid, cg)
-      use constants,   only: xdim, ydim, zdim, big
-      use dataio_pub,  only: die
-      use domain,      only: dom
-      use fluidindex,  only: iarr_all_dn, iarr_all_mx, iarr_all_my, iarr_all_mz
-      use grid_cont,   only: grid_container
-      use named_array, only: qna
+      use cg_list_global, only: all_cg
+      use constants,      only: xdim, zdim, big, ndims, INT4
+      use domain,         only: dom
+      use fluidindex,     only: iarr_all_dn
+      use grid_cont,      only: grid_container
+      use named_array,    only: qna
 
       implicit none
 
       integer(kind=4),               intent(in)    :: ifluid
       type(grid_container), pointer, intent(inout) :: cg
-      real, dimension(:,:,:), pointer :: divvel
-      integer                         :: i, j, k
-      real, parameter                 :: p3_4 = 3./4., m3_20 = -3./20., p1_60 = 1./60.
+      real, dimension(:),   pointer                :: divvel, mom, dn
+      integer(kind=4)                              :: dir, dir2, dir3
+      integer                                      :: i2, i3
+      real, parameter                              :: p3_4 = 3./4., m3_20 = -3./20., p1_60 = 1./60.
 
-      divvel => cg%q(qna%ind(divv_n))%arr
-      if (.not. associated(divvel)) call die("[crhelpers:div_v] cannot get divvel")
+      cg%q(qna%ind(divv_n))%arr(:,:,:) = 0.0
 
-      divvel(:,:,:) = 0.0
-
-      if (dom%has_dir(xdim)) then
-         do k = 1, cg%n_(zdim)
-            do j = 1, cg%n_(ydim)
-               associate( &
-                  vx => (cg%u(iarr_all_mx(ifluid), :, j, k) / cg%u(iarr_all_dn(ifluid), :, j, k)), &
-                  nx => cg%n_(xdim), &
-                  idx => cg%idx &
-               )
-                  divvel(4:nx-3, j, k) = divvel(4:nx-3, j, k) + (vx(5:nx-2) - vx(3:nx-4)) * (p3_4  * idx)
-                  divvel(4:nx-3, j, k) = divvel(4:nx-3, j, k) + (vx(6:nx-1) - vx(2:nx-5)) * (m3_20 * idx)
-                  divvel(4:nx-3, j, k) = divvel(4:nx-3, j, k) + (vx(7:nx  ) - vx(1:nx-6)) * (p1_60 * idx)
+      do dir = xdim, zdim
+      dir2 = mod(dir,ndims)+1_INT4 ; dir3 = mod(dir+1_INT4,ndims)+1_INT4
+      if (dom%has_dir(dir)) then
+         do i2 = 1, cg%n_(dir2)
+            do i3 = 1, cg%n_(dir3)
+               divvel => cg%q(qna%ind(divv_n))%get_sweep(dir, i2, i3)
+               mom    => cg%w(all_cg%fi)%get_sweep(dir, iarr_all_dn(ifluid)+dir, i2, i3)
+               dn     => cg%w(all_cg%fi)%get_sweep(dir, iarr_all_dn(ifluid)    , i2, i3)
+               associate( vv  => (mom(:) / dn(:)), nn  => cg%n_(dir), idl => cg%idl(dir) )
+                  divvel(4:nn-3)  = divvel(4:nn-3) + (vv(5:nn-2) - vv(3:nn-4)) * (p3_4  * idl)
+                  divvel(4:nn-3)  = divvel(4:nn-3) + (vv(6:nn-1) - vv(2:nn-5)) * (m3_20 * idl)
+                  divvel(4:nn-3)  = divvel(4:nn-3) + (vv(7:nn  ) - vv(1:nn-6)) * (p1_60 * idl)
+                  divvel(1:3)     = big
+                  divvel(nn-2:nn) = big
                end associate
             enddo
          enddo
-         divvel(1:3, :, :) = big
-         divvel(cg%n_(xdim)-2:cg%n_(xdim), :, :) = big
       endif
-
-      if (dom%has_dir(ydim)) then
-         do k = 1, cg%n_(zdim)
-            do i = 1, cg%n_(xdim)
-               associate( &
-                  vy => (cg%u(iarr_all_my(ifluid), i, :, k) / cg%u(iarr_all_dn(ifluid), i, :, k)), &
-                  ny => cg%n_(ydim), &
-                  idy => cg%idy &
-               )
-                  divvel(i, 4:ny-3, k) = divvel(i, 4:ny-3, k) + (vy(5:ny-2) - vy(3:ny-4)) * (p3_4  * idy)
-                  divvel(i, 4:ny-3, k) = divvel(i, 4:ny-3, k) + (vy(6:ny-1) - vy(2:ny-5)) * (m3_20 * idy)
-                  divvel(i, 4:ny-3, k) = divvel(i, 4:ny-3, k) + (vy(7:ny  ) - vy(1:ny-6)) * (p1_60 * idy)
-               end associate
-            enddo
-         enddo
-         divvel(:, 1:3, :) = big
-         divvel(:, cg%n_(ydim)-2:cg%n_(ydim), :) = big
-      endif
-
-      if (dom%has_dir(zdim)) then
-         do j = 1, cg%n_(ydim)
-            do i = 1, cg%n_(xdim)
-               associate( &
-                  vz => (cg%u(iarr_all_mz(ifluid), i, j, :) / cg%u(iarr_all_dn(ifluid), i, j, :)), &
-                  nz => cg%n_(zdim), &
-                  idz => cg%idz &
-               )
-                  divvel(i, j, 4:nz-3) = divvel(i, j, 4:nz-3) + (vz(5:nz-2) - vz(3:nz-4)) * (p3_4  * idz)
-                  divvel(i, j, 4:nz-3) = divvel(i, j, 4:nz-3) + (vz(6:nz-1) - vz(2:nz-5)) * (m3_20 * idz)
-                  divvel(i, j, 4:nz-3) = divvel(i, j, 4:nz-3) + (vz(7:nz  ) - vz(1:nz-6)) * (p1_60 * idz)
-               end associate
-            enddo
-         enddo
-         divvel(:, :, 1:3) = big
-         divvel(:, :, cg%n_(zdim)-2:cg%n_(zdim)) = big
-      endif
+      enddo
 
    end subroutine div_v_6th_lp
 !>
@@ -168,70 +132,40 @@ contains
 
    subroutine div_v_1st(ifluid, cg)
 
-      use constants,   only: xdim, ydim, zdim, half
-      use dataio_pub,  only: die
-      use domain,      only: dom
-      use fluidindex,  only: iarr_all_dn, iarr_all_mx, iarr_all_my, iarr_all_mz
-      use grid_cont,   only: grid_container
-      use named_array, only: qna
+      use cg_list_global, only: all_cg
+      use constants,      only: xdim, zdim, half, ndims, INT4
+      use domain,         only: dom
+      use fluidindex,     only: iarr_all_dn
+      use grid_cont,      only: grid_container
+      use named_array,    only: qna
 
       implicit none
 
       integer(kind=4),               intent(in)    :: ifluid
       type(grid_container), pointer, intent(inout) :: cg
+      real, dimension(:),   pointer                :: divvel, mom, dn
+      integer(kind=4)                              :: dir, dir2, dir3
+      integer                                      :: i2, i3
 
-      real, dimension(:,:,:), pointer :: divvel
-      integer                         :: i, j, k
+      cg%q(qna%ind(divv_n))%arr(:,:,:) = 0.0
 
-      divvel => cg%q(qna%ind(divv_n))%arr
-      if (.not. associated(divvel)) call die("[crhelpers:div_v] cannot get divvel")
-
-      divvel(:,:,:) = 0.0
-
-      if (dom%has_dir(xdim)) then
-         do k = 1, cg%n_(zdim)
-            do j = 1, cg%n_(ydim)
-               associate( &
-                  vx => (cg%u(iarr_all_mx(ifluid), :, j, k) / cg%u(iarr_all_dn(ifluid), :, j, k)), &
-                  nx => cg%n_(xdim) &
-               )
-                  divvel(2:nx-1, j, k) = (vx(3:nx) - vx(1:nx-2)) * (half * cg%idx)
-               end associate
+      do dir = xdim, zdim
+         dir2 = mod(dir,ndims)+1_INT4 ; dir3 = mod(dir+1_INT4,ndims)+1_INT4
+         if (dom%has_dir(dir)) then
+            do i2 = 1, cg%n_(dir2)
+               do i3 = 1, cg%n_(dir3)
+                  divvel => cg%q(qna%ind(divv_n))%get_sweep(dir, i2, i3)
+                  mom    => cg%w(all_cg%fi)%get_sweep(dir, iarr_all_dn(ifluid)+dir, i2, i3)
+                  dn     => cg%w(all_cg%fi)%get_sweep(dir, iarr_all_dn(ifluid)    , i2, i3)
+                  associate( vv  => (mom(:) / dn(:)), nn  => cg%n_(dir), idl => cg%idl(dir) )
+                     divvel(2:nn-1) = divvel(2:nn-1) + (vv(3:nn) - vv(1:nn-2)) * (half * idl)
+                     divvel(1) = divvel(2)
+                     divvel(nn) = divvel(nn-1) ! for sanity
+                  end associate
+               enddo
             enddo
-         enddo
-         divvel(1, :, :) = divvel(2, :, :)
-         divvel(cg%n_(xdim), :, :) = divvel(cg%n_(xdim)-1, :, :) ! for sanity
-      endif
-
-      if (dom%has_dir(ydim)) then
-         do k = 1, cg%n_(zdim)
-            do i = 1, cg%n_(xdim)
-               associate( &
-                  vy => (cg%u(iarr_all_my(ifluid), i, :, k) / cg%u(iarr_all_dn(ifluid), i, :, k)), &
-                  ny => cg%n_(ydim) &
-               )
-                  divvel(i, 2:ny-1, k) = divvel(i, 2:ny-1, k) + (vy(3:ny) - vy(1:ny-2)) * (half * cg%idy)
-               end associate
-            enddo
-         enddo
-         divvel(:, 1, :) = divvel(:, 2, :)
-         divvel(:, cg%n_(ydim), :) = divvel(:, cg%n_(ydim)-1, :) ! for sanity
-      endif
-
-      if (dom%has_dir(zdim)) then
-         do j = 1, cg%n_(ydim)
-            do i = 1, cg%n_(xdim)
-               associate( &
-                  vz => (cg%u(iarr_all_mz(ifluid), i, j, :) / cg%u(iarr_all_dn(ifluid), i, j, :)), &
-                  nz => cg%n_(zdim) &
-               )
-                  divvel(i, j, 2:cg%n_(zdim)-1) = divvel(i, j, 2:nz-1) + (vz(3:nz) - vz(1:nz-2)) * (half * cg%idz)
-               end associate
-            enddo
-         enddo
-         divvel(:, :, 1) = divvel(:, :, 2)
-         divvel(:, :, cg%n_(zdim)) = divvel(:, :, cg%n_(zdim)-1) ! for sanity
-      endif
+         endif
+      enddo
 
    end subroutine div_v_1st
 
