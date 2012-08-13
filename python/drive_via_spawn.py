2a0b5d0300726e804e29688b9abb533095eaf598
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2012-08-13 14:58:14 +0000

    trunk: [drive_via_spawn] add parser for mpisignals

--- python/drive_via_spawn.py
+++ python/drive_via_spawn.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 from mpi4py import MPI
 from array import array
-#import numpy as np
 import os
 import sys
 try:
@@ -18,6 +17,31 @@ def get_np():
         print "It seems we're not running in PBS shell"
         temp = -1
     return int(temp)
+
+
+def parse_mpisignals(fn='../src/base/mpi_signals.F90'):
+   import re
+   enum = re.compile("enumerator", re.IGNORECASE)
+   equal = re.compile("=")
+
+   f = file(fn,'r')
+   lines = [line.split('::')[-1].strip() 
+            for line in  filter(enum.search, f.readlines())]
+   f.close()
+
+   mpi_signals = {}
+   i = 0
+   for line in lines:
+      if equal.search(line):
+         temp = line.split("=")
+         name, i = temp[0], int(temp[-1])
+      else:
+         name = line
+      mpi_signals[name[4:].lower().strip()] = i
+      i += 1
+
+   return mpi_signals
+
 
 if have_argparse:
     parser = argparse.ArgumentParser(description='Run PIERNIK via MPI_Spawn')
@@ -43,6 +67,7 @@ if num_procs < 2:
     sys.exit(2)
 
 signal = array('i', [0])
+mpi_signals = parse_mpisignals('src/base/mpi_signals.F90')
 
 piernik = MPI.COMM_SELF.Spawn(run_args.exe, maxprocs=num_procs - 1,
                              args=run_args.args.split())
@@ -51,13 +76,16 @@ while True:
     piernik.Recv([signal, MPI.INT],
          MPI.ANY_SOURCE, MPI.ANY_TAG,
          status)
-    print signal
-    if signal[0] == 1:
+    if not signal[0] in mpi_signals.values():
+        print("I've got signal %i but I have no clue what to do with it :/" % signal[0])
+
+    if signal[0] == mpi_signals['tsl_updated']:
         print "Python script: TSL was updated"
         print "GRID magic can happen here \o/"
+    elif signal[0] == mpi_signals['clean_exit']:
+        print "It seems that Piernik finished execution gracefully \o/"
 #  if status.Get_tag() == 1:
     if signal[0] <= 0:
         break
 
 piernik.Disconnect()
-print "done in python too"
