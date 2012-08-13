a6a80f011a285bfd64c5a769a80419c23e3c7dee
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2012-08-13 12:46:45 +0000

    trunk: [drive_via_spawn] make it python2.6 compatible

--- python/drive_via_spawn.py
+++ python/drive_via_spawn.py
@@ -4,7 +4,12 @@ from array import array
 #import numpy as np
 import os
 import sys
-import argparse
+try:
+   import argparse
+   have_argparse = True
+except ImportError:
+   from optparse import OptionParser
+   have_argparse = False
 
 def get_np():
    temp = os.getenv('PBS_NP')
@@ -13,24 +18,30 @@ def get_np():
       temp = -1
    return int(temp)
 
-parser = argparse.ArgumentParser(description='Run PIERNIK via MPI_Spawn')
-parser.add_argument('-e', '--exe', type=str, default='obj/piernik',
-                   help='Path to executable')
-parser.add_argument('-a', '--args', type=str,
-                   default='-p obj/ -w obj/',
-                   help='arguments passed to PIERNIK')
-
-run_args = parser.parse_args()
+if have_argparse:
+   parser = argparse.ArgumentParser(description='Run PIERNIK via MPI_Spawn')
+   parser.add_argument('-e', '--exe', type=str, default='obj/piernik',
+                      help='Path to executable')
+   parser.add_argument('-a', '--args', type=str,
+                      default='-p obj/ -w obj/',
+                      help='arguments passed to PIERNIK')
+   run_args = parser.parse_args()
+else:
+   parser = OptionParser()
+   parser.add_option('-e', '--exe', type=str, default='obj/piernik',
+                    help='Path to executable')
+   parser.add_option('-a', '--args', type=str,
+                    default='-p obj/ -w obj/',
+                    help='arguments passed to PIERNIK')
+   run_args = parser.parse_args()
+   (run_args, add_args) = parser.parse_args()
 
 num_procs = get_np()
 if num_procs < 2:
    print "I need more than two processors to run"
    sys.exit(2)
 
-executable = 'obj/piernik'
-args = ['-p', 'runs/sedov']
 signal  = array('i', [0])
-#n = np.array([0])
 
 piernik = MPI.COMM_SELF.Spawn(run_args.exe, maxprocs=num_procs-1, args=run_args.args.split())
 status = MPI.Status()
