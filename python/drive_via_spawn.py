4a2454b482a29b2232f5db32b20697925b7d8f11
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2012-08-13 11:04:02 +0000

    trunk: [drive_via_spawn] add argument parsing with sane defaults

--- python/drive_via_spawn.py
+++ python/drive_via_spawn.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 from mpi4py import MPI
 from array import array
+#import numpy as np
 import os
 import sys
-#import numpy as np
+import argparse
 
 def get_np():
    temp = os.getenv('PBS_NP')
@@ -11,6 +12,15 @@ def get_np():
       print "It seems we're not running in PBS shell"
       temp = -1
    return int(temp)
+
+parser = argparse.ArgumentParser(description='Run PIERNIK via MPI_Spawn')
+parser.add_argument('-e', '--exe', type=str, default='obj/piernik',
+                   help='Path to executable')
+parser.add_argument('-a', '--args', type=str,
+                   default='-p obj/ -w obj/',
+                   help='arguments passed to PIERNIK')
+
+run_args = parser.parse_args()
 
 num_procs = get_np()
 if num_procs < 2:
@@ -22,17 +32,14 @@ args = ['-p', 'runs/sedov']
 signal  = array('i', [0])
 #n = np.array([0])
 
-piernik = MPI.COMM_SELF.Spawn(executable, maxprocs=num_procs-1, args=args)
+piernik = MPI.COMM_SELF.Spawn(run_args.exe, maxprocs=num_procs-1, args=run_args.args.split())
 status = MPI.Status()
 while True:
-#  piernik.Reduce(sendbuf=None,
-#                 recvbuf=[n, MPI.INT],
-#                 op=MPI.MAX, root=MPI.ROOT)
    piernik.Recv([signal, MPI.INT],
        MPI.ANY_SOURCE, MPI.ANY_TAG,
        status)
    print signal
-   if signal[0] == 10:
+   if signal[0] == 1:
       print "Python script: TSL was updated"
       print "GRID magic can happen here \o/"
 #  if status.Get_tag() == 1:
