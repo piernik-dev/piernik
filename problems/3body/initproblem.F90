e37a63c9dc125bec20d02e5b0f427055f84ca669
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2012-09-23 11:50:45 +0000

    trunk: [3body] update problem wrt r6654, use associate construct instead of importing modules and creating temporary variables

--- problems/3body/initproblem.F90
+++ problems/3body/initproblem.F90
@@ -44,17 +44,13 @@ module initproblem
    namelist /PROBLEM_CONTROL/ d0, p0, bx0, by0, bz0, Eexpl, x0, y0, z0, r0, n_sn, dt_sn
 
 contains
-
 !-----------------------------------------------------------------------------
-
    subroutine problem_pointers
 
       implicit none
 
    end subroutine problem_pointers
-
 !-----------------------------------------------------------------------------
-
    subroutine read_problem_par
 
       implicit none
@@ -68,36 +64,30 @@ contains
       use cg_leaves,    only: leaves
       use dataio_pub,   only: printinfo
       use fluidindex,   only: flind
-      use fluidtypes,   only: component_fluid
-      use grid_cont,    only: grid_container
-      use particle_pub, only: pset, psolver
+      use particle_pub, only: pset
 
       implicit none
 
       integer                         :: i, j, k, p
-      class(component_fluid), pointer :: fl
       type(cg_list_element),  pointer :: cgl
-      type(grid_container),   pointer :: cg
 
       do p = lbound(flind%all_fluids, dim=1), ubound(flind%all_fluids, dim=1)
-         fl => flind%all_fluids(p)%fl
-
-! Uniform equilibrium state
-
          cgl => leaves%first
          do while (associated(cgl))
-            cg => cgl%cg
-
-            do k = 1, cg%n_(zdim)
-               do j = 1, cg%n_(ydim)
-                  do i = 1, cg%n_(xdim)
-                     cg%u(fl%idn,i,j,k) = 1.0
-                     cg%u(fl%imx,i,j,k) = 0.0
-                     cg%u(fl%imy,i,j,k) = 0.0
-                     cg%u(fl%imz,i,j,k) = 0.0
+            associate(cg => cgl%cg)
+               do k = 1, cg%n_(zdim)
+                  do j = 1, cg%n_(ydim)
+                     do i = 1, cg%n_(xdim)
+                        associate( fl => flind%all_fluids(p)%fl )
+                           cg%u(fl%idn,i,j,k) = 1.0
+                           cg%u(fl%imx,i,j,k) = 0.0
+                           cg%u(fl%imy,i,j,k) = 0.0
+                           cg%u(fl%imz,i,j,k) = 0.0
+                        end associate
+                     enddo
                   enddo
                enddo
-            enddo
+            end associate
             cgl => cgl%nxt
          enddo
       enddo
@@ -105,7 +95,6 @@ contains
       call pset%add(1.0, [ 0.9700436,  -0.24308753,  0.0], [ 0.466203685,  0.43236573, 0.0])
       call pset%add(1.0, [-0.9700436,   0.24308753,  0.0], [ 0.466203685,  0.43236573, 0.0])
       call pset%add(1.0, [ 0.0,         0.0,         0.0], [-0.932407370, -0.86473146, 0.0])
-      if (associated(psolver)) call pset%evolve(psolver, 0.0, 10.0)   !! move it somewhere into main code
       call printinfo('To see results type: gnuplot -p -e ''plot "nbody_out.log" u 2:3'' ')
 
    end subroutine init_prob
