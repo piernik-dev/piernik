b839e503e9948f32aed276b4e4ece29791aa2a44
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2012-10-21 14:54:43 +0000

    trunk: [validate] allow to spawn jobs for all available problems

--- bin/validate.py
+++ bin/validate.py
@@ -7,40 +7,59 @@
 #    anything, make it configurable
 
 import sys
+sys.path.append("python/")
 import os
 import json
 import tempfile
 import hashlib
 import argparse
+from DirWalk import DirectoryWalker
 try:
-   import requests
+    import requests
 except ImportError:
-   print "You need request: http://pypi.python.org/pypi/requests"
-   sys.exit(-1)
+    print "You need request: http://pypi.python.org/pypi/requests"
+    sys.exit(-1)
 try:
-   import pysvn
+    import pysvn
 except ImportError:
-   print "You need pysvn"
-   sys.exit(-1)
+    print "You need pysvn"
+    sys.exit(-1)
+
+
+def run_jenkins_job(branch, setup_args):
+    JENKINS = "http://ladon:8080/job/compile.%s.setup/build" % branch
+
+    file_param = {'name': 'patch.diff', 'file': 'file0'}
+    prob_param = {'name': 'problem_name', 'value': setup_args}
+
+    files = {'file0': open(f.name, 'rb')}
+    payload = {
+        'json': json.dumps({'parameter': [prob_param, file_param]}),
+        'Submit': 'Build'
+    }
+    r = requests.post(JENKINS, data=payload, files=files,
+                      auth=requests.auth.HTTPBasicAuth(USER, PASSWORD))
+
 
 parser = argparse.ArgumentParser(
-   description='Validate repo changes using jenkins'
+    description='Validate repo changes using jenkins'
 )
-parser.add_argument('setup_args', type=str, nargs=1,
+parser.add_argument('setup_args', type=str, nargs='?', default='',
                     help="string that is directly passed to setup")
 parser.add_argument('-p', '--pretend', action='store_true')
+parser.add_argument('-a', '--all', action='store_true')
 args = parser.parse_args()
 
 if os.path.exists('.jenkins'):
-   f = open('.jenkins', 'rb')
-   USER = f.readline().strip()
-   PASSWORD = f.readline().strip()
-   f.close()
+    f = open('.jenkins', 'rb')
+    USER = f.readline().strip()
+    PASSWORD = f.readline().strip()
+    f.close()
 else:
-   print("You need to create .jenkins file with:")
-   print("user")
-   print("pass")
-   sys.exit(-1)
+    print("You need to create .jenkins file with:")
+    print("user")
+    print("pass")
+    sys.exit(-1)
 
 
 svn = pysvn.Client()
@@ -59,26 +78,26 @@ hash_file.write(diff_hash.hexdigest())
 hash_file.close()
 
 entry = svn.info('.')
-url = entry.url.replace(entry.repos+'/','')
+url = entry.url.replace(entry.repos + '/', '')
 if url.find('branches') == -1:
-   branch = url.partition("piernik/")[2].partition("/")[0]
+    branch = url.partition("piernik/")[2].partition("/")[0]
 else:
-   branch = url.partition("branches/")[2].partition("/")[0]
+    branch = url.partition("branches/")[2].partition("/")[0]
 
-JENKINS="http://ladon:8080/job/compile.%s.setup/build" % branch
-
-file_param={'name': 'patch.diff', 'file': 'file0'}
-prob_param={'name': 'problem_name', 'value': "".join(args.setup_args)}
-
-files={'file0': open(f.name, 'rb')}
-payload = {
-   'json': json.dumps({'parameter' : [prob_param, file_param]}),
-   'Submit': 'Build'
-}
 
 if args.pretend:
-   print("Validation was not performed due to --pretend")
+    print("Validation was not performed due to --pretend")
 else:
-   r = requests.post(JENKINS, data=payload, allow_redirects=True, files=files,
-                     auth=requests.auth.HTTPBasicAuth(USER, PASSWORD))
+    if args.all:
+        for file in DirectoryWalker(os.path.abspath('.')):
+            if "initproblem.F90" in file and not os.path.islink(file):
+                directory, problem = os.path.split(file)
+                if not os.path.exists(os.path.join(directory, 'OBSOLETE')):
+                    arg = "%s %s" % (os.path.split(
+                        directory)[-1], args.setup_args)
+                    run_jenkins_job(branch, arg)
+    else:
+        if args.setup_args == '':
+            args.setup_args = "sedov"
+        run_jenkins_job(branch, args.setup_args)
 os.unlink(f.name)
