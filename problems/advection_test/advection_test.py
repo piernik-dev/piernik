73b0464c9b989718578ccedcf87b3bb62a5ba6a6
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2012-10-26 06:58:07 +0000

    trunk: [advection_test] add new tests for answer testing

---
+++ problems/advection_test/advection_test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+from yt.config import ytcfg
+ytcfg["yt", "loglevel"] = "50"
+from yt.mods import load
+import numpy as np
+
+
+def calculate_norm(fn):
+    pf = load(fn)
+
+    data = pf.h.all_data()
+    diff = data['inid'] - data['denn']
+    norm = np.sqrt((diff ** 2).sum() / data['inid'].sum())
+    print("Calculating difference between numerical and analytical solution")
+    print("L2 error norm = %12.6f, min and max error = %15.6f %15.6f" %
+          (norm, diff.min(), diff.max()))
+    del data, diff, pf
+
+
+def diff_files(fn1, fn2, field):
+    pf1 = load(fn1)
+    data1 = pf1.h.all_data()[field]
+    pf2 = load(fn2)
+    data2 = pf2.h.all_data()[field]
+    diff = data1 - data2
+    norm = np.sqrt((diff ** 2).sum() / data1.sum())
+    print("Calculating difference in %s between files %s and %s" %
+          (field, pf1, pf2))
+    print("L2 error norm = %12.6f, min and max error = %15.6f %15.6f" %
+          (norm, diff.min(), diff.max()))
+    del data1, data2, diff, pf1, pf2
+
+for i in range(1, 3):
+    fn1 = "moving_pulse_ts1_%4.4i.h5" % i
+    fn2 = "moving_pulse_ts2_%4.4i.h5" % i
+    calculate_norm(fn1)
+    calculate_norm(fn2)
+    diff_files(fn1, fn2, 'denn')
---
+++ problems/advection_test/restart_test_v2_jenkins.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+rm -rf runs/advection_test
+
+echo "Compilation started"
+./setup advection_test -p problem.par.restart_test_v2 -c gnufast &> /dev/null
+echo "Compilation finished"
+
+cp problems/advection_test/*py runs/advection_test
+
+cd runs/advection_test
+echo "Starting run for tend = 1.0 and nproc = 1 run_id = ts1"
+mpiexec -n 1 ./piernik > /dev/null
+echo "Finished run for tend = 1.0 and nproc = 1 run_id = ts1"
+
+echo "Starting run for tend = 2.0 and nproc = 1 run_id = ts1"
+mpiexec -n 1 ./piernik -n '$END_CONTROL tend = 2.0 /' > /dev/null
+echo "Finished run for tend = 2.0 and nproc = 1 run_id = ts1"
+
+echo "Starting run for tend = 1.0 and nproc = 5 run_id = ts2"
+mpiexec -n 5 ./piernik -n '$OUTPUT_CONTROL run_id = "ts2" /' > /dev/null
+echo "Finished run for tend = 1.0 and nproc = 5 run_id = ts2"
+
+echo "Starting run for tend = 2.0 and nproc = 3 run_id = ts2"
+mpiexec -n 3 ./piernik -n '$END_CONTROL tend = 2.0 /' \
+   -n '$OUTPUT_CONTROL run_id = "ts2" /' > /dev/null
+echo "Finished run for tend = 2.0 and nproc = 3 run_id = ts2"
+
+echo "Starting testsuite"
+python advection_test.py
+echo "Testsuite finished"
