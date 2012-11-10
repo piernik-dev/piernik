c775cfbe46d7d31f664c44a0a4b182adc2c3a80c
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2012-11-10 14:09:23 +0000

    trunk: Modularize drive_via_spawn

--- python/drive_via_spawn.py
+++ python/drive_via_spawn.py
@@ -43,50 +43,51 @@ def parse_mpisignals(fn='../src/base/mpi_signals.F90'):
     return mpi_signals
 
 
-if have_argparse:
-    parser = argparse.ArgumentParser(description='Run PIERNIK via MPI_Spawn')
-    parser.add_argument('-e', '--exe', type=str, default='obj/piernik',
-                       help='Path to executable')
-    parser.add_argument('-a', '--args', type=str,
-                       default='-p obj/ -w obj/',
-                       help='arguments passed to PIERNIK')
-    run_args = parser.parse_args()
-else:
-    parser = OptionParser()
-    parser.add_option('-e', '--exe', type=str, default='obj/piernik',
-                     help='Path to executable')
-    parser.add_option('-a', '--args', type=str,
-                     default='-p obj/ -w obj/',
-                     help='arguments passed to PIERNIK')
-    run_args = parser.parse_args()
-    (run_args, add_args) = parser.parse_args()
+if __name__ == "__main__":
+    if have_argparse:
+        parser = argparse.ArgumentParser(description='Run PIERNIK via MPI_Spawn')
+        parser.add_argument('-e', '--exe', type=str, default='obj/piernik',
+                        help='Path to executable')
+        parser.add_argument('-a', '--args', type=str,
+                        default='-p obj/ -w obj/',
+                        help='arguments passed to PIERNIK')
+        run_args = parser.parse_args()
+    else:
+        parser = OptionParser()
+        parser.add_option('-e', '--exe', type=str, default='obj/piernik',
+                        help='Path to executable')
+        parser.add_option('-a', '--args', type=str,
+                        default='-p obj/ -w obj/',
+                        help='arguments passed to PIERNIK')
+        run_args = parser.parse_args()
+        (run_args, add_args) = parser.parse_args()
 
-num_procs = get_np()
-if num_procs < 2:
-    print "I need more than two processors to run"
-    sys.exit(2)
+    num_procs = get_np()
+    if num_procs < 2:
+        print "I need more than two processors to run"
+        sys.exit(2)
 
-signal = array('i', [0])
-mpi_signals = parse_mpisignals('src/base/mpi_signals.F90')
+    signal = array('i', [0])
+    mpi_signals = parse_mpisignals('src/base/mpi_signals.F90')
 
-piernik = MPI.COMM_SELF.Spawn(run_args.exe, maxprocs=num_procs - 1,
-                             args=run_args.args.split())
-status = MPI.Status()
-while True:
-    piernik.Recv([signal, MPI.INT],
-         MPI.ANY_SOURCE, MPI.ANY_TAG,
-         status)
-    if not signal[0] in mpi_signals.values():
-        print("I've got signal %i but I have no clue what to do with it :/" %
-        signal[0])
+    piernik = MPI.COMM_SELF.Spawn(run_args.exe, maxprocs=num_procs - 1,
+                                args=run_args.args.split())
+    status = MPI.Status()
+    while True:
+        piernik.Recv([signal, MPI.INT],
+            MPI.ANY_SOURCE, MPI.ANY_TAG,
+            status)
+        if not signal[0] in mpi_signals.values():
+            print("I've got signal %i but I have no clue what to do with it :/" %
+            signal[0])
 
-    if signal[0] == mpi_signals['tsl_updated']:
-        print "Python script: TSL was updated"
-        print "GRID magic can happen here \o/"
-    elif signal[0] == mpi_signals['clean_exit']:
-        print "It seems that Piernik finished execution gracefully \o/"
-#  if status.Get_tag() == 1:
-    if signal[0] <= 0:
-        break
+        if signal[0] == mpi_signals['tsl_updated']:
+            print "Python script: TSL was updated"
+            print "GRID magic can happen here \o/"
+        elif signal[0] == mpi_signals['clean_exit']:
+            print "It seems that Piernik finished execution gracefully \o/"
+        #if status.Get_tag() == 1:
+        if signal[0] <= 0:
+            break
 
-piernik.Disconnect()
+    piernik.Disconnect()
