b2530f7523ae850b4a46c3f95d2f2ac0679efe0d
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2012-11-12 16:42:21 +0000

    trunk: [sedov] add problem specific python helpers

---
+++ problems/sedov/piernik_problem.py
@@ -0,0 +1,12 @@
+from yt.pmods import load, SlicePlot, parallel_objects
+
+
+def visualize(files):
+    output = []
+    for fn in parallel_objects(files, njobs=-1):
+        pf = load(fn)
+        slc = SlicePlot(pf, 2, 'denn')
+        output += slc.save()
+        slc = SlicePlot(pf, 2, 'enen')
+        output += slc.save()
+    return output
