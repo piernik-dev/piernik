2ef03883ea37381a9b5af6402859385e16272cc7
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2012-11-13 10:35:06 +0000

    trunk: [sedov] update vis

--- problems/sedov/piernik_problem.py
+++ problems/sedov/piernik_problem.py
@@ -1,14 +1,25 @@
+import matplotlib
+matplotlib.use('cairo')
+import matplotlib.pyplot as plt
 from yt.mods import load, SlicePlot, parallel_objects
 
+fields = ['denn', 'enen']
 
 def visualize(files):
     output = []
     for fn in parallel_objects(files, njobs=-1):
         pf = load(fn)
-        slc = SlicePlot(pf, 2, 'denn')
-        output += slc.save()
-        slc = SlicePlot(pf, 2, 'enen')
-        output += slc.save()
+        s = pf.h.slice(2, 0.0, fields=fields)
+        img = s.to_frb(2.0, (512, 512))
+        for field in fields:
+            fig = plt.figure(0, figsize=(8, 6))
+            ax = plt.subplot(111)
+            ax.imshow(img[field])
+            plt.draw()
+            fn_out = fn.replace('.h5', '_%s.png' % field)
+            print fn_out
+            plt.savefig(fn_out)
+        output.append(fn_out)
     return output
 
 if __name__ == "__main__":
