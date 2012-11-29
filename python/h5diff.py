c9ec47fe1004f32fb696019c1e286fb1b68c7e0a
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2012-11-29 19:24:04 +0000

    trunk: [h5diff] add stupid h5 comparison script

---
+++ python/h5diff.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+'''Stupid h5 content comparison script'''
+
+import sys
+from yt.mods import load
+
+THRESHOLD = 1e-9
+
+if len(sys.argv) != 3:
+    print("Wrong number of arguments!")
+    sys.exit(-1)
+
+PF1 = load(sys.argv[1])
+PF2 = load(sys.argv[2])
+
+DATA1 = PF1.h.all_data()
+DATA2 = PF2.h.all_data()
+
+if not PF1.h.field_list == PF2.h.field_list:
+    print("Fields in files differ!")
+    sys.exit(-1)
+
+for field in PF1.h.field_list:
+    if abs(DATA1[field] - DATA2[field]).max() >= THRESHOLD:
+        print("Field %s differs" % field)
+        sys.exit(-1)
