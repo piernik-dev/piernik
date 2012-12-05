072d7a38026f57deb63f824977c634f4264eed0a
Dominik Wóltański <minikwolt@astri.umk.pl>
2012-12-05 11:50:17 +0000

    trunk: [mcrwind] update: * use gravitational potential rather than force, cleaning with clauses

--- problems/mcrwind/initproblem.F90
+++ problems/mcrwind/initproblem.F90
@@ -27,6 +27,15 @@
 !
 #include "piernik.h"
 #include "macros.h"
+#if defined GALAXY && defined SN_SRC
+#define SN_GALAXY
+#endif /* GALAXY && SN_SRC */
+#if defined COSM_RAYS_SOURCES || defined SN_GALAXY
+#define CRS_GALAXY
+#endif /* COSM_RAYS_SOURCES || SN_GALAXY */
+#if defined COSM_RAYS && defined SN_SRC
+#define CR_SN
+#endif /* COSM_RAYS && SN_SRC */
 
 module initproblem
 
@@ -35,13 +44,16 @@ module initproblem
 ! Written by: M. Hanasz, February 2006
 ! Modified by M.Hanasz for CR-driven dynamo
 
+   use constants, only: ndims
+
    implicit none
 
    private
    public :: read_problem_par, init_prob, problem_pointers
 
-   real :: d0, alpha, bxn,byn,bzn, amp_cr, beta_cr                           !< galactic disk specific parameters
+   real :: d0, alpha, bxn, byn, bzn, amp_cr, beta_cr                         !< galactic disk specific parameters
    real :: x0, y0, z0                                                        !< parameters for a single supernova exploding at t=0
+   real, dimension(ndims) :: b_n, sn_pos
 
    namelist /PROBLEM_CONTROL/  d0, bxn, byn, bzn, x0, y0, z0, alpha, amp_cr, beta_cr
 
@@ -51,12 +63,12 @@ contains
 
    subroutine problem_pointers
 
-      use gravity,    only: grav_accel
+!      use gravity,    only: grav_accel
       use user_hooks, only: problem_customize_solution
 
       implicit none
 
-      grav_accel                 => galactic_grav_accel
+!      grav_accel                 => galactic_grav_accel
       problem_customize_solution => supernovae_wrapper
 
    end subroutine problem_pointers
@@ -115,7 +127,13 @@ contains
 
       endif
 
-      if (user_grav) grav_pot_3d => my_grav_pot_3d
+      sn_pos = [x0,  y0,  z0 ]
+      b_n    = [bxn, byn, bzn]
+
+#ifdef GRAV
+!      if (user_grav) grav_pot_3d => my_grav_pot_3d
+      if (user_grav) grav_pot_3d => galactic_grav_pot_3d
+#endif /* GRAV */
 
    end subroutine read_problem_par
 
@@ -137,21 +155,31 @@ contains
 #endif /* SHEAR */
 #ifdef COSM_RAYS
       use initcosmicrays, only: gamma_crn, iarr_crn
+#ifdef CRS_GALAXY
+      use cr_data,        only: eCRSP, cr_table
+#endif /* CRS_GALAXY */
+#ifdef SN_GALAXY
+      use cr_data,        only: icr_H1, icr_C12
+      use domain,         only: dom
+      use snsources,      only: r_sn
+#endif /* SN_GALAXY */
 #endif /* COSM_RAYS */
 #ifdef GRAV
       use gravity,        only: grav_pot_3d
 #endif /* GRAV */
 #ifdef COSM_RAYS_SOURCES
-      use cr_data,        only: eCRSP, cr_sigma, cr_table, icr_N14, icr_O16
+      use cr_data,        only: cr_sigma, icr_N14, icr_O16
 #endif /* COSM_RAYS_SOURCES */
       implicit none
 
       class(component_fluid), pointer :: fl
       integer                         :: i, j, k
       real                            :: b0, csim2
-      real, dimension(3)              :: sn_pos
       type(cg_list_element),  pointer :: cgl
       type(grid_container),   pointer :: cg
+#ifdef SN_GALAXY
+      real                            :: decr, x1, x2, y1, y2, z1
+#endif /* SN_GALAXY */
 
 #ifdef COSM_RAYS_SOURCES
 ! really workaround for the gold
@@ -160,8 +188,6 @@ contains
 #endif /* COSM_RAYS_SOURCES */
 
       call grav_pot_3d
-
-      sn_pos = [x0,y0,z0]
 
 !   Secondary parameters
       fl => flind%ion
@@ -175,7 +201,9 @@ contains
          cg => cgl%cg
 
          call set_default_hsparams(cg)
-         call hydrostatic_zeq_densmid(1, 1, d0, csim2)
+         i = cg%lhn(xdim,LO)
+         j = cg%lhn(ydim,LO)
+         call hydrostatic_zeq_densmid(i, j, d0, csim2)
 
          do k = cg%lhn(zdim,LO), cg%lhn(zdim,HI)
             do j = cg%lhn(ydim,LO), cg%lhn(ydim,HI)
@@ -195,24 +223,17 @@ contains
 #ifdef COSM_RAYS
                   cg%u(iarr_crn,i,j,k)  = 0.0
                   cg%u(iarr_crn(1),i,j,k) = beta_cr*fl%cs2 * cg%u(fl%idn,i,j,k)/( gamma_crn(1) - 1.0 )
-!#ifdef GALAXY
-!! Single SN explosion in x0,y0,z0 at t = 0 if amp_cr /= 0
-!
-!
-!               cg%u(iarr_crn(icr_H1),i,j,k)= cg%u(iarr_crn(icr_H1),i,j,k) &
-!                     + amp_cr*exp(-((x(i)- x0    )**2 + (y(j)- y0    )**2 + (z(k)-z0)**2)/r_sn**2) &
-!                     + amp_cr*exp(-((x(i)-(x0+Lx))**2 + (y(j)- y0    )**2 + (z(k)-z0)**2)/r_sn**2) &
-!                     + amp_cr*exp(-((x(i)- x0    )**2 + (y(j)-(y0+Ly))**2 + (z(k)-z0)**2)/r_sn**2) &
-!                     + amp_cr*exp(-((x(i)-(x0+Lx))**2 + (y(j)-(y0+Ly))**2 + (z(k)-z0)**2)/r_sn**2)
-!
-!               cg%u(iarr_crn(icr_C12),i,j,k)= cg%u(iarr_crn(icr_C12),i,j,k) &
-!                     + 0.1*amp_cr*exp(-((x(i)- x0    )**2 + (y(j)- y0    )**2 + (z(k)-z0)**2)/r_sn**2) &
-!                     + 0.1*amp_cr*exp(-((x(i)-(x0+Lx))**2 + (y(j)- y0    )**2 + (z(k)-z0)**2)/r_sn**2) &
-!                     + 0.1*amp_cr*exp(-((x(i)- x0    )**2 + (y(j)-(y0+Ly))**2 + (z(k)-z0)**2)/r_sn**2) &
-!                     + 0.1*amp_cr*exp(-((x(i)-(x0+Lx))**2 + (y(j)-(y0+Ly))**2 + (z(k)-z0)**2)/r_sn**2)
-!!
-!
-!#endif /* GALAXY */
+#ifdef SN_GALAXY
+! Single SN explosion in x0,y0,z0 at t = 0 if amp_cr /= 0
+                  if (any([eCRSP(icr_H1), eCRSP(icr_C12)])) then
+                     x1 = (cg%x(i)- x0)**2 ; x2 = (cg%x(i)-(x0+dom%L_(xdim)))**2
+                     y1 = (cg%y(j)- y0)**2 ; y2 = (cg%y(j)-(y0+dom%L_(ydim)))**2
+                     z1 = (cg%z(k)- z0)**2
+                     decr = amp_cr * (exp(-(x1 + y1 + z1)/r_sn**2) + exp(-(x2 + y1 + z1)/r_sn**2) + exp(-(x1 + y2 + z1)/r_sn**2) + exp(-(x2 + y2 + z1)/r_sn**2))
+                  endif
+                  if (eCRSP(icr_H1 )) cg%u(iarr_crn(cr_table(icr_H1 )),i,j,k)= cg%u(iarr_crn(cr_table(icr_H1 )),i,j,k) +     decr
+                  if (eCRSP(icr_C12)) cg%u(iarr_crn(cr_table(icr_C12)),i,j,k)= cg%u(iarr_crn(cr_table(icr_C12)),i,j,k) + 0.1*decr
+#endif /* SN_GALAXY */
 #endif /* COSM_RAYS */
                enddo
             enddo
@@ -221,11 +242,9 @@ contains
          cgl => cgl%nxt
       enddo
 
-#ifdef COSM_RAYS
-
+#ifdef CR_SN
       call cr_sn_beware(sn_pos)
-
-#endif /* COSM_RAYS */
+#endif /* CR_SN */
 
 
       cgl => leaves%first
@@ -235,11 +254,9 @@ contains
          do k = cg%lhn(zdim,LO), cg%lhn(zdim,HI)
             do j = cg%lhn(ydim,LO), cg%lhn(ydim,HI)
                do i = cg%lhn(xdim,LO), cg%lhn(xdim,HI)
-                  cg%b(xdim,i,j,k)   = b0*sqrt(cg%u(fl%idn,i,j,k)/d0)* bxn/sqrt(bxn**2+byn**2+bzn**2)
-                  cg%b(ydim,i,j,k)   = b0*sqrt(cg%u(fl%idn,i,j,k)/d0)* byn/sqrt(bxn**2+byn**2+bzn**2)
-                  cg%b(zdim,i,j,k)   = b0*sqrt(cg%u(fl%idn,i,j,k)/d0)* bzn/sqrt(bxn**2+byn**2+bzn**2)
+                  cg%b(:,i,j,k) = b0*sqrt(cg%u(fl%idn,i,j,k)/d0) * b_n/sqrt(sum(b_n**2))
 #ifndef ISO
-                  cg%u(fl%ien,i,j,k)   = cg%u(fl%ien,i,j,k) + emag(cg%b(xdim,i,j,k), cg%b(ydim,i,j,k), cg%b(zdim,i,j,k))
+                  cg%u(fl%ien,i,j,k) = cg%u(fl%ien,i,j,k) + emag(cg%b(xdim,i,j,k), cg%b(ydim,i,j,k), cg%b(zdim,i,j,k))
 #endif /* !ISO */
                enddo
             enddo
@@ -251,15 +268,15 @@ contains
    end subroutine init_prob
 
    subroutine supernovae_wrapper(forward)
-
+#ifdef SN_SRC
       use snsources, only: random_sn
-
+#endif /* SN_SRC */
       implicit none
 
       logical, intent(in) :: forward
-
+#ifdef SN_SRC
       if (forward) call random_sn
-
+#endif /* SN_SRC */
    end subroutine supernovae_wrapper
 
    subroutine my_grav_pot_3d
@@ -330,8 +347,78 @@ contains
       endif
 
    end subroutine galactic_grav_accel
+!------------------------------------------------------------------------------
+
+   subroutine galactic_grav_pot_3d
+
+      use axes_M,    only: axes
+      use cg_leaves, only: leaves
+      use cg_list,   only: cg_list_element
+      use gravity,   only: grav_type
+      use grid_cont, only: grid_container
+
+      implicit none
+
+      type(axes)                     :: ax
+      type(cg_list_element), pointer :: cgl
+      type(grid_container),  pointer :: cg
+
+      cgl => leaves%first
+      do while (associated(cgl))
+         cg => cgl%cg
+         call ax%allocate_axes(cg%lhn)
+         ax%x(:) = cg%x(:)
+         ax%y(:) = cg%y(:)
+         ax%z(:) = cg%z(:)
+
+         call galactic_grav_pot(cg%gp, ax, cg%lhn)
+         grav_type => galactic_grav_pot
+
+         call ax%deallocate_axes
+
+         cgl => cgl%nxt
+      enddo
+
+   end subroutine galactic_grav_pot_3d
+
+   subroutine galactic_grav_pot(gp, ax, lhn, flatten)
+
+      use axes_M,    only: axes
+      use constants, only: ndims, LO, HI, zdim, half
+      use gravity,   only: r_gc
+      use units,     only: r_gc_sun, kpc
+
+      implicit none
+
+      real, dimension(:,:,:), pointer                     :: gp
+      type(axes),                              intent(in) :: ax
+      integer(kind=4), dimension(ndims,LO:HI), intent(in) :: lhn
+      logical,                       optional, intent(in) :: flatten
+      integer                                             :: k
+
+      real, parameter :: f1 = 3.23e8, f2 = -4.4e-9, f3 = 1.7e-9
+      real            :: r1, r22, r32, s4, s5
+
+      r1  = 4.9*kpc
+      r22 = (0.2*kpc)**2
+      r32 = (2.2*kpc)**2
+      s4  = f2 * exp(-(r_gc-r_gc_sun)/(r1))
+      s5  = f3 * (r_gc_sun**2 + r32)/(r_gc**2 + r32)
+
+!      grav = f1 * ((s4 * xsw/sqrt(xsw**2+r22)) - (s5 * xsw/kpc) )
+!!          -Om*(Om+G) * Z * (kpc ?) ! in the transition region between rigid and flat rotation F'98: eq.(36)
+
+      do k = lhn(zdim,LO), lhn(zdim,HI)
+         gp(:,:,k) = -f1 * (s4 * sqrt(ax%z(k)**2+r22) - s5 * half * ax%z(k)**2 / kpc)
+      enddo
+      return
+
+      if (.false. .and. present(flatten)) k = 0 ! suppress compiler warnings
+
+   end subroutine galactic_grav_pot
 
 !------------------------------------------------------------------------------
+#ifdef CR_SN
 !BEWARE!
 !>
 !! \brief Routine that inserts an amount of cosmic ray energy around the position of supernova
@@ -346,10 +433,12 @@ contains
       use cg_list,        only: cg_list_element
       use cg_leaves,      only: leaves
       use constants,      only: xdim, ydim, zdim, ndims, LO, HI
-      use cr_data,        only: cr_table, cr_primary, eCRSP, icr_H1, icr_C12 !, icr_N14, icr_O16
       use domain,         only: dom
       use grid_cont,      only: grid_container
+#ifdef COSM_RAYS_SOURCES
+      use cr_data,        only: cr_table, cr_primary, eCRSP, icr_H1, icr_C12 !, icr_N14, icr_O16
       use initcosmicrays, only: iarr_crn
+#endif /* COSM_RAYS_SOURCES */
       use snsources,      only: r_sn
 #ifdef SHEAR
       use snsources,      only: sn_shear
@@ -400,13 +489,14 @@ contains
                      enddo ! jpm
                   enddo ! ipm
                   decr = decr * amp_cr
+#ifdef COSM_RAYS_SOURCES
 !                     cg%u(iarr_crn,i,j,k) = cg%u(iarr_crn,i,j,k) + max(decr,1e-10) * [1., primary_C12*12., primary_N14*14., primary_O16*16.]
                   if (eCRSP(icr_H1 )) cg%u(iarr_crn(cr_table(icr_H1 )),i,j,k) = cg%u(iarr_crn(cr_table(icr_H1 )),i,j,k) + decr
                   if (eCRSP(icr_C12)) cg%u(iarr_crn(cr_table(icr_C12)),i,j,k) = cg%u(iarr_crn(cr_table(icr_C12)),i,j,k) + cr_primary(cr_table(icr_C12))*12*decr
                   !> \deprecated BEWARE: following lines are inconsistent with the gold for some reason
 !                  if (eCRSP(icr_N14)) cg%u(iarr_crn(cr_table(icr_N14)),i,j,k) = cg%u(iarr_crn(cr_table(icr_N14)),i,j,k) + cr_primary(cr_table(icr_N14))*14*decr
 !                  if (eCRSP(icr_O16)) cg%u(iarr_crn(cr_table(icr_O16)),i,j,k) = cg%u(iarr_crn(cr_table(icr_O16)),i,j,k) + cr_primary(cr_table(icr_O16))*16*decr
-
+#endif /* COSM_RAYS_SOURCES */
                enddo ! i
             enddo ! j
          enddo ! k
@@ -414,5 +504,5 @@ contains
       enddo
 
    end subroutine cr_sn_beware
-
+#endif /* CR_SN */
 end module initproblem
