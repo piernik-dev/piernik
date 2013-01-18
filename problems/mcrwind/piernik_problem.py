920f9aebfa10c555d05dc520fc8557ec85737b82
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2013-01-18 07:54:25 +0000

    trunk: [mcrwind] add python goodies for validation test

---
+++ problems/mcrwind/piernik_problem.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import sys
+import numpy as np
+from yt.mods import load as yt_load
+from pylab import *
+
+THRESHOLD = 1e-9
+FIELD = "cr1"
+
+
+def plot_diff(pf1, pf2, data1, data2, field):
+    wd = pf1.domain_width
+    n_d = pf1.domain_dimensions
+    ext = np.array([pf1.domain_left_edge[1], pf1.domain_right_edge[1],
+                    pf1.domain_left_edge[2], pf1.domain_right_edge[2]])
+    ext *= pf1['pc']
+    img1 = data1.to_frb(wd[1], (n_d[2] * 10, n_d[1] * 10),
+                        center=np.array([0, 0, 0]), height=wd[2])
+    img2 = data2.to_frb(wd[1], (n_d[2] * 10, n_d[1] * 10),
+                        center=np.array([0, 0, 0]), height=wd[2])
+    diff = (img2[field] - img1[field]) / (img1[field] + THRESHOLD)
+    v = abs(diff).max()
+    figure(1, (6, 8))
+    imshow(diff, vmin=-v, vmax=v, extent=ext, cmap='RdBu')
+    bar = colorbar()
+    bar.ax.set_xlabel(
+        r"$\frac{\rm{%s}^{\rm{new}} - \rm{%s}^{\rm{old}}}{\rm{%s}^{\rm{old}}}$" % (field, field, field))
+    draw()
+    xlabel('y [pc]')
+    ylabel('z [pc]')
+    savefig('diff.png')
+
+
+if len(sys.argv) != 3:
+    print("Wrong number of arguments!")
+    sys.exit(-1)
+PF1 = yt_load(sys.argv[1])
+PF2 = yt_load(sys.argv[2])
+
+DATA1 = PF1.h.slice(1, 0.0, fields=[FIELD])
+DATA2 = PF2.h.slice(1, 0.0, fields=[FIELD])
+
+if not PF1.h.field_list == PF2.h.field_list:
+    print("Fields in files differ!")
+    sys.exit(-1)
+
+for field in PF1.h.field_list:
+    if abs(DATA1[field] - DATA2[field]).max() >= THRESHOLD:
+        print("Field %s differs" % field)
+        plot_diff(PF1, PF2, DATA1, DATA2, field)
+        sys.exit(-1)
+
+figure(1, (8,6))
+draw()
+savefig('diff.png')
