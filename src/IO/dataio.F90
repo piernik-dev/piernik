85958250463baa0d2e5c67dc8dab3caf01599307
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2013-01-18 10:49:06 +0000

    trunk: [dataio:write_timeslice] properly print E descriptor for huge/tiny numbers

--- src/IO/dataio.F90
+++ src/IO/dataio.F90
@@ -950,7 +950,7 @@ contains
       if (associated(user_tsl)) call user_tsl(tsl_vars)
 
       if (master) then
-         write(tsl_lun, '(1x,i8,100(1x,es15.8))') nstep, tsl_vars
+         write(tsl_lun, '(1x,i8,100(1x,es17.8e3))') nstep, tsl_vars
          ! some quantities computed in "write_log".One can add more, or change.
          deallocate(tsl_vars)
       endif
