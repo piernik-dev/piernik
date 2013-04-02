a90ac14f4cddf1672d834d82434682376ef42a10
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2013-04-02 17:58:26 +0000

    trunk: [mcrwind:piernik_problem] slice through proper axis, plot unnormalized diff

--- problems/mcrwind/piernik_problem.py
+++ problems/mcrwind/piernik_problem.py
@@ -10,6 +10,17 @@ from pylab import *
 THRESHOLD = 1e-9
 FIELD = "cr1"
 
+def _myplot(diff, fname, ext, clbl):
+    v = abs(diff).max()
+    figure(1, (6, 8))
+    imshow(diff, vmin=-v, vmax=v, extent=ext, cmap='RdBu')
+    bar = colorbar()
+    bar.ax.set_xlabel(clbl)
+    draw()
+    xlabel('y [pc]')
+    ylabel('z [pc]')
+    savefig(fname)
+    clf()
 
 def plot_diff(pf1, pf2, data1, data2, field):
     wd = pf1.domain_width
@@ -21,17 +32,14 @@ def plot_diff(pf1, pf2, data1, data2, field):
                         center=np.array([0, 0, 0]), height=wd[2])
     img2 = data2.to_frb(wd[1], (n_d[2] * 10, n_d[1] * 10),
                         center=np.array([0, 0, 0]), height=wd[2])
-    diff = (img2[field] - img1[field]) / (img1[field] + THRESHOLD)
-    v = abs(diff).max()
-    figure(1, (6, 8))
-    imshow(diff, vmin=-v, vmax=v, extent=ext, cmap='RdBu')
-    bar = colorbar()
-    bar.ax.set_xlabel(
-        r"$\frac{\rm{%s}^{\rm{new}} - \rm{%s}^{\rm{old}}}{\rm{%s}^{\rm{old}}}$" % (field, field, field))
-    draw()
-    xlabel('y [pc]')
-    ylabel('z [pc]')
-    savefig('diff.png')
+    diff = (img2[field] - img1[field])
+    clbl = \
+        r"$\rm{%s}^{\rm{new}} - \rm{%s}^{\rm{old}}$" % (field, field)
+    _myplot(diff, 'diff_bare.png', ext, clbl)
+
+    clbl = \
+        r"$\frac{\rm{%s}^{\rm{new}} - \rm{%s}^{\rm{old}}}{\rm{%s}^{\rm{old}}}$" % (field, field, field)
+    _myplot(diff / (img1[field] + THRESHOLD), 'diff.png', ext, clbl)
 
 
 if len(sys.argv) != 3:
@@ -40,8 +48,9 @@ if len(sys.argv) != 3:
 PF1 = yt_load(sys.argv[1])
 PF2 = yt_load(sys.argv[2])
 
-DATA1 = PF1.h.slice(1, 0.0, fields=[FIELD])
-DATA2 = PF2.h.slice(1, 0.0, fields=[FIELD])
+axis = np.where(PF1.h.grids[0].ActiveDimensions == 1)[0][0]
+DATA1 = PF1.h.slice(axis, 0.0, fields=[FIELD])
+DATA2 = PF2.h.slice(axis, 0.0, fields=[FIELD])
 
 if not PF1.h.field_list == PF2.h.field_list:
     print("Fields in files differ!")
