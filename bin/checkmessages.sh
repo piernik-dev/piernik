3c28317667edd5930b0d89c456d64ffb1f1d7368
Artur Gawryszczak <gawrysz@gmail.com>
2013-06-08 21:17:40 +0000

    trunk: [checkmessages] cosmetics

--- bin/checkmessages.sh
+++ bin/checkmessages.sh
@@ -5,4 +5,4 @@ SEP="===================================================="
 for i in `find src -name "*.F90"` ; do
 	echo $SEP $i
 	grep -rn '\[[a-zA-Z_0-9]*:[a-zA-Z_0-9]*]' $i | grep -v $( basename ${i/.F90/} )":"
-done | awk '{ if ($1 !~"'$SEP'") {if (NR == lastsep+1) print "\n"fname; print} else {lastsep=NR; fname=$2}}'
+done | awk '{ if ($1 !~"'$SEP'") {if (NR == lastsep+1) print "\n"fname; print;} else {lastsep=NR; fname=$2;}}'
