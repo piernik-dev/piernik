dfc4ddaa633a022348fda751767aebafab00899e
Artur Gawryszczak <gawrysz@gmail.com>
2013-06-08 21:17:13 +0000

    trunk: [checkmessages] Print only what is important

--- bin/checkmessages.sh
+++ bin/checkmessages.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
+SEP="====================================================" 
+
 for i in `find src -name "*.F90"` ; do
-	echo "====================================================" $i
+	echo $SEP $i
 	grep -rn '\[[a-zA-Z_0-9]*:[a-zA-Z_0-9]*]' $i | grep -v $( basename ${i/.F90/} )":"
-done
+done | awk '{ if ($1 !~"'$SEP'") {if (NR == lastsep+1) print "\n"fname; print} else {lastsep=NR; fname=$2}}'
