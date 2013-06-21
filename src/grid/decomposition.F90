8c13f23ece841a2d6b9121a7c06d70d6031351c0
Artur Gawryszczak <gawrysz@gmail.com>
2013-06-21 15:23:09 +0200

    [decomposition] print more info on failsafe decomposition

--- src/grid/decomposition.F90
+++ src/grid/decomposition.F90
@@ -242,6 +242,11 @@ contains
          ml = maxloc(p_size(:), dim=1)
          if (p_size(ml) > 1) p_size(ml) = p_size(ml) - I_ONE
       enddo
+      if (master) then
+         write(msg,'(a,i3,a,3i4,a,f7.4)')"[decomposition:decompose_patch_int]        Level ",level_id,": grid divided to [",p_size(:), &
+              &                          " ] pieces, balance = ", product(p_size(:))/real(nproc) ! rough esitmate, this might be nonuniform decomposition
+         call printinfo(msg)
+      endif
       call patch%cartesian_tiling(p_size(:), product(p_size(:)), level_id)
       patch_divided = patch%is_not_too_small("decompose_patch_cartesian_less_than_nproc")
       if (patch_divided) return
