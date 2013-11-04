piernik-dev/piernik

commit 3ff38c0f25b6ed75c92c4cb128a50e237ecd6b12

[gravity] Call grav_pot_3d after each refinement change to update gp array. Use cg%is_old to update only new grids

src/base/piernik.F90
@@ -43,6 +43,7 @@ program piernik
    use fluidupdate,       only: fluid_update
    use func,              only: operator(.equals.)
    use global,            only: t, nstep, dt, dtm, cfl_violated
+   use gravity,           only: manage_grav_pot_3d
    use initpiernik,       only: init_piernik
    use list_of_cg_lists,  only: all_lists
    use mpisetup,          only: master, piernik_MPI_Barrier, piernik_MPI_Bcast
@@ -149,6 +150,9 @@ program piernik
 
       call user_msg_handler(end_sim)
       call update_refinement
+#ifdef GRAV
+      call manage_grav_pot_3d(.false., update_gp = .true.)
+#endif /* GRAV */
       if (try_rebalance) then
          !> \todo try to rewrite this ugly chain of flags passed through global variables into something more fool-proof
          call leaves%balance_and_update(" (re-balance) ")

src/gravity/gravity.F90
@@ -368,7 +368,7 @@ contains
 
    end subroutine g_cg_init
 
-   subroutine manage_grav_pot_3d(first_approach)
+   subroutine manage_grav_pot_3d(first_approach, update_gp)
 
       use dataio_pub, only: die
 #ifdef VERBOSE
@@ -377,16 +377,17 @@ contains
 
       implicit none
 
-      logical, intent(in) :: first_approach
+      logical, intent(in)           :: first_approach
+      logical, optional, intent(in) :: update_gp
 
       if (associated(grav_pot_3d)) then
-         if (first_approach .or. .not.grav_pot_3d_called) then
+         if (first_approach .or. .not. grav_pot_3d_called .or. update_gp) then
             call grav_pot_3d
             grav_pot_3d_called = .true.
          endif
       else
 #ifdef VERBOSE
-         if (first_approach) call warn("[gravity:manage_grav_pot_3d] grav_pot_3d is not associated! Will try to call it once more after problem_initial_conditionslem.")
+         if (first_approach) call warn("[gravity:manage_grav_pot_3d] grav_pot_3d is not associated! Will try to call it once more after problem_initial_conditions.")
 #endif /* VERBOSE */
          if (.not.(first_approach .or. grav_pot_3d_called)) call die("[gravity:manage_grav_pot_3d] grav_pot_3d failed for the 2nd time!")
       endif

problems/streaming_global/initproblem.F90
@@ -655,29 +655,26 @@ contains
 
       implicit none
 
-      logical, save                  :: frun = .true.
       real                           :: r2
       integer                        :: i, k
       type(cg_list_element), pointer :: cgl
       type(grid_container),  pointer :: cg
 
-      if (frun) then
-         cgl => leaves%first
-         do while (associated(cgl))
-            cg => cgl%cg
+      cgl => leaves%first
+      do while (associated(cgl))
+         cg => cgl%cg
 
+         if (.not. cg%is_old) then
             do i = cg%lhn(xdim, LO), cg%lhn(xdim, HI)
                do k = cg%lhn(zdim, LO), cg%lhn(zdim, HI)
                   r2 = cg%x(i)**2! + cg%z(k)**2
                   cg%gp(i,:,k) = -newtong*ptmass / sqrt(r2)
                enddo
             enddo
+         endif
 
-            cgl => cgl%nxt
-         enddo
-      endif
-
-      frun = .false.
+         cgl => cgl%nxt
+      enddo
 
    end subroutine my_grav_pot_3d
 !-----------------------------------------------------------------------------------------------------------------------

src/base/initpiernik.F90
@@ -248,7 +248,7 @@ contains
 
             call all_bnd !> \warning Never assume that problem_initial_conditions set guardcells correctly
 #ifdef GRAV
-            call manage_grav_pot_3d(.false.)
+            call manage_grav_pot_3d(.false., update_gp = (nit /= 0))
             call cleanup_hydrostatic
 #endif /* GRAV */
 

commit 4876e0f8c0e4d10e1408950ce6104703492604c6

BUGFIX: 3ff38c0 caused multigrid to be unnecessarily called

src/base/piernik.F90
@@ -43,7 +43,7 @@ program piernik
    use fluidupdate,       only: fluid_update
    use func,              only: operator(.equals.)
    use global,            only: t, nstep, dt, dtm, cfl_violated
-   use gravity,           only: manage_grav_pot_3d
+   use gravity,           only: update_gp
    use initpiernik,       only: init_piernik
    use list_of_cg_lists,  only: all_lists
    use mpisetup,          only: master, piernik_MPI_Barrier, piernik_MPI_Bcast
@@ -151,7 +151,7 @@ program piernik
       call user_msg_handler(end_sim)
       call update_refinement
 #ifdef GRAV
-      call manage_grav_pot_3d(.false., update_gp = .true.)
+      call update_gp
 #endif /* GRAV */
       if (try_rebalance) then
          !> \todo try to rewrite this ugly chain of flags passed through global variables into something more fool-proof

src/gravity/gravity.F90
@@ -43,7 +43,7 @@ module gravity
    implicit none
 
    private
-   public :: init_grav, init_grav_ext, grav_accel, source_terms_grav, grav_pot2accel, grav_pot_3d, grav_type, get_gprofs, grav_accel2pot, sum_potential, manage_grav_pot_3d
+   public :: init_grav, init_grav_ext, grav_accel, source_terms_grav, grav_pot2accel, grav_pot_3d, grav_type, get_gprofs, grav_accel2pot, sum_potential, manage_grav_pot_3d, update_gp
    public :: r_gc, ptmass, ptm_x, ptm_y, ptm_z, r_smooth, nsub, tune_zeq, tune_zeq_bnd, r_grav, n_gravr, user_grav, gprofs_target, ptm2_x
 
    integer, parameter         :: gp_stat_len   = 9
@@ -443,6 +443,19 @@ contains
       call sum_potential
 
    end subroutine source_terms_grav
+
+!> \brief update static potential (gp field) in case of grid changes. Assume multigrid has been called
+
+   subroutine update_gp
+
+      implicit none
+
+      if (associated(grav_pot_3d)) then
+         call grav_pot_3d
+         call sum_potential
+      endif
+
+   end subroutine update_gp
 
    subroutine sum_potential
 