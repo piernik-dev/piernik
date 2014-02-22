3e35cdd5511a50e4dfd67bc17061ac0571205c2c
Andrzej Skalbania <misteramper@gmail.com>
2014-02-22 04:23:10 +0100

    2 particles

--- problems/2body/initproblem.F90
+++ problems/2body/initproblem.F90
@@ -67,6 +67,7 @@ contains
       implicit none
 
       integer                         :: i, j, k, p
+      logical, save                   :: first_run = .true.
       type(cg_list_element),  pointer :: cgl
 
       do p = lbound(flind%all_fluids, dim=1), ubound(flind%all_fluids, dim=1)
@@ -90,14 +91,15 @@ contains
          enddo
       enddo
 
-      !call pset%add(1.0, [ 0.0,  0.0,  0.0], [ 0.0,  0.0, 0.0])
-      !call pset%add(1.0, [ 2.0,  0.0,  0.0], [ 0.0,  0.21294617, 0.0])
-!      call pset%add(1.0, [ 0.0,         0.0,         0.0], [-0.932407370, -0.86473146, 0.0])
-      !stare
-      call pset%add(1.0, [ 0.9700436,  -0.24308753,  0.0], [ 0.466203685,  0.43236573, 0.0])
-      call pset%add(1.0, [-0.9700436,   0.24308753,  0.0], [ 0.466203685,  0.43236573, 0.0])
-      call pset%add(1.0, [ 0.0,         0.0,         0.0], [-0.932407370, -0.86473146, 0.0])
-      call printinfo('To see results type: gnuplot -p -e ''plot "nbody_out.log" u 2:3'' ')
+      if (first_run) then
+         !call pset%add(1.0, [ 0.9700436,  -0.24308753,  0.0], [ 0.466203685,  0.43236573, 0.0])
+         !call pset%add(1.0, [-0.9700436,   0.24308753,  0.0], [ 0.466203685,  0.43236573, 0.0])
+         !call pset%add(1.0, [ 0.0,         0.0,         0.0], [-0.932407370, -0.86473146, 0.0])
+         call pset%add(1.0, [ 2.0,         0.0,         0.0], [0.0, 0.21294, 0.0])
+         call pset%add(100.0, [ 0.0,         0.0,         0.0], [0.0, 0.0, 0.0])
+         call printinfo('To see results type: gnuplot -p -e ''plot "nbody_out.log" u 2:3'' ')
+         first_run = .false.
+      endif
 
    end subroutine problem_initial_conditions
 !-----------------------------------------------------------------------------
