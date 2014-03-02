6953ddfb90f84dc8cb2ba954adcfa48db81235e7
Andrzej Skalbania <misteramper@gmail.com>
2014-03-02 22:22:20 +0100

    3body problem.

--- problems/2body/initproblem.F90
+++ problems/2body/initproblem.F90
@@ -92,12 +92,12 @@ contains
       enddo
 
       if (first_run) then
-         !call pset%add(1.0, [ 0.9700436,  -0.24308753,  0.0], [ 0.466203685,  0.43236573, 0.0])
-         !call pset%add(1.0, [-0.9700436,   0.24308753,  0.0], [ 0.466203685,  0.43236573, 0.0])
-         !call pset%add(1.0, [ 0.0,         0.0,         0.0], [-0.932407370, -0.86473146, 0.0])
-         call pset%add(0.0001, [ 2.0,         0.0,         0.0], [0.0, 0.81294617, 0.0])
-         call pset%add(10.0, [ 0.0,         0.0,         0.0], [0.0, 0.0, 0.0])
-         call printinfo('To see results type: gnuplot -p -e ''plot "nbody_out.log" u 2:3'' ')
+         call pset%add(1.0, [ 0.9700436,  -0.24308753,  0.0], [ 0.466203685,  0.43236573, 0.0])
+         call pset%add(1.0, [-0.9700436,   0.24308753,  0.0], [ 0.466203685,  0.43236573, 0.0])
+         call pset%add(1.0, [ 0.0,         0.0,         0.0], [-0.932407370, -0.86473146, 0.0])
+         !call pset%add(0.0001, [ 2.0,         0.0,         0.0], [0.0, 0.81294617, 0.0])
+         !call pset%add(10.0, [ 0.0,         0.0,         0.0], [0.0, 0.0, 0.0])
+         !call printinfo('To see results type: gnuplot -p -e ''plot "nbody_out.log" u 2:3'' ')
          first_run = .false.
       endif
 
