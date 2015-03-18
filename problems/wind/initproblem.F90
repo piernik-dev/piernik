71d4058300af3c5e52b92a2759055caf8d6a34ae
Miguel de Val-Borro <miguel@archlinux.net>
2015-03-18 14:19:32 -0400

    remove unused module variables

--- problems/wind/initproblem.F90
+++ problems/wind/initproblem.F90
@@ -31,9 +31,7 @@ contains
 
    subroutine read_problem_par
 
-      use constants,  only: xdim
       use dataio_pub, only: nh      ! QA_WARN required for diff_nml
-      use domain,     only: dom
       use mpisetup,   only: rbuff, master, slave, piernik_MPI_Bcast
 
       implicit none
