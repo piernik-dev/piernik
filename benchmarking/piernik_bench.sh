01762a1d49f3a4c1c57b400bf5e4ea21e29bfddc
Artur Gawryszczak <gawrysz@gmail.com>
2016-04-02 13:00:27 +0200

    [benchmarking] timed make

--- benchmarking/piernik_bench.sh
+++ benchmarking/piernik_bench.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# create list of thread count to be tested
 if [ $# -lt 1 ] ; then
     N=$( awk 'BEGIN {c=0} /processor/ {if ($NF > c) c=$NF} END {print c+1}' /proc/cpuinfo )
     N_PROC_LIST=$( seq $N ) 
@@ -15,3 +16,56 @@ for i in $N_PROC_LIST ; do
     fi
 done
 
+TIMEFORMAT='real/user/sys/CPU%%: %1R %1U %1S %P%%'
+MAKE=make
+
+# some cleanup
+PROBLEM_LIST="maclaurin advection_test crtest jeans tearing sedov otvortex 3body"
+rm -rf obj_B_*
+for i in $PROBLEM_LIST; do
+    rm -rf runs/${i}_B_${i}
+done
+
+{
+# create object and run directories
+echo -n "Preparing objects "
+SETUP_PARAMS="-c ../benchmarking/benchmarking -n"
+time for i in $PROBLEM_LIST; do
+    ./setup $i $SETUP_PARAMS -o "B_"$i > /dev/null
+done
+
+get_n_problems() {
+    echo $1 $PROBLEM_LIST | awk '{for (i=0; i<$1; i++) printf("obj_B_%s ",$(2+i)); print ""}'
+}
+
+#
+# Benchmarking: make objects
+#
+
+OBJ_LIST=$( get_n_problems 1 )
+
+echo -n "Single-thread make object "
+time $MAKE $OBJ_LIST > /dev/null
+$MAKE $OBJ_LIST CL=1 > /dev/null
+
+echo -n "Multi-thread make object "
+time $MAKE -j $OBJ_LIST > /dev/null
+$MAKE $OBJ_LIST CL=1 > /dev/null
+
+OBJ_LIST=$( get_n_problems 2 )
+
+echo -n "Multi-thread make two objects "
+time $MAKE -j $OBJ_LIST > /dev/null
+$MAKE $OBJ_LIST CL=1 > /dev/null
+
+OBJ_LIST=$( get_n_problems 4 )
+
+echo -n "Multi-thread make four objects "
+time $MAKE -j $OBJ_LIST > /dev/null
+$MAKE $OBJ_LIST CL=1 > /dev/null
+
+OBJ_LIST=$( get_n_problems 8 )
+
+echo -n "Multi-thread make eight objects "
+time $MAKE -j $OBJ_LIST > /dev/null
+} 2>&1 | $( dirname $0 )"/pretty_time_form.awk"
