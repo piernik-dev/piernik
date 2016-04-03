4f7765819b0012c7f24e49b13b398e9bbe05a10e
Artur Gawryszczak <gawrysz@gmail.com>
2016-04-04 00:58:43 +0200

    [benchmarking/piernik_bench.sh] allow to skip make when execs are in place

--- benchmarking/piernik_bench.sh
+++ benchmarking/piernik_bench.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#check if we're allowed to skip making and want to just run existing piernik
+DO_MAKE=1
+if [ $# -ge 1 ] ; then
+    if [ $1 == "-fast" -o $1 == "-f" ] ; then
+	DO_MAKE=0
+	shift
+    fi
+fi
+
 # create list of thread count to be tested
 if [ $# -lt 1 ] ; then
     N=$( awk 'BEGIN {c=0} /processor/ {if ($NF > c) c=$NF} END {print c+1}' /proc/cpuinfo )
@@ -19,63 +28,73 @@ done
 TIMEFORMAT='real/user/sys/CPU%%: %1R %1U %1S %P%%'
 MAKE=make
 
+B_PROBLEM_LIST="sedov crtest maclaurin"
+if [ $DO_MAKE == 0 ] ; then
+    for p in $B_PROBLEM_LIST ; do
+	[ -x runs/${p}_B_${p}/piernik ] || DO_MAKE=1
+    done
+fi
+
 # some cleanup
 PROBLEM_LIST="maclaurin advection_test crtest jeans tearing sedov otvortex 3body"
-rm -rf obj_B_*
-for i in $PROBLEM_LIST; do
-    rm -rf runs/${i}_B_${i}
-done
 
-{
-# create object and run directories
-echo -n "Preparing objects "
-SETUP_PARAMS="-c ../benchmarking/benchmarking -n"
-time for i in $PROBLEM_LIST; do
-    ./setup $i $SETUP_PARAMS -o "B_"$i > /dev/null
-done
+if [ $DO_MAKE == 1 ] ; then
+    rm -rf obj_B_*
+    for i in $PROBLEM_LIST; do
+	rm -rf runs/${i}_B_${i}
+    done
 
-get_n_problems() {
-    echo $1 $PROBLEM_LIST | awk '{for (i=0; i<$1; i++) printf("obj_B_%s ",$(2+i)); print ""}'
-}
+    {
+	# create object and run directories
+	echo -n "Preparing objects "
+	SETUP_PARAMS="-c ../benchmarking/benchmarking -n"
+	time for i in $PROBLEM_LIST; do
+	./setup $i $SETUP_PARAMS -o "B_"$i > /dev/null
+	done
 
-#
-# Benchmarking: make objects
-#
+	get_n_problems() {
+	    echo $1 $PROBLEM_LIST | awk '{for (i=0; i<$1; i++) printf("obj_B_%s ",$(2+i)); print ""}'
+	}
 
-OBJ_LIST=$( get_n_problems 1 )
+	#
+	# Benchmarking: make objects
+	#
 
-echo -n "Single-thread make object "
-time $MAKE $OBJ_LIST > /dev/null
-$MAKE $OBJ_LIST CL=1 > /dev/null
+	OBJ_LIST=$( get_n_problems 1 )
 
-echo -n "Multi-thread make object "
-time $MAKE -j $OBJ_LIST > /dev/null
-$MAKE $OBJ_LIST CL=1 > /dev/null
+	echo -n "Single-thread make object "
+	time $MAKE $OBJ_LIST > /dev/null
+	$MAKE $OBJ_LIST CL=1 > /dev/null
 
-OBJ_LIST=$( get_n_problems 2 )
+	echo -n "Multi-thread make object "
+	time $MAKE -j $OBJ_LIST > /dev/null
+	$MAKE $OBJ_LIST CL=1 > /dev/null
 
-echo -n "Multi-thread make two objects "
-time $MAKE -j $OBJ_LIST > /dev/null
-$MAKE $OBJ_LIST CL=1 > /dev/null
+	OBJ_LIST=$( get_n_problems 2 )
 
-OBJ_LIST=$( get_n_problems 4 )
+	echo -n "Multi-thread make two objects "
+	time $MAKE -j $OBJ_LIST > /dev/null
+	$MAKE $OBJ_LIST CL=1 > /dev/null
 
-echo -n "Multi-thread make four objects "
-time $MAKE -j $OBJ_LIST > /dev/null
-$MAKE $OBJ_LIST CL=1 > /dev/null
+	OBJ_LIST=$( get_n_problems 4 )
 
-OBJ_LIST=$( get_n_problems 8 )
+	echo -n "Multi-thread make four objects "
+	time $MAKE -j $OBJ_LIST > /dev/null
+	$MAKE $OBJ_LIST CL=1 > /dev/null
 
-echo -n "Multi-thread make eight objects "
-time $MAKE -j $OBJ_LIST > /dev/null
-} 2>&1 | $( dirname $0 )"/pretty_time_form.awk"
-echo
+	OBJ_LIST=$( get_n_problems 8 )
+
+	echo -n "Multi-thread make eight objects "
+	time $MAKE -j $OBJ_LIST > /dev/null
+    } 2>&1 | $( dirname $0 )"/pretty_time_form.awk"
+    echo
+fi
 
 #
 # Benchmarking: Piernik
 #
 
-for p in sedov crtest maclaurin ; do
+for p in $B_PROBLEM_LIST ; do
     for t in weak strong ; do
 	echo "Benchmarking $p, $t scaling"
 	(
