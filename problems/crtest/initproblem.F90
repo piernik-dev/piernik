9cd1520d39c6975557b314beee649f2855e65cae
Artur Gawryszczak <gawrysz@gmail.com>
2016-05-30 14:34:01 +0200

    [crtest] BUGFIX: fixed coefficient computation for analytical field after restart

--- problems/crtest/initproblem.F90
+++ problems/crtest/initproblem.F90
@@ -68,7 +68,7 @@ contains
    subroutine read_problem_par
 
       use cg_list_global, only: all_cg
-      use constants,      only: I_ONE, I_TEN, AT_NO_B
+      use constants,      only: xdim, ydim, zdim, I_ONE, I_TEN, AT_NO_B
       use dataio_pub,     only: nh      ! QA_WARN required for diff_nml
       use dataio_pub,     only: die
       use domain,         only: dom
@@ -110,6 +110,10 @@ contains
          write(nh%lun,nml=PROBLEM_CONTROL)
          close(nh%lun)
          call nh%compare_namelist()
+
+         if (.not.dom%has_dir(xdim)) bx0 = 0. ! ignore B field in nonexistent direction to match the analytical solution
+         if (.not.dom%has_dir(ydim)) by0 = 0.
+         if (.not.dom%has_dir(zdim)) bz0 = 0.
 
          rbuff(1)  = d0
          rbuff(2)  = p0
@@ -193,10 +197,6 @@ contains
 ! Uniform equilibrium state
 
       cs_iso = sqrt(p0/d0)
-
-      if (.not.dom%has_dir(xdim)) bx0 = 0. ! ignore B field in nonexistent direction to match the analytical solution
-      if (.not.dom%has_dir(ydim)) by0 = 0.
-      if (.not.dom%has_dir(zdim)) bz0 = 0.
 
       cgl => leaves%first
       do while (associated(cgl))
