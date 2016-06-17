b4f92763119b1847d1e1fa99beaa5792661b9274
Mateusz Ogrodnik <502260@doktorant.umk.pl>
2016-06-17 14:12:28 +0200

    Detecting i_lo and i_up works properly

--- src/fluids/cosmicrays/p_diffusion.F90
+++ src/fluids/cosmicrays/p_diffusion.F90
@@ -13,75 +13,76 @@ contains
 
   subroutine cresp_p_cut_diff
   use initcosmicrays, only: iarr_cre, iarr_cre_pl, iarr_cre_pu
+  use initcrspectrum, only: ncre
   use cg_leaves,        only: leaves
   use cg_list,          only: cg_list_element
   use fluidtypes,       only: var_numbers
   use grid_cont,        only: grid_container
+  use domain,  only: dom
+  use constants, only: I_ZERO, I_ONE
   implicit none
   integer       :: i,j,k!, id, jd, kd
-  integer       :: i_lo, i_up
    type(grid_container), pointer   :: cg
    type(cg_list_element), pointer  :: cgl
- 
+   integer(kind=4),allocatable, dimension(:,:,:)  :: i_lo_dom !dimension(dom%n_d(1),dom%n_d(2),dom%n_d(3))
+   integer(kind=4),allocatable, dimension(:,:,:)  :: i_up_dom !dimension(dom%n_d(1),dom%n_d(2),dom%n_d(3))
+   
+   allocate(i_lo_dom(0:dom%n_d(1)-I_ONE,0:dom%n_d(2)-I_ONE,0:dom%n_d(3)-I_ONE))
+   allocate(i_up_dom(0:dom%n_d(1)-I_ONE,0:dom%n_d(2)-I_ONE,0:dom%n_d(3)-I_ONE))
+
+   i_lo_dom(:,:,:) = I_ZERO
+   i_up_dom(:,:,:) = I_ZERO
+
       cgl => leaves%first
+      
       do while (associated(cgl))
          cg => cgl%cg
          do k = cg%ks, cg%ke
            do j = cg%js, cg%je
               do i = cg%is, cg%ie
-                i_lo = i_lo_detect(cg%u(iarr_cre_pl,i,j,k))
-                i_up = i_up_detect(cg%u(iarr_cre_pu,i,j,k))
+                i_lo_dom(i,j,k) = i_lo_detect(cg%u(iarr_cre_pl,i,j,k))
+                i_up_dom(i,j,k) = i_up_detect(cg%u(iarr_cre_pu,i,j,k))
 ! #ifdef VERBOSE                
-!                 print *,'@p_diffusion(', i,j,k, '): i_lo = ', i_lo , ', i_up = ,', i_up 
+!                 print *,'@p_diffusion(', i,j,k, '): i_lo = ', cg%u(iarr_cre_pl,i,j,k), i_lo_dom(i,j,k) , ', i_up = ,', cg%u(iarr_cre_pu,i,j,k), i_up_dom(i,j,k)
 ! #endif /* VERBOSE */
               enddo
            enddo
          enddo
-         
       cgl => cgl%nxt  
       enddo
+  if(allocated(i_lo_dom)) deallocate(i_lo_dom)
+  if(allocated(i_up_dom)) deallocate(i_up_dom)
   
   end subroutine cresp_p_cut_diff
   
 !------------------------------------------  
   function i_lo_detect(p_lo)
-  use initcrspectrum, only: ncre, p_fix
+  use initcrspectrum, only: ncre, p_fix, w
   implicit none
    real(kind=8), intent(in) :: p_lo
-   integer   :: i
    integer(kind=4)   :: i_lo_detect
-  
-  i = 1
-  do while (i .le. ncre)
-      i_lo_detect = i
-!       print *, 'p_lo', i, p_lo, p_fix(i)
-      if ( p_lo .lt. p_fix(i)) exit ! checked and works properly
-      i = i + I_ONE
+! We are intrested in bin number in this case, so (*) is increased by one.
+   
+    i_lo_detect = int(floor(log10(p_lo/p_fix(1))/w)) + 1
+    i_lo_detect = max(0, i_lo_detect)
+    i_lo_detect = min(i_lo_detect, ncre - 1) + 1  ! (*)
+!     print *,'iul = ', i_lo_detect, int(floor(log10(p_lo/p_fix(1))/w))
 
-  enddo
-  
-  
-!   return p_up_ind
   end function i_lo_detect
  
 !------------------------------------------
   function i_up_detect(p_up)
-  use initcrspectrum, only: ncre, p_fix
-  use constants, only: I_ONE
+  use initcrspectrum, only: ncre, p_fix, w
+!   use constants, only: I_ONE
   implicit none 
    real(kind=8), intent(in) :: p_up
-   integer   :: i
    integer(kind=4)   :: i_up_detect
+
+    i_up_detect = int(floor(log10(p_up/p_fix(1))/w)) + 2
+    i_up_detect = max(1,i_up_detect)
+    i_up_detect = min(i_up_detect,ncre)
+!       print *, 'iud = ', i_up_detect
   
-  i = ncre - I_ONE
-  do while (i .ge. 0)
-    i_up_detect = i+I_ONE
-!     print *, 'p_up ', i, p_up, p_fix(i)
-    if (p_up .gt.  p_fix(i)) exit   ! checked and works properly - if p_fix(i) is lt p_up, then e(max) must be in i-th + 1 bin.
-    i = i - I_ONE
-  enddo
-  
-!   return p_up_ind
   end function i_up_detect
 
 end module cresp_p_diffusion
