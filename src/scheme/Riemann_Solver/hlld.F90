faf34a01ab77803e78c1caa8b5e37cdc32b4277b
varadarajan87 <varada987@gmail.com>
2017-11-14 08:09:41 +0100

    added test function chspeed

--- src/scheme/Riemann_Solver/hlld.F90
+++ src/scheme/Riemann_Solver/hlld.F90
@@ -46,7 +46,7 @@ module hlld
   implicit none
 
   private
-  public :: riemann_hlld, fluxes
+  public :: riemann_hlld, fluxes, chspeed
 
 contains
 
@@ -119,6 +119,62 @@ contains
     return
 
   end function fluxes
+
+  !-------------------------------------------------------------------------------------------------------------------------------------------------
+
+  function chspeed(u,b_cc,gamma) ! Calculates Eq 15 from Mignone, Tzeferacos, JCoPh 229 (2010) 2117-2138
+
+    use constants,  only: half, zero, xdim, ydim, zdim
+    use fluidindex, only: flind
+    use fluidtypes, only: component_fluid
+    use func,       only: ekin
+    
+    implicit none
+    
+    real, dimension(:,:), intent(in) :: u
+    real, dimension(:,:), intent(in) :: b_cc
+    real,                 intent(in) :: gamma
+
+    real, dimension(size(u,2))                           :: vx, vy, vz, pr
+    integer                                              :: ip, boff
+    real, dimension(size(u,2))                           :: v_amp, cf
+    class(component_fluid), pointer                      :: fl
+    real, parameter                                      :: four = 4.0
+    real, dimension(size(u,2))                           :: chspeed
+
+    boff = size(u, 1) ! assume xdim == 1
+
+    chspeed = zero
+
+    do ip = 1, flind%fluids
+
+       fl => flind%all_fluids(ip)%fl
+
+       vx  =  u(fl%imx,:)/u(fl%idn,:)
+       vy  =  u(fl%imy,:)/u(fl%idn,:)
+       vz  =  u(fl%imz,:)/u(fl%idn,:)
+
+       if (fl%has_energy) then
+          ! Gas pressure without magnetic fields. Pg 317, Eq. 2. (1) and (2) are markers for HD and MHD
+          pr = fl%gam_1*(u(fl%ien,:) - ekin(u(fl%imx,:), u(fl%imy,:), u(fl%imz,:), u(fl%idn,:))) ! (1)
+          if (fl%is_magnetized) then
+             ! Gas pressure with magnetic fields. Pg 317, Eq. 2.
+             pr = pr - half*fl%gam_1*sum(b_cc(xdim:zdim,:)**2, dim=1) ! (2)
+          endif
+       else
+          ! Dust
+          pr = 0.
+       endif
+
+       v_amp = sqrt( sum(vx(:)*vx(:) + vy(:)*vy(:) + vz(:)*vz(:) ) )
+       cf    = sqrt(half*((gamma*pr(:) +sum(b_cc(xdim:zdim,:)**2,dim=1) ) + sqrt((gamma*pr(:) + sum(b_cc(xdim:zdim,:)**2,dim=1))**2 - four*gamma*pr(:)*b_cc(xdim,:)**2))/u(fl%idn,:))
+       chspeed = max(chspeed,v_amp+cf)
+
+    end do
+
+    return
+    
+  end function chspeed
 
  !-------------------------------------------------------------------------------------------------------------------------------------------------
 
