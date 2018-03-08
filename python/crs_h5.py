cb6fc70878825af8d0683b62e7be81a5d87a30eb
mogrodnik <mateusz.ogrodnik@astri.uni.torun.pl>
2018-03-08 14:45:00 +0100

    [crs_h5] Enabled fixed spectrum aplitude for other plottable variables, reduced amount of imported modules.

--- python/crs_h5.py
+++ python/crs_h5.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python
-from pylab import *
+from pylab import zeros, sqrt, size
 import matplotlib.pyplot as plt
-import numpy as np
-from math import pi
-from numpy import log, log10
+from numpy import log10, log, pi, asfarray, array
 import h5py
 import os
 import sys
 
+#------ default values of parameters --------
+e_small = 1.0e-5
+eps     = 1.0e-15
+ncre      = 45
+p_min_fix = 0.4e0  #
+p_max_fix = 1.65e4 #
+cre_eff = 0.01
+c = 1.0
+first_run = True
+fixed_width = 1
+
 def nq2f(n,q,p_l,p_r):
       if p_r> 0.0 and p_l > 0 :
         pr_by_pl = p_r / p_l
-        nq2f = n / (4*pi*p_l**3)
+        nq2f = n / (4* pi * p_l**3)
         if abs(q-3.0) > eps:
             nq2f = nq2f *(3.0 - q) / (( pr_by_pl )**( 3.0 - q ) - 1.0)
         else:
@@ -63,15 +72,15 @@ def plot_data(plot_var, pl, pr, fl, fr, q, time, dt, i_lo_cut, i_up_cut, imgNbr)
       plot_var_lo_cut  = f_lo_cut
       plot_var_up_cut = f_up_cut
    elif plot_var == 'n' :
-      plot_var_l      = 4*np.pi*fl*pl**2
-      plot_var_r      = 4*np.pi*fr*pr**2
-      plot_var_lo_cut = 4*np.pi*f_lo_cut*p_lo_cut**2
-      plot_var_up_cut = 4*np.pi*f_up_cut*p_up_cut**2
+      plot_var_l      = 4*pi*fl*pl**2
+      plot_var_r      = 4*pi*fr*pr**2
+      plot_var_lo_cut = 4*pi*f_lo_cut*p_lo_cut**2
+      plot_var_up_cut = 4*pi*f_up_cut*p_up_cut**2
    elif plot_var == 'e' :
-      plot_var_l      = 4*np.pi*c**2*fl*pl**3
-      plot_var_r      = 4*np.pi*c**2*fr*pr**3
-      plot_var_lo_cut = 4*np.pi*c**2*f_lo_cut*p_lo_cut**3
-      plot_var_up_cut = 4*np.pi*c**2*f_up_cut*p_up_cut**3
+      plot_var_l      = 4*pi*c**2*fl*pl**3
+      plot_var_r      = 4*pi*c**2*fr*pr**3
+      plot_var_lo_cut = 4*pi*c**2*f_lo_cut*p_lo_cut**3
+      plot_var_up_cut = 4*pi*c**2*f_up_cut*p_up_cut**3
 
    s = plt.subplot(122)
    plt.cla()
@@ -88,9 +97,15 @@ def plot_data(plot_var, pl, pr, fl, fr, q, time, dt, i_lo_cut, i_up_cut, imgNbr)
       plot_p_max    =  p_up_cut
       plot_var_min = 0.1*e_small
       first_run = False
-   
+      if (plot_var == "e"):
+        plot_var_min = 0.1 * e_small
+      elif (plot_var == "f" ):
+        plot_var_min = e_small / (4*pi * (c ** 2)  * p_max_fix **3) /10.
+      elif (plot_var == "n" ):
+        plot_var_min = 0.1 / (4*pi * p_max_fix ** 2)
+
    if fixed_width == 1:
-      plt.xlim (0.01*plot_p_min   ,  10.*plot_p_max ) 
+      plt.xlim (0.1 * plot_p_min   ,  10.*plot_p_max ) 
       plt.xlim
    
    plt.ylim (plot_var_min , plot_ymax)   
@@ -108,15 +123,7 @@ def plot_data(plot_var, pl, pr, fl, fr, q, time, dt, i_lo_cut, i_up_cut, imgNbr)
 #-----------------------------------------------------------------
 
 def crs_plot_main(parameter_names, parameter_values, plot_var, ncrs, ecrs, field_max, time, dt, image_number):
-    global e_small, eps, c, first_run, fixed_width
-    
-    e_small = 1.0e-5
-    eps     = 1.0e-15
-    ncre      = 45
-    p_min_fix = 0.4e0  #
-    p_max_fix = 1.65e4 #
-    cre_eff = 0.01
-    c = 1.0
+    global first_run
 
     try:
         for i in range(len(parameter_names)):
@@ -124,7 +131,6 @@ def crs_plot_main(parameter_names, parameter_values, plot_var, ncrs, ecrs, field
     except:
         print "Exiting: len(names) not equal len(values)"
         sys.exit()
-
 # TODO -------- do it under *args TODO
     fixed_width = 1  
 # TODO -------- do it under *args TODO
@@ -140,12 +146,13 @@ def crs_plot_main(parameter_names, parameter_values, plot_var, ncrs, ecrs, field
 # WARNING !!! cutoff momenta are not precisely described here !!! WARNING
     log_width   = (log10(p_max_fix/p_min_fix))/(ncre-2.0)
 # TODO: do it in the first run / calling script TODO
-    for i in range(0,ncre-1):
-        p_fix[i+1]  = p_min_fix * 10.0**( log_width * edges[i])
-        p_fix_ratio = 10.0 ** log_width
-        p_fix[0]    = ( sqrt(p_fix[1] * p_fix[2]) ) / p_fix_ratio
-        p_fix[ncre]    = ( sqrt(p_fix[ncre-2]* p_fix[ncre-1]) ) * p_fix_ratio
-        p_fix = np.asfarray(p_fix)
+    if first_run:
+        for i in range(0,ncre-1):
+            p_fix[i+1]  = p_min_fix * 10.0**( log_width * edges[i])
+            p_fix_ratio = 10.0 ** log_width
+            p_fix[0]    = ( sqrt(p_fix[1] * p_fix[2]) ) / p_fix_ratio
+            p_fix[ncre]    = ( sqrt(p_fix[ncre-2]* p_fix[ncre-1]) ) * p_fix_ratio
+            p_fix = asfarray(p_fix)
 
     i_lo = 0
     i_up = ncre
@@ -160,21 +167,23 @@ def crs_plot_main(parameter_names, parameter_values, plot_var, ncrs, ecrs, field
     for i in range(ncre,1,-1):
         i_up = i
         if ecrs[i-1] >= e_small: break
+
     print('time = %6.2f |  i_lo = %2d, i_up = %2d, %11s'%(time, i_lo if not empty_cell else 0, i_up if not empty_cell else 0, '(empty cell)' if empty_cell else ' '))        
     pln = p_fix[i_lo:i_up]
     prn = p_fix[i_lo+1:i_up+1]
-    pln = np.array(pln)
-    prn = np.array(prn)
+    pln = array(pln)
+    prn = array(prn)
     ncrs_act = ncrs[i_lo-2:i_up-2]
     ecrs_act = ecrs[i_lo-2:i_up-2]
     q_nr = [] ; fln = [] ; frn = []
     for i in range(0,i_up - i_lo):
         q_nr.append(nr_get_q(ecrs[i+i_lo]/(ncrs[i+i_lo]*c*pln[i]), prn[i]/pln[i], ))
         fln.append(nq2f(ncrs[i+i_lo], q_nr[-1], pln[i], prn[i]))
-    q_nr = np.array(q_nr)
-    fln  = np.array(fln) 
-    frn  = np.array(fln)
+
+    q_nr = array(q_nr)
+    fln  = array(fln)
+    frn  = array(fln)
     frn  = frn * (prn/pln) ** (-q_nr)
+
     if empty_cell==False: 
         plot_data(plot_var, pln, prn, fln, frn, q_nr, time, dt, i_lo, i_up, image_number)
-
