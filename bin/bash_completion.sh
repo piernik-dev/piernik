7da2e65f3ce20d452c32ae541beb59297c6c00df
Artur Gawryszczak <gawrysz@gmail.com>
2018-04-15 22:17:54 +0200

    [bash_completion] expanded bash completion functios

--- bin/bash_completion.sh
+++ bin/bash_completion.sh
@@ -1,13 +1,19 @@
 #/usr/bin/env bash
 
-_setup_completions()
+_piernik_completions()
 {
+  COMPREPLY=()
   if [ "${#COMP_WORDS[@]}" -gt "2" ]; then
-    return
-    # ToDo: add setup options here
+    COMPREPLY=($( compgen -W "-v --verbose -q --laconic --debug \
+    -n --nocompile --copy -l --linkexe -p --param= -d --define= --f90flags= \
+    -c --compiler= -o --obj=" -- "${COMP_WORDS[COMP_CWORD]}" ))
+    [[ $COMPREPLY == *= ]] && compopt -o nospace
   else
-    COMPREPLY=($( compgen -W "$( find problems/ -type d | sed 's/^problems\///' ) --help --last --problems --units" -- "${COMP_WORDS[1]}" ))
+    COMPREPLY=($( compgen -W "$( find problems/ -type d | sed 's/^problems\///' ) \
+    -h --help --last --problems -u --units" -- "${COMP_WORDS[1]}" ))
   fi
 }
 
-complete -F _setup_completions ./setup
+for i in ./setup piernik_setup.py ; do
+  complete -F _piernik_completions $i
+done
