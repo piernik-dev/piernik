66d8221fb652824994de0ff1e6df941435606000
Artur Gawryszczak <gawrysz@gmail.com>
2018-11-30 14:39:13 +0100

    [RTVD] CT parts moved to a separate file

---
+++ src/scheme/rtvd_split/ct.F90
@@ -0,0 +1,205 @@
+!
+! PIERNIK Code Copyright (C) 2006 Michal Hanasz
+!
+!    This file is part of PIERNIK code.
+!
+!    PIERNIK is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    PIERNIK is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with PIERNIK.  If not, see <http://www.gnu.org/licenses/>.
+!
+!    Initial implementation of PIERNIK code was based on TVD split MHD code by
+!    Ue-Li Pen
+!        see: Pen, Arras & Wong (2003) for algorithm and
+!             http://www.cita.utoronto.ca/~pen/MHD
+!             for original source code "mhd.f90"
+!
+!    For full list of developers see $PIERNIK_HOME/license/pdt.txt
+!
+#include "piernik.h"
+
+!>
+!! \brief COMMENT ME
+!<
+
+module ct
+! pulled by MAGNETIC
+
+   implicit none
+
+   private
+   public :: magfield
+
+contains
+
+   subroutine magfield(dir)
+
+      use advects,     only: advectb
+      use constants,   only: ndims, I_ONE
+#ifdef RESISTIVE
+      use resistivity, only: diffuseb
+#endif /* RESISTIVE */
+
+      implicit none
+
+      integer(kind=4), intent(in) :: dir
+
+      integer(kind=4)             :: bdir, dstep
+
+      do dstep = 0, 1
+         bdir  = I_ONE + mod(dir+dstep,ndims)
+         call advectb(bdir, dir)
+#ifdef RESISTIVE
+         call diffuseb(bdir, dir)
+#endif /* RESISTIVE */
+         call mag_add(dir, bdir)
+      enddo
+
+   end subroutine magfield
+
+!------------------------------------------------------------------------------------------
+
+   subroutine mag_add(dim1, dim2)
+
+      use cg_leaves,        only: leaves
+      use cg_list,          only: cg_list_element
+      use grid_cont,        only: grid_container
+      use all_boundaries,   only: all_mag_boundaries
+      use user_hooks,       only: custom_emf_bnd
+#ifdef RESISTIVE
+      use constants,        only: wcu_n
+      use dataio_pub,       only: die
+      use domain,           only: is_multicg
+      use named_array_list, only: qna
+#endif /* RESISTIVE */
+
+      implicit none
+
+      integer(kind=4), intent(in)    :: dim1, dim2
+
+      type(cg_list_element), pointer :: cgl
+      type(grid_container),  pointer :: cg
+#ifdef RESISTIVE
+      real, dimension(:,:,:), pointer :: wcu
+#endif /* RESISTIVE */
+
+      cgl => leaves%first
+      do while (associated(cgl))
+         cg => cgl%cg
+#ifdef RESISTIVE
+! DIFFUSION FULL STEP
+         wcu => cg%q(qna%ind(wcu_n))%arr
+         if (is_multicg) call die("[fluidupdate:mag_add] multiple grid pieces per processor not implemented yet") ! not tested custom_emf_bnd
+         if (associated(custom_emf_bnd)) call custom_emf_bnd(wcu)
+         cg%b(dim2,:,:,:) = cg%b(dim2,:,:,:) -              wcu*cg%idl(dim1)
+         cg%b(dim2,:,:,:) = cg%b(dim2,:,:,:) + pshift(wcu,dim1)*cg%idl(dim1)
+         cg%b(dim1,:,:,:) = cg%b(dim1,:,:,:) +              wcu*cg%idl(dim2)
+         cg%b(dim1,:,:,:) = cg%b(dim1,:,:,:) - pshift(wcu,dim2)*cg%idl(dim2)
+#endif /* RESISTIVE */
+! ADVECTION FULL STEP
+         if (associated(custom_emf_bnd)) call custom_emf_bnd(cg%wa)
+         cg%b(dim2,:,:,:) = cg%b(dim2,:,:,:) - cg%wa*cg%idl(dim1)
+         cg%wa = mshift(cg%wa,dim1)
+         cg%b(dim2,:,:,:) = cg%b(dim2,:,:,:) + cg%wa*cg%idl(dim1)
+         cg%b(dim1,:,:,:) = cg%b(dim1,:,:,:) - cg%wa*cg%idl(dim2)
+         cg%wa = pshift(cg%wa,dim2)
+         cg%b(dim1,:,:,:) = cg%b(dim1,:,:,:) + cg%wa*cg%idl(dim2)
+         cgl => cgl%nxt
+      enddo
+
+      call all_mag_boundaries
+
+   end subroutine mag_add
+!------------------------------------------------------------------------------------------
+
+!>
+!! \brief Function pshift makes one-cell, forward circular shift of 3D array in any direction
+!! \param tab input array
+!! \param d direction of the shift, where 1,2,3 corresponds to \a x,\a y,\a z respectively
+!! \return real, dimension(size(tab,1),size(tab,2),size(tab,3))
+!!
+!! The function was written in order to significantly improve
+!! the performance at the cost of the flexibility of original \p CSHIFT.
+!<
+   function pshift(tab, d)
+
+      use dataio_pub,    only: warn
+
+      implicit none
+
+      real, dimension(:,:,:), intent(inout) :: tab
+      integer(kind=4),        intent(in)    :: d
+
+      integer :: ll
+      real, dimension(size(tab,1),size(tab,2),size(tab,3)) :: pshift
+
+      ll = size(tab,d)
+
+      if (ll==1) then
+         pshift = tab
+         return
+      endif
+
+      if (d==1) then
+         pshift(1:ll-1,:,:) = tab(2:ll,:,:); pshift(ll,:,:) = tab(1,:,:)
+      else if (d==2) then
+         pshift(:,1:ll-1,:) = tab(:,2:ll,:); pshift(:,ll,:) = tab(:,1,:)
+      else if (d==3) then
+         pshift(:,:,1:ll-1) = tab(:,:,2:ll); pshift(:,:,ll) = tab(:,:,1)
+      else
+         call warn('[fluidupdate:pshift]: Dim ill defined in pshift!')
+      endif
+
+      return
+   end function pshift
+
+!>
+!! \brief Function mshift makes one-cell, backward circular shift of 3D array in any direction
+!! \param tab input array
+!! \param d direction of the shift, where 1,2,3 corresponds to \a x,\a y,\a z respectively
+!! \return real, dimension(size(tab,1),size(tab,2),size(tab,3))
+!!
+!! The function was written in order to significantly improve
+!! the performance at the cost of the flexibility of original \p CSHIFT.
+!<
+   function mshift(tab,d)
+
+      use dataio_pub,    only: warn
+
+      implicit none
+
+      real, dimension(:,:,:), intent(inout) :: tab
+      integer(kind=4),        intent(in)    :: d
+
+      integer :: ll
+      real, dimension(size(tab,1) , size(tab,2) , size(tab,3)) :: mshift
+
+      ll = size(tab,d)
+
+      if (ll==1) then
+         mshift = tab
+         return
+      endif
+
+      if (d==1) then
+         mshift(2:ll,:,:) = tab(1:ll-1,:,:); mshift(1,:,:) = tab(ll,:,:)
+      else if (d==2) then
+         mshift(:,2:ll,:) = tab(:,1:ll-1,:); mshift(:,1,:) = tab(:,ll,:)
+      else if (d==3) then
+         mshift(:,:,2:ll) = tab(:,:,1:ll-1); mshift(:,:,1) = tab(:,:,ll)
+      else
+         call warn('[fluidupdate:mshift]: Dim ill defined in mshift!')
+      endif
+
+      return
+   end function mshift
+
+end module ct
