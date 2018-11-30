85bd8bdb755180ac9d1847b387022e84f2705a9a
Artur Gawryszczak <gawrysz@gmail.com>
2018-11-30 15:18:49 +0100

    [RTVD] fuse advects and ct

--- src/scheme/rtvd_split/ct.F90
+++ src/scheme/rtvd_split/ct.F90
@@ -37,12 +37,12 @@ module ct
 
    private
    public :: magfield
+   real, dimension(:), pointer :: vibj => null()
 
 contains
 
    subroutine magfield(dir)
 
-      use advects,     only: advectb
       use constants,   only: ndims, I_ONE
 #ifdef RESISTIVE
       use resistivity, only: diffuseb
@@ -66,6 +66,104 @@ contains
    end subroutine magfield
 
 !------------------------------------------------------------------------------------------
+
+!>
+!!   advectby_x --> advectb(bdir=ydim, vdir=xdim, emf='vxby')
+!!   advectbz_x --> advectb(bdir=zdim, vdir=xdim, emf='vxbz')
+!!   advectbx_y --> advectb(bdir=xdim, vdir=ydim, emf='vybx')
+!!   advectbz_y --> advectb(bdir=zdim, vdir=ydim, emf='vybz')
+!!   advectbx_z --> advectb(bdir=xdim, vdir=zdim, emf='vzbx')
+!!   advectby_z --> advectb(bdir=ydim, vdir=zdim, emf='vzby')
+!<
+
+!>
+!! \todo remove workaround for http://gcc.gnu.org/bugzilla/show_bug.cgi?id=48955
+!<
+
+  subroutine advectb(bdir, vdir)
+
+      use cg_leaves,        only: leaves
+      use cg_list,          only: cg_list_element
+      use constants,        only: xdim, ydim, zdim, LO, HI, ndims, INT4
+      use dataio_pub,       only: die
+      use domain,           only: dom
+      use fluidindex,       only: flind
+      use global,           only: dt
+      use grid_cont,        only: grid_container
+      use magboundaries,    only: bnd_emf
+      use named_array_list, only: qna, wna
+      use rtvd,             only: tvdb
+
+      implicit none
+
+      integer(kind=4),       intent(in) :: bdir, vdir
+      integer, pointer                  :: i1, i2, i1m, i2m
+      integer, dimension(ndims), target :: ii, im
+      integer                           :: rdir, i, j
+      integer(kind=4)                   :: imom                   !< index of vdir momentum
+      integer(kind=4)                   :: dir
+      integer(kind=4), dimension(ndims) :: emf
+      real, dimension(:), allocatable   :: vv, vv0 !< \todo workaround for bug in gcc-4.6, REMOVE ME
+      real, dimension(:),    pointer    :: pm1, pm2, pd1, pd2
+      type(cg_list_element), pointer    :: cgl
+      type(grid_container),  pointer    :: cg
+
+      imom = flind%ion%idn + int(vdir, kind=4)
+      rdir = sum([xdim,ydim,zdim]) - bdir - vdir
+      emf(vdir) = 1_INT4 ; emf(bdir) = 2_INT4 ; emf(rdir) = 3_INT4
+
+      if (mod(3+vdir-bdir,3) == 1) then     ! even permutation
+         i1 => ii(rdir) ; i1m => ii(rdir) ; i2 => ii(bdir) ; i2m => im(bdir)
+      elseif (mod(3+vdir-bdir,3) == 2) then !  odd permutation
+         i1 => ii(bdir) ; i1m => im(bdir) ; i2 => ii(rdir) ; i2m => ii(rdir)
+      else
+         call die('[advects:advectb] neither even nor odd permutation.')
+         i1 => ii(rdir) ; i1m => ii(rdir) ; i2 => ii(rdir) ; i2m => ii(rdir) ! suppress compiler warnings
+      endif
+
+      cgl => leaves%first
+      do while (associated(cgl))
+         cg => cgl%cg
+
+         if (any([allocated(vv), allocated(vv0)])) call die("[advects:advectb] vv or vv0 already allocated")
+         allocate(vv(cg%n_(vdir)), vv0(cg%n_(vdir)))
+
+         im(bdir) = cg%lhn(bdir, LO)
+         do i = cg%lhn(bdir, LO) + dom%D_(bdir), cg%lhn(bdir, HI)
+            ii(bdir) = i
+            do j = cg%ijkse(rdir,LO), cg%ijkse(rdir,HI)
+               ii(rdir) = j
+               im(rdir) = ii(rdir)
+               vv=0.0
+               pm1 => cg%w(wna%fi)%get_sweep(vdir,imom,i1m,i2m)
+               pm2 => cg%w(wna%fi)%get_sweep(vdir,imom,i1 ,i2 )
+               pd1 => cg%w(wna%fi)%get_sweep(vdir,flind%ion%idn,i1m,i2m)
+               pd2 => cg%w(wna%fi)%get_sweep(vdir,flind%ion%idn,i1 ,i2 )
+               vv0(:) = (pm1+pm2)/(pd1+pd2) !< \todo workaround for bug in gcc-4.6, REMOVE ME
+               !vv(2:cg%n_(vdir)-1)=(vv(1:cg%n_(vdir)-2) + vv(3:cg%n_(vdir)) + 2.0*vv(2:cg%n_(vdir)-1))*0.25
+               vv(2:cg%n_(vdir)-1)=(vv0(1:cg%n_(vdir)-2) + vv0(3:cg%n_(vdir)) + 2.0*vv0(2:cg%n_(vdir)-1))*0.25 !< \todo workaround for bug in gcc-4.6, REMOVE ME
+               vv(1)  = vv(2)
+               vv(cg%n_(vdir)) = vv(cg%n_(vdir)-1)
+
+               vibj => cg%q(qna%wai)%get_sweep(vdir,i1,i2)
+               call tvdb(vibj, cg%w(wna%bi)%get_sweep(vdir,bdir,i1,i2), vv, cg%n_(vdir),dt, cg%idl(vdir))
+               NULLIFY(pm1, pm2, pd1, pd2)
+
+            enddo
+            im(bdir) = ii(bdir)
+         enddo
+
+         do dir = xdim, zdim
+            if (dom%has_dir(dir)) call bnd_emf(qna%wai,emf(dir),dir, cg)
+         enddo
+
+         deallocate(vv, vv0)
+
+         cgl => cgl%nxt
+      enddo
+      NULLIFY(i1, i1m, i2, i2m)
+
+   end subroutine advectb
 
    subroutine mag_add(dim1, dim2)
 
