72ef27c580064394b14ee55a6df585beacec1334
Artur Gawryszczak <gawrysz@gmail.com>
2018-11-30 16:42:37 +0100

    [Riemann] grab CT-related functions together

--- src/scheme/Riemann_Solver/ct.F90
+++ src/scheme/Riemann_Solver/ct.F90
@@ -34,8 +34,7 @@ module ct
    implicit none
 
    private
-   public  :: advectb
-   real, dimension(:), pointer :: vibj => null()
+   public  :: magfield
 
 contains
 
@@ -127,6 +126,35 @@ contains
       return
 
     end subroutine ctb
+!-------------------------------------------------------------------------------------------------------------------
+
+   subroutine magfield(dir)
+
+      use constants,   only: ndims, I_ONE
+      use dataio_pub,  only: die
+      use global,      only: force_cc_mag
+#ifdef RESISTIVE
+      use resistivity, only: diffuseb
+#endif /* RESISTIVE */
+
+       implicit none
+
+       integer(kind=4), intent(in) :: dir
+
+       integer(kind=4)             :: bdir, dstep
+
+       if (force_cc_mag) call die("[fluidupdate:magfield] forcing cell-centered magnetic field is not allowed for constrained transport")
+
+       do dstep = 0, 1
+          bdir  = I_ONE + mod(dir+dstep,ndims)
+          call advectb(bdir, dir)
+#ifdef RESISTIVE
+         call diffuseb(bdir, dir)
+#endif /* RESISTIVE */
+         call mag_add(dir, bdir)
+      enddo
+
+   end subroutine magfield
 
 !---------------------------------------------------------------------------------------------------------------------
    subroutine advectb(bdir, vdir)
@@ -141,7 +169,6 @@ contains
       use grid_cont,        only: grid_container
       use magboundaries,    only: bnd_emf
       use named_array_list, only: qna, wna
-      !use rtvd,             only: tvdb
 
       implicit none
 
@@ -156,6 +183,8 @@ contains
       real, dimension(:),    pointer    :: pm1, pm2, pd1, pd2
       type(cg_list_element), pointer    :: cgl
       type(grid_container),  pointer    :: cg
+      real, dimension(:), pointer       :: vibj => null()
+
 
       imom = flind%ion%idn + int(vdir, kind=4)
       rdir = sum([xdim,ydim,zdim]) - bdir - vdir
@@ -213,6 +242,148 @@ contains
       NULLIFY(i1, i1m, i2, i2m)
 
    end subroutine advectb
+
+!-------------------------------------------------------------------------------------------------------------------
+
+ subroutine mag_add(dim1, dim2)
+
+      use cg_leaves,        only: leaves
+      use cg_list,          only: cg_list_element
+      use dataio_pub,       only: die
+      use global,           only: force_cc_mag
+      use grid_cont,        only: grid_container
+      use all_boundaries,   only: all_mag_boundaries
+      use user_hooks,       only: custom_emf_bnd
+#ifdef RESISTIVE
+     use constants,        only: wcu_n
+     use dataio_pub,       only: die
+     use domain,           only: is_multicg
+     use named_array_list, only: qna
+#endif /* RESISTIVE */
+
+      implicit none
+
+      integer(kind=4), intent(in)    :: dim1, dim2
+
+      type(cg_list_element), pointer :: cgl
+      type(grid_container),  pointer :: cg
+#ifdef RESISTIVE
+      real, dimension(:,:,:), pointer :: wcu
+#endif /* RESISTIVE */
+
+      if (force_cc_mag) call die("[fluidupdate:mag_add] forcing cell-centered magnetic field is not allowed for constrained transport")
+
+      cgl => leaves%first
+      do while (associated(cgl))
+         cg => cgl%cg
+#ifdef RESISTIVE
+! DIFFUSION FULL STEP
+         wcu => cg%q(qna%ind(wcu_n))%arr
+         if (is_multicg) call die("[fluidupdate:mag_add] multiple grid pieces per processor not implemented yet") ! not tested custom_emf_bnd
+         if (associated(custom_emf_bnd)) call custom_emf_bnd(wcu)
+         cg%b(dim2,:,:,:) = cg%b(dim2,:,:,:) -              wcu*cg%idl(dim1)
+         cg%b(dim2,:,:,:) = cg%b(dim2,:,:,:) + pshift(wcu,dim1)*cg%idl(dim1)
+         cg%b(dim1,:,:,:) = cg%b(dim1,:,:,:) +              wcu*cg%idl(dim2)
+         cg%b(dim1,:,:,:) = cg%b(dim1,:,:,:) - pshift(wcu,dim2)*cg%idl(dim2)
+#endif /* RESISTIVE */
+! ADVECTION FULL STEP
+         if (associated(custom_emf_bnd)) call custom_emf_bnd(cg%wa)
+         cg%b(dim2,:,:,:) = cg%b(dim2,:,:,:) - cg%wa*cg%idl(dim1)
+         cg%wa = mshift(cg%wa,dim1)
+         cg%b(dim2,:,:,:) = cg%b(dim2,:,:,:) + cg%wa*cg%idl(dim1)
+         cg%b(dim1,:,:,:) = cg%b(dim1,:,:,:) - cg%wa*cg%idl(dim2)
+         cg%wa = pshift(cg%wa,dim2)
+         cg%b(dim1,:,:,:) = cg%b(dim1,:,:,:) + cg%wa*cg%idl(dim2)
+         cgl => cgl%nxt
+      enddo
+
+      call all_mag_boundaries
+
+   end subroutine mag_add
+
+!-------------------------------------------------------------------------------------------------------------------
+
+!>
+!! \brief Function pshift makes one-cell, forward circular shift of 3D array in any direction
+!! \param tab input array
+!! \param d direction of the shift, where 1,2,3 corresponds to \a x,\a y,\a z respectively
+!! \return real, dimension(size(tab,1),size(tab,2),size(tab,3))
+!!
+!! The function was written in order to significantly improve
+!! the performance at the cost of the flexibility of original \p CSHIFT.
+!<
+   function pshift(tab, d)
+
+      use dataio_pub,    only: warn
+
+      implicit none
+
+      real, dimension(:,:,:), intent(inout) :: tab
+      integer(kind=4),        intent(in)    :: d
+
+      integer :: ll
+      real, dimension(size(tab,1),size(tab,2),size(tab,3)) :: pshift
+
+      ll = size(tab,d)
+
+      if (ll==1) then
+         pshift = tab
+         return
+      endif
+
+      if (d==1) then
+         pshift(1:ll-1,:,:) = tab(2:ll,:,:); pshift(ll,:,:) = tab(1,:,:)
+      else if (d==2) then
+         pshift(:,1:ll-1,:) = tab(:,2:ll,:); pshift(:,ll,:) = tab(:,1,:)
+      else if (d==3) then
+         pshift(:,:,1:ll-1) = tab(:,:,2:ll); pshift(:,:,ll) = tab(:,:,1)
+      else
+         call warn('[fluidupdate:pshift]: Dim ill defined in pshift!')
+      endif
+
+      return
+   end function pshift
+
+!>
+!! \brief Function mshift makes one-cell, backward circular shift of 3D array in any direction
+!! \param tab input array
+!! \param d direction of the shift, where 1,2,3 corresponds to \a x,\a y,\a z respectively
+!! \return real, dimension(size(tab,1),size(tab,2),size(tab,3))
+!!
+!! The function was written in order to significantly improve
+!! the performance at the cost of the flexibility of original \p CSHIFT.
+!<
+   function mshift(tab,d)
+
+      use dataio_pub,    only: warn
+
+      implicit none
+
+      real, dimension(:,:,:), intent(inout) :: tab
+      integer(kind=4),        intent(in)    :: d
+
+      integer :: ll
+      real, dimension(size(tab,1) , size(tab,2) , size(tab,3)) :: mshift
+
+      ll = size(tab,d)
+
+      if (ll==1) then
+         mshift = tab
+         return
+      endif
+
+      if (d==1) then
+         mshift(2:ll,:,:) = tab(1:ll-1,:,:); mshift(1,:,:) = tab(ll,:,:)
+      else if (d==2) then
+         mshift(:,2:ll,:) = tab(:,1:ll-1,:); mshift(:,1,:) = tab(:,ll,:)
+      else if (d==3) then
+         mshift(:,:,2:ll) = tab(:,:,1:ll-1); mshift(:,:,1) = tab(:,:,ll)
+      else
+         call warn('[fluidupdate:mshift]: Dim ill defined in mshift!')
+      endif
+
+      return
+   end function mshift
 
  end module ct
 
