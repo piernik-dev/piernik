3e0ed4076ca06d5bacd00f0ef3820cd29d113934
Dominik Wóltański <wolt@umk.pl>
2019-02-12 13:23:26 +0100

    [gravity] revert part of ther previous commit - changed too much in DUST dynamics (to be resolved in future)

--- src/gravity/gravity.F90
+++ src/gravity/gravity.F90
@@ -888,20 +888,20 @@ contains
       case (RK2_1)
          select case (sweep)
             case (xdim)
-               grav(2:n-1) = half*(cg%hgpot(cg%lhn(xdim, LO):cg%lhn(xdim, HI)-2, i1, i2) - cg%hgpot(cg%lhn(xdim, LO)+2:cg%lhn(xdim, HI), i1, i2))*cg%idl(xdim)
+               grav(2:n-1) = half*(cg%hgpot(cg%lhn(xdim, LO):cg%lhn(xdim, HI)-2, i1, i2) - cg%hgpot(cg%lhn(xdim, LO)+2:cg%lhn(xdim, HI), i1, i2))/cg%dl(xdim)
             case (ydim)
-               grav(2:n-1) = half*(cg%hgpot(i2, cg%lhn(ydim, LO):cg%lhn(ydim, HI)-2, i1) - cg%hgpot(i2, cg%lhn(ydim, LO)+2:cg%lhn(ydim, HI), i1))*cg%idl(ydim)
+               grav(2:n-1) = half*(cg%hgpot(i2, cg%lhn(ydim, LO):cg%lhn(ydim, HI)-2, i1) - cg%hgpot(i2, cg%lhn(ydim, LO)+2:cg%lhn(ydim, HI), i1))/cg%dl(ydim)
             case (zdim)
-               grav(2:n-1) = half*(cg%hgpot(i1, i2, cg%lhn(zdim, LO):cg%lhn(zdim, HI)-2) - cg%hgpot(i1, i2, cg%lhn(zdim, LO)+2:cg%lhn(zdim, HI)))*cg%idl(zdim)
+               grav(2:n-1) = half*(cg%hgpot(i1, i2, cg%lhn(zdim, LO):cg%lhn(zdim, HI)-2) - cg%hgpot(i1, i2, cg%lhn(zdim, LO)+2:cg%lhn(zdim, HI)))/cg%dl(zdim)
          end select
       case (RK2_2, EULER)
          select case (sweep)
             case (xdim)
-               grav(2:n-1) = half*(cg%gpot(cg%lhn(xdim, LO):cg%lhn(xdim, HI)-2, i1, i2) - cg%gpot(cg%lhn(xdim, LO)+2:cg%lhn(xdim, HI), i1, i2))*cg%idl(xdim)
+               grav(2:n-1) = half*(cg%gpot(cg%lhn(xdim, LO):cg%lhn(xdim, HI)-2, i1, i2) - cg%gpot(cg%lhn(xdim, LO)+2:cg%lhn(xdim, HI), i1, i2))/cg%dl(xdim)
             case (ydim)
-               grav(2:n-1) = half*(cg%gpot(i2, cg%lhn(ydim, LO):cg%lhn(ydim, HI)-2, i1) - cg%gpot(i2, cg%lhn(ydim, LO)+2:cg%lhn(ydim, HI), i1))*cg%idl(ydim)
+               grav(2:n-1) = half*(cg%gpot(i2, cg%lhn(ydim, LO):cg%lhn(ydim, HI)-2, i1) - cg%gpot(i2, cg%lhn(ydim, LO)+2:cg%lhn(ydim, HI), i1))/cg%dl(ydim)
             case (zdim)
-               grav(2:n-1) = half*(cg%gpot(i1, i2, cg%lhn(zdim, LO):cg%lhn(zdim, HI)-2) - cg%gpot(i1, i2, cg%lhn(zdim, LO)+2:cg%lhn(zdim, HI)))*cg%idl(zdim)
+               grav(2:n-1) = half*(cg%gpot(i1, i2, cg%lhn(zdim, LO):cg%lhn(zdim, HI)-2) - cg%gpot(i1, i2, cg%lhn(zdim, LO)+2:cg%lhn(zdim, HI)))/cg%dl(zdim)
          end select
       case default
          call die("[gravity:grav_pot2accel_ord2] Unsupported substep")
@@ -945,25 +945,25 @@ contains
          select case (sweep)
             case (xdim)
                grav(3:n-2) = onetw*(cg%hgpot(cg%lhn(xdim, LO)+4:cg%lhn(xdim, HI),i1,i2) - 8.*cg%hgpot(cg%lhn(xdim, LO)+3:cg%lhn(xdim, HI)-1,i1,i2) + &
-                                    8.*cg%hgpot(cg%lhn(xdim, LO)+1:cg%lhn(xdim, HI)-3,i1,i2) - cg%hgpot(cg%lhn(xdim, LO):cg%lhn(xdim, HI)-4,i1,i2)) * cg%idl(xdim)
+                                    8.*cg%hgpot(cg%lhn(xdim, LO)+1:cg%lhn(xdim, HI)-3,i1,i2) - cg%hgpot(cg%lhn(xdim, LO):cg%lhn(xdim, HI)-4,i1,i2)) / cg%dl(xdim)
             case (ydim)
                grav(3:n-2) = onetw*(cg%hgpot(i2,cg%lhn(ydim, LO)+4:cg%lhn(ydim, HI),i1) - 8.*cg%hgpot(i2,cg%lhn(ydim, LO)+3:cg%lhn(ydim, HI)-1,i1) + &
-                                    8.*cg%hgpot(i2,cg%lhn(ydim, LO)+1:cg%lhn(ydim, HI)-3,i1) - cg%hgpot(i2,cg%lhn(ydim, LO):cg%lhn(ydim, HI)-4,i1)) * cg%idl(ydim)
+                                    8.*cg%hgpot(i2,cg%lhn(ydim, LO)+1:cg%lhn(ydim, HI)-3,i1) - cg%hgpot(i2,cg%lhn(ydim, LO):cg%lhn(ydim, HI)-4,i1)) / cg%dl(ydim)
             case (zdim)
                grav(3:n-2) = onetw*(cg%hgpot(i1,i2,cg%lhn(zdim, LO)+4:cg%lhn(zdim, HI)) - 8.*cg%hgpot(i1,i2,cg%lhn(zdim, LO)+3:cg%lhn(zdim, HI)-1) + &
-                                    8.*cg%hgpot(i1,i2,cg%lhn(zdim, LO)+1:cg%lhn(zdim, HI)-3) - cg%hgpot(i1,i2,cg%lhn(zdim, LO):cg%lhn(zdim, HI)-4)) * cg%idl(zdim)
+                                    8.*cg%hgpot(i1,i2,cg%lhn(zdim, LO)+1:cg%lhn(zdim, HI)-3) - cg%hgpot(i1,i2,cg%lhn(zdim, LO):cg%lhn(zdim, HI)-4)) / cg%dl(zdim)
          end select
       case (RK2_2, EULER)
          select case (sweep)
             case (xdim)
                grav(3:n-2) = onetw*(cg%gpot(cg%lhn(xdim, LO)+4:cg%lhn(xdim, HI),i1,i2) - 8.*cg%gpot(cg%lhn(xdim, LO)+3:cg%lhn(xdim, HI)-1,i1,i2) + &
-                                    8.*cg%gpot(cg%lhn(xdim, LO)+1:cg%lhn(xdim, HI)-3,i1,i2) - cg%gpot(cg%lhn(xdim, LO):cg%lhn(xdim, HI)-4,i1,i2)) * cg%idl(xdim)
+                                    8.*cg%gpot(cg%lhn(xdim, LO)+1:cg%lhn(xdim, HI)-3,i1,i2) - cg%gpot(cg%lhn(xdim, LO):cg%lhn(xdim, HI)-4,i1,i2)) / cg%dl(xdim)
             case (ydim)
                grav(3:n-2) = onetw*(cg%gpot(i2,cg%lhn(ydim, LO)+4:cg%lhn(ydim, HI),i1) - 8.*cg%gpot(i2,cg%lhn(ydim, LO)+3:cg%lhn(ydim, HI)-1,i1) + &
-                                    8.*cg%gpot(i2,cg%lhn(ydim, LO)+1:cg%lhn(ydim, HI)-3,i1) - cg%gpot(i2,cg%lhn(ydim, LO):cg%lhn(ydim, HI)-4,i1)) * cg%idl(ydim)
+                                    8.*cg%gpot(i2,cg%lhn(ydim, LO)+1:cg%lhn(ydim, HI)-3,i1) - cg%gpot(i2,cg%lhn(ydim, LO):cg%lhn(ydim, HI)-4,i1)) / cg%dl(ydim)
             case (zdim)
                grav(3:n-2) = onetw*(cg%gpot(i1,i2,cg%lhn(zdim, LO)+4:cg%lhn(zdim, HI)) - 8.*cg%gpot(i1,i2,cg%lhn(zdim, LO)+3:cg%lhn(zdim, HI)-1) + &
-                                    8.*cg%gpot(i1,i2,cg%lhn(zdim, LO)+1:cg%lhn(zdim, HI)-3) - cg%gpot(i1,i2,cg%lhn(zdim, LO):cg%lhn(zdim, HI)-4)) * cg%idl(zdim)
+                                    8.*cg%gpot(i1,i2,cg%lhn(zdim, LO)+1:cg%lhn(zdim, HI)-3) - cg%gpot(i1,i2,cg%lhn(zdim, LO):cg%lhn(zdim, HI)-4)) / cg%dl(zdim)
          end select
       case default
          call die("[gravity:grav_pot2accel_ord4] Unsupported substep")
