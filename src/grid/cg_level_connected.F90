7e0c66387a8b12173f7951fc1807a001168a80d6
Artur Gawryszczak <gawrysz@gmail.com>
2019-03-17 13:29:16 +0100

    [cg_level_connected] don't execute vertical_b_prep when not needed (BUGFIX)

--- src/grid/cg_level_connected.F90
+++ src/grid/cg_level_connected.F90
@@ -43,7 +43,7 @@ module cg_level_connected
       type(cg_level_connected_T), pointer :: coarser          !< coarser level cg set or null()
       type(cg_level_connected_T), pointer :: finer            !< finer level cg set or null()
       integer(kind=4)                     :: ord_prolong_set  !< Number of boundary cells for prolongation used in last update of cg_level_connected_T%vertical_prep
-      logical                             :: need_vb_update   !< If .true. then execute vertical_b_prep
+      logical, private                    :: need_vb_update   !< If .true. then execute vertical_b_prep
 
     contains
 
@@ -62,7 +62,7 @@ module cg_level_connected
       procedure :: arr3d_boundaries                           !< Set up all guardcells (internal, external and fine-coarse) for given rank-3 arrays.
       procedure :: arr4d_boundaries                           !< Set up all guardcells (internal, external and fine-coarse) for given rank-4 arrays.
       procedure :: prolong_bnd_from_coarser                   !< Interpolate boundaries from coarse level at fine-coarse interfaces
-      procedure :: vertical_b_prep                            !< Initialize prolongation targets for fine-coarse boundary exchange
+      procedure, private :: vertical_b_prep                   !< Initialize prolongation targets for fine-coarse boundary exchange
       procedure, private :: vertical_bf_prep                  !< Initialize prolongation targets for fine->coarse flux exchange
       procedure :: sync_ru                                    !< Synchronize this%recently_changed and set flags for update requests
       procedure :: free_all_cg                                !< Erase all data on the level, leave it empty
@@ -1044,7 +1044,10 @@ contains
       if (.not. this%need_vb_update) return
 
       coarse => this%coarser
-      if (.not. associated(coarse)) return ! check is some null allocations are required
+      if (.not. associated(coarse)) then
+         this%need_vb_update = .false.
+         return ! check if some null allocations are required
+      endif
 
       ! Unfortunately we can't use finest%level%l%id
       curl => this
