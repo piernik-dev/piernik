6a6e9433a2a3785837d5a2aebcac536bc5e22fd8
Artur Gawryszczak <gawrysz@gmail.com>
2019-04-03 18:38:37 +0200

    [grid_container] bits related to named arrays went to grid_container_na

--- src/grid/grid_container.F90
+++ src/grid/grid_container.F90
@@ -30,11 +30,10 @@
 
 module grid_cont
 
-   use constants,        only: xdim, zdim, LO, HI
-   use grid_cont_base,   only: grid_container_base_T
-   use fluxtypes,        only: fluxarray, fluxpoint
-   use named_array,      only: named_array4d, named_array3d
-   use refinement_flag,  only: ref_flag
+   use constants,       only: xdim, zdim, LO, HI
+   use grid_cont_na,    only: grid_container_na_T
+   use fluxtypes,       only: fluxarray, fluxpoint
+   use refinement_flag, only: ref_flag
 
    implicit none
 
@@ -71,32 +70,6 @@ module grid_cont
       type(segment), dimension(:), allocatable :: seg              !< a segment of data to be received or sent
    end type tgt_list
 
-   !>
-   !! \brief Multigrid-specific storage
-   !!
-   !! \details The FFT-related arrays here cannot be declared as named arrays in cg%q(:) because their size or type differ
-   !!
-   !! \todo Provide an initialization (procedure pointer) in multigrid gravity for these things when a cg is dynamically added
-   !<
-   type :: mg_arr
-      ! storage
-      real, allocatable, dimension(:,:,:)   :: bnd_x, bnd_y, bnd_z    !< given boundary values for potential; \todo make an array of pointers, indexed by (xdim:zdim)
-
-      ! data for FFT multigrid self-gravity solver
-      integer                                :: nxc                   !< first index (complex or real: fft(:,:,:) or fftr(:,:,:)) cell count
-      complex, allocatable, dimension(:,:,:) :: fft                   !< a complex array for FFT operations (Fourier space)
-      real,    allocatable, dimension(:,:,:) :: fftr                  !< a real array for FFT operations (Fourier space for sine transform)
-      real,    allocatable, dimension(:,:,:) :: src                   !< an input array for FFT (real space data)
-      real,    allocatable, dimension(:,:,:) :: Green3D               !< Green's function (0.5 * fft_norm / (kx + ky + kz))
-      integer (kind = selected_int_kind(16)) :: planf, plani          !< FFT forward and inverse plans
-      real                                   :: fft_norm              !< normalization factor
-
-      ! geometrical factors
-      !! \todo move to cg
-      real    :: r, rx, ry, rz                                        !< geometric factors for relaxation (diffusion) used in approximate_solution_{rbgs,relax}*
-
-   end type mg_arr
-
    !> \brief Array of boundary segments to exchange
    type :: bnd_list
       type(segment), dimension(:), allocatable :: seg !< segments
@@ -105,7 +78,7 @@ module grid_cont
    end type bnd_list
 
    !> \brief Everything required for autonomous computation of a single sweep on a portion of the domain on a single process
-   type, extends(grid_container_base_T) :: grid_container
+   type, extends(grid_container_na_T) :: grid_container
 
       ! External boundary conditions and internal boundaries
 
@@ -129,25 +102,6 @@ module grid_cont
       logical, allocatable, dimension(:,:,:) :: leafmap           !< .true. when a cell is not covered by finer cells, .false. otherwise
       logical, allocatable, dimension(:,:,:) :: refinemap         !< .true. when a cell triggers refinement criteria, .false. otherwise
 
-
-      ! Registered variables
-
-      type(named_array3d), allocatable, dimension(:) :: q        !< 3D arrays such as gravitational potential pr user-defined quantities or gravitational potential
-      type(named_array4d), allocatable, dimension(:) :: w        !< 4D arrays such as u, vector fields (b) or other vector/multi-scalar user-defined quantities
-
-      ! handy shortcuts to some entries in q(:)
-      real, dimension(:,:,:), pointer :: gpot    => null()       !< Array for sum of gravitational potential at t += dt
-      real, dimension(:,:,:), pointer :: hgpot   => null()       !< Array for sum of gravitational potential at t += 0.5*dt
-      real, dimension(:,:,:), pointer :: gp      => null()       !< Array for gravitational potential from external fields
-      real, dimension(:,:,:), pointer :: sgp     => null()       !< Array for gravitational potential from multigrid or FFT solver
-      real, dimension(:,:,:), pointer :: sgpm    => null()       !< Array for gravitational potential from multigrid or FFT solver at previous timestep saved by source_terms_grav.
-      real, dimension(:,:,:), pointer :: cs_iso2 => null()       !< COMMENT ME
-      real, dimension(:,:,:), pointer :: wa      => null()       !< Temporary array used for different purposes, usually has dimension (grid::nx, grid::ny, grid::nz)
-
-      ! handy shortcuts to some entries in w(:)
-      real, dimension(:,:,:,:), pointer :: u     => null()       !< Main array of all fluids' components
-      real, dimension(:,:,:,:), pointer :: b     => null()       !< Main array of magnetic field's components
-
       ! Misc
       integer(kind=8) :: SFC_id                                  !< position of the grid on space-filling curve
       type(ref_flag) :: refine_flags                             !< refine or derefine this grid container?
@@ -160,15 +114,11 @@ module grid_cont
 
       procedure          :: init_gc                              !< Initialization
       procedure          :: cleanup                              !< Deallocate all internals
-      procedure, private :: add_all_na                           !< Register all known named arrays for this cg, sey up shortcuts to the crucial fields
-      procedure          :: add_na                               !< Register a new 3D entry in current cg with given name.
-      procedure          :: add_na_4d                            !< Register a new 4D entry in current cg with given name.
       procedure          :: update_leafmap                       !< Check if the grid container has any parts covered by finer grids and update appropriate map
       procedure          :: set_fluxpointers
       procedure          :: save_outfluxes
       procedure          :: prolong                              !< perform prolongation of the data stored in this%prolong_
       procedure          :: refinemap2SFC_list                   !< create list of SFC indices to be created from refine flags
-      procedure          :: set_constant_b_field                 !< set constant magnetic field on whole block
 
    end type grid_container
 
@@ -300,19 +250,7 @@ contains
          endif
       enddo
 
-      if (allocated(this%q)) then
-         do g = lbound(this%q(:), dim=1), ubound(this%q(:), dim=1)
-            call this%q(g)%clean
-         enddo
-         deallocate(this%q)
-      endif
-
-      if (allocated(this%w)) then
-         do g = lbound(this%w(:), dim=1), ubound(this%w(:), dim=1)
-            call this%w(g)%clean
-         enddo
-         deallocate(this%w)
-      endif
+      call this%cleanup_na
 
       ! arrays not handled through named_array feature
       if (associated(this%prolong_xyz)) deallocate(this%prolong_xyz)
@@ -334,103 +272,6 @@ contains
       call cpr%fpcleanup
 
    end subroutine cleanup
-
-!> \brief Register all known named arrays for this cg, sey up shortcuts to the crucial fields
-
-   subroutine add_all_na(this)
-
-      use named_array_list, only: qna, wna
-#ifdef ISO
-      use constants,   only: cs_i2_n
-      use fluids_pub,  only: cs2_max
-#endif /* ISO */
-
-      implicit none
-
-      class(grid_container), intent(inout) :: this  !< object invoking type-bound procedure
-
-      integer :: i
-
-      if (allocated(qna%lst)) then
-         do i = lbound(qna%lst(:), dim=1), ubound(qna%lst(:), dim=1)
-            call this%add_na(qna%lst(i)%multigrid)
-         enddo
-      endif
-      if (allocated(wna%lst)) then
-         do i = lbound(wna%lst(:), dim=1), ubound(wna%lst(:), dim=1)
-            call this%add_na_4d(wna%lst(i)%dim4)
-         enddo
-      endif
-
-      ! shortcuts
-      this%u  => this%w(wna%fi)%arr
-      this%b  => this%w(wna%bi)%arr
-      this%wa => this%q(qna%wai)%arr
-#ifdef ISO
-      this%cs_iso2 => this%q(qna%ind(cs_i2_n))%arr
-      if (associated(this%cs_iso2)) this%cs_iso2(:,:,:) = cs2_max   ! set cs2 with sane values on non-multigrid grid pieces
-#endif /* ISO */
-
-   end subroutine add_all_na
-
-!>
-!! \brief Register a new 3D entry in current cg with given name. Called from cg_list_glob::reg_var
-!!
-!! \warning This routine should not be called directly from user routines
-!<
-   subroutine add_na(this, multigrid)
-
-      use constants,   only: base_level_id
-      use named_array, only: named_array3d
-
-      implicit none
-
-      class(grid_container), intent(inout) :: this          !< object invoking type-bound procedure
-      logical,               intent(in)    :: multigrid     !< If .true. then cg%q(:)%arr and cg%w(:)%arr are allocated also below base level
-
-      type(named_array3d), allocatable, dimension(:) :: tmp
-
-      if (.not. allocated(this%q)) then
-         allocate(this%q(1))
-      else
-         allocate(tmp(lbound(this%q(:),dim=1):ubound(this%q(:), dim=1) + 1))
-         tmp(:ubound(this%q(:), dim=1)) = this%q(:)
-         call move_alloc(from=tmp, to=this%q)
-      endif
-
-      if (multigrid .or. this%l%id >= base_level_id) call this%q(ubound(this%q(:), dim=1))%init(this%lhn(:, LO), this%lhn(:, HI))
-
-   end subroutine add_na
-
-!>
-!! \brief Register a new 4D entry in current cg with given name. Called from cg_list_glob::reg_var
-!!
-!! \warning This routine should not be called directly from user routines
-!! \deprecated Almost duplicated code with add_na
-!<
-   subroutine add_na_4d(this, n)
-
-      use constants,   only: base_level_id, INT4
-      use named_array, only: named_array4d
-
-      implicit none
-
-      class(grid_container), intent(inout) :: this         !< object invoking type-bound procedure
-      integer(kind=4),       intent(in)    :: n            !< Length of the vector quantity to be stored (first dimension of the array)
-
-      type(named_array4d), allocatable, dimension(:) :: tmp
-
-      if (.not. allocated(this%w)) then
-         allocate(this%w(1))
-      else
-         allocate(tmp(lbound(this%w(:),dim=1):ubound(this%w(:), dim=1) + 1))
-         tmp(:ubound(this%w(:), dim=1)) = this%w(:)
-         call move_alloc(from=tmp, to=this%w)
-      endif
-
-      if (this%l%id >= base_level_id) call this%w(ubound(this%w(:), dim=1))%init( [1_INT4, this%lhn(:, LO)], [n, this%lhn(:, HI)] ) !< \deprecated magic integer
-
-   end subroutine add_na_4d
 
 !> \brief Check if the grid container has any parts covered by finer grids and update appropriate map
 
@@ -1003,26 +844,5 @@ contains
       this%refinemap = .false.
 
    end subroutine refinemap2SFC_list
-
-!< \brief set constant magnetic field on whole block
-
-   subroutine set_constant_b_field(this, b)
-
-      use constants,  only: xdim, zdim
-
-      implicit none
-
-      class(grid_container),      intent(inout) :: this !< object invoking type-bound procedure
-      real, dimension(xdim:zdim), intent(in)    :: b    !< the value of the magnetic field vector in whole block
-
-      integer :: d
-
-      if (associated(this%b)) then
-         do d = xdim, zdim
-            this%b(d, this%is:this%ie, this%js:this%je, this%ks:this%ke) = b(d)
-         enddo
-      endif
-
-   end subroutine set_constant_b_field
 
 end module grid_cont
