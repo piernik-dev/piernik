e387c8a9325a4a9f02eb42c30abde6238bfb5ffc
mogrodnik <mateusz.ogrodnik@astri.uni.torun.pl>
2019-04-16 00:19:04 +0200

    [interactive_plot_crs] Added option parser+options+support, rearranged affected parts, fixed bugs

--- python/interactive_plot_crs.py
+++ python/interactive_plot_crs.py
@@ -1,49 +1,64 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from   colored_io import die, prtinfo, prtwarn, read_var
+import crs_pf, crs_h5, read_h5
+from   crs_pf import initialize_pf_arrays
+from   math   import isnan, pi
 import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
-import numpy as np
-from math import pi
-from numpy import log, log10
-import crs_pf
-import os, weakref
-import sys
-import read_h5
-import crs_h5
-import re
-from colored_io import prtinfo, prtwarn, read_var, die
+from   matplotlib.colors import LogNorm
+from   numpy  import array as np_array, log, log10
+import os
+from   optparse import OptionParser
+from   re     import search
+from   sys    import argv
 try:
+    import h5py   # DEPRECATED
     import yt
-    import h5py
-    from yt.units import dimensions
+    from   yt.units import dimensions
 except:
     die("You must make yt & h5py available somehow")
 
-plot_field = "cree05"
-plot_var = "e"
+#------- Parse arguments
+parser = OptionParser("Usage: %prog FILE [options] [args] or %prog [options] [args] -F FILENAME")
+parser.add_option("-F", "--file",  dest="filename",     default="None",       help=u"File to use", type="str")
+parser.add_option("-v", "--var",   dest="var_name",     default= "e",         help=u"Variable to plot the spectrum (default: e)")
+parser.add_option("-f", "--field", dest="fieldname",    default="cree_tot",   help=u"DS fieldname to image (default:cree_tot)")
+parser.add_option("-z", "--zlim",  dest="plot_range",   default=("x","y"),    help=u"Plot image with this range", nargs=2, metavar="ZMIN ZMAX")
+parser.add_option("-s", "--slice", dest="slice_info",   default=("a","Nan"),  help=u"DS slice coords to image (default:cree_tot)", metavar="AX COORDINATE", nargs=2)
+parser.add_option("-d", "--def",   dest="default_range",default="False",      help=u"Use min/max on yt.ds(fieldname) for clickable image",   action="store_true")
+parser.add_option("-l", "--lin",   dest="use_linscale", default="False",      help=u"Use linear scale for clickable plot (default: log)",    action="store_true")
+parser.add_option("-V", "--vel" ,  dest="plot_vel",     default="False",      help=u"Plot velocity field vectors ",  action="store_true")
+parser.add_option("-m", "--mag",   dest="plot_mag",     default="False",      help=u"Plot magnetic field vectors ",  action="store_true")
+parser.add_option("-t", "--time",  dest="annotate_time",default="False",      help=u"Annotate time on resulting DS plot", action="store_true")
+parser.add_option("", "--nosave",  dest="not_save_spec",default="False",      help=u"Do not save output spectrum ",  action="store_true")
 
-simple_plot = False # True
-plot_vel = False
-plot_mag = True
+(options, args) = parser.parse_args(argv[1:]) #argv[1] is filename
 
-user_limits = True ### True# False # use values below as limits for clickable plot
-plot_user_min = 1.0e-3 #1.0e-3 #1.0e-5
-plot_user_max = 0.47#1.78#0.15#0.37#0.47#0.034 #0.86#9.93 # 1.6e-3 #9.93
-use_logscale  = True
-hdf_save_fpq  = False
-
+plot_var = options.var_name
+user_annot_time = options.annotate_time
+user_limits     = (options.default_range!=True)
+user_save_spec  = (options.not_save_spec!=True)
+#simple_plot = False # True ### DEPRECATED
+use_logscale = ( options.use_linscale!=True)
+plot_field   = options.fieldname
+plot_var     = options.var_name
+plot_vel = bool(options.plot_vel)         ;  plot_mag = bool(options.plot_mag)
+if (plot_vel == True): plot_mag = False
+if (plot_mag == True): plot_vel = False
 user_annot_line = False
-user_annot_time = False
-user_save_spectrum = True
-par_epsilon   = 1.0e-15
-#####################################
 
+#####################################
+#------- Parameters ----------------
+par_epsilon   = 1.0e-15
+hdf_save_fpq  = False
 f_run = True
 pf_initialized = False
+
+#------- Local functions -----------
 def _total_cree(field,data):
    list_cree = []
    for element in h5ds.field_list:
-      if re.search("cree",str(element[1])):
+      if search("cree",str(element[1])):
          list_cree.append(element[1])
    cree_tot = data[str(list_cree[0])]
    for element in list_cree[1:]:
@@ -53,21 +68,20 @@ def _total_cree(field,data):
 def _total_cren(field,data):
    list_cren = []
    for element in h5ds.field_list:
-      if re.search("cren",str(element[1])):
+      if search("cren",str(element[1])):
          list_cren.append(element[1])
    cren_tot = data[str(list_cren[0])]
    for element in list_cren[1:]:
       cren_tot = cren_tot + data[element]
    return cren_tot
 
-def en_ratio(field,data):
+def en_ratio(field,data):     #DEPRECATED (?)
    bin_nr = field.name[1][-2:]
    for element in h5ds.field_list:
-      if re.search("cree"+str(bin_nr.zfill(2)),str(element[1])):
+      if search("cree"+str(bin_nr.zfill(2)),str(element[1])):
          cren_data = data["cren"+str(bin_nr.zfill(2))]
-         cren_data[cren_data == 0.0 ] = par_epsilon # necessary to avoid FPEs
+         cren_data[cren_data <= par_epsilon**2 ] = par_epsilon # necessary to avoid FPEs
          cree_data = data["cree"+str(bin_nr.zfill(2))]
-         cree_data[cree_data == 0.0 ] = par_epsilon # necessary to avoid FPEs
          en_ratio=cree_data / cren_data
    return en_ratio
 
@@ -76,10 +90,11 @@ def copy_field(field,data):
    copied_field = data[field_name_to_copy]
    return copied_field
 
-
 #---------- reading parameters
-
-filename=sys.argv[-1]
+if (options.filename != "None"):
+   filename = options.filename
+else:
+   filename=argv[1]
 filename_ext= filename.split('.')[-1]
 filename_nam  = filename.split('.')[0].split('/')[-1]
 if filename_ext[0:2] != 'h5':
@@ -112,19 +127,25 @@ if f_run == True:
 
     crs_pf.initialize_pf_arrays(pf_initialized)
 #---------- Open file
-    h5File = h5py.File(filename,"r") # sorry, I'm not sure how to access timestep via yt
+    #h5File = h5py.File(filename,"r") # sorry, I'm not sure how to access timestep via yt # DEPRECATED ?
     h5ds = yt.load(filename)
 #---------- bounds on domain size
     grid_dim = h5ds.domain_dimensions
     dim_map  = {'x':0,'y':1,'z':2}
-    dom_l = np.array(h5ds.domain_left_edge[0:3])
-    dom_r = np.array(h5ds.domain_right_edge[0:3])
+    dom_l = np_array(h5ds.domain_left_edge[0:3])
+    dom_r = np_array(h5ds.domain_right_edge[0:3])
 
 #----------- Loading other data
     t = h5ds.current_time[0]
-    dt= h5File.attrs["timestep"]
+    #dt= h5File.attrs["timestep"]      # DEPRECATED ?
     time = t.in_units('Myr')
-
+#----------- Checking user image limits
+    try:
+      plot_user_min = float(options.plot_range[0])
+      plot_user_max = float(options.plot_range[1]) # min: 1.0e-3 #1.0e-3 #1.0e-5, max: 0.47#1.78#0.15#0.37#0.47#0.034 #0.86#9.93 # 1.6e-3 #9.93
+    except:
+      prtwarn("No provided ZLIM or failed to convert it into float -- using default (min/max of ds(field) )")
+      user_limits   = False
 #------------ Organizing domain data
     length_unit = 'pc'
     prtinfo("Domain shape of in provided file            (i, j, k): [%i, %i, %i] \033[0m" %(grid_dim[0], grid_dim[1], grid_dim[2]) )
@@ -134,8 +155,8 @@ if f_run == True:
     avail_dims_by_slice = [[1,2],[0,2],[0,1]]
 
     if len(grid_dim) == 3 and min(grid_dim) != 1:
-        slice_ax = ""
-        slice_coord = ""
+        slice_ax     = str(options.slice_info[0])
+        slice_coord  = float(options.slice_info[1])
         while slice_ax not in dim_map.keys():
             slice_ax    = read_var("Choose slice ax (x, y, z)      : ")
         while (slice_coord < dom_l[dim_map[slice_ax]]) or (slice_coord > dom_r[dim_map[slice_ax]]): # or slice_coord < -10000:
@@ -198,19 +219,20 @@ if f_run == True:
          except:
             die("Failed to construct field %s" % plot_field)
 
-    if (plot_field[-3:] != "tot" and plot_field[0:3] == "cre"):
+# For elegant labels when plot_field is cree?? or cren??
+    if (plot_field[-3:] != "tot" and plot_field[0:3] == "cre" and plot_field[3:-2] != "ratio"):
        if (plot_field[0:4] == "cree"):
-          disp_name="CR electron energy density (bin %2i)" %int(plot_field[-2:])
+          disp_name="energy"
           new_field_dimensions = dimensions.energy/dimensions.volume
-          new_field_units      = dsSlice[plot_field].units
        elif (plot_field[0:4] == "cren"):
-          disp_name="CR electron number density (bin %2i)" %int(plot_field[-2:])
+          disp_name="number"
           new_field_dimensions = 1./dimensions.volume
-          new_field_units      = dsSlice[plot_field].units
        prtinfo( "Adding display name: %s" %disp_name)
-       plot_field_new = str(plot_field+"_updated")
-       h5ds.add_field(("gdf",plot_field_new), units=new_field_units, function=copy_field, display_name=disp_name,dimensions=new_field_dimensions, sampling_type="cell")
-       plot_field = plot_field_new
+       disp_name="CR electron %s density (bin %2i)" %(disp_name, int(plot_field[-2:]) )
+       new_field            = str(plot_field+"_updated")
+       new_field_units      = dsSlice[plot_field].units
+       h5ds.add_field(("gdf", new_field), units=new_field_units, function=copy_field, display_name=disp_name, dimensions=new_field_dimensions, sampling_type="cell")
+       plot_field = new_field
 
     click_coords = [0, 0]
     image_number = 0
@@ -219,32 +241,41 @@ if f_run == True:
 
     w = dom_r[avail_dim[0]] + abs(dom_l[avail_dim[0]])
     h = dom_r[avail_dim[1]] + abs(dom_l[avail_dim[1]])
-    if (plot_field[0:-2] != "en_ratio"):
-      frb = np.array(dsSlice.to_frb(w, resolution, height=h)[plot_field])
-      plot_max   = h5ds.find_max(plot_field)[0]
-      plot_units = str(plot_max).split(' ')[1]
-      plot_min   = h5ds.find_min(plot_field)[0]
-    else:
-      frb = np.array(dsSlice.to_frb(w, resolution, height=h)[plot_field])
-      plot_min = h5ds.find_min(plot_field)[0]
-      plot_max = h5ds.find_max(plot_field)[0]
-      h5ds.find_max("cre"+plot_var+str(plot_field[-2:]))[0]
-      plot_units = "Msun*pc**2/Myr**2"
 
-    plt.xlabel("Domain cooridnates "+dim_map.keys()[dim_map.values().index(avail_dim[0])]+" ("+length_unit+")" )
-    plt.ylabel("Domain cooridnates "+dim_map.keys()[dim_map.values().index(avail_dim[1])]+" ("+length_unit+")" )
-    plt.colormap="plasma"
+    if (plot_field[0:-2] != "en_ratio"):
+      frb = np_array(dsSlice.to_frb(w, resolution, height=h)[plot_field])
+      if (not user_limits): plot_max = h5ds.find_max(plot_field)[0]
+      if (not user_limits): plot_min = h5ds.find_min(plot_field)[0]
+      plot_units = str(h5ds.all_data()[plot_field].units)
+    else:               #== "en_ratio"
+      frb = np_array(dsSlice.to_frb(w, resolution, height=h)[plot_field])
+      if (not user_limits): plot_min = h5ds.find_min(plot_field)[0]
+      if (not user_limits): plot_max = h5ds.find_max(plot_field)[0]
+      plot_units = str(h5ds.all_data()[plot_field].units)
+
     if (user_limits == True): # Overwrites previously found values
        plot_min = plot_user_min
        plot_max = plot_user_max
 
+    plt.xlabel("Domain cooridnates "+dim_map.keys()[dim_map.values().index(avail_dim[0])]+" ("+length_unit+")" )
+    plt.ylabel("Domain cooridnates "+dim_map.keys()[dim_map.values().index(avail_dim[1])]+" ("+length_unit+")" )
+    plt.colormap="plasma"
+
+    encountered_nans = False
     plot_max   = float(plot_max)
-    plot_min   = max(float(plot_min), plot_user_min)
+    plot_min   = max(float(plot_min), par_epsilon)
+    if (isnan(plot_min)==True or isnan(plot_max)==True): encountered_nans = True; prtwarn("Invalid data encountered (NaN), ZLIM will be adjusted")
 
     if (use_logscale):
-        plt.imshow(frb,extent=[dom_l[avail_dim[0]], dom_r[avail_dim[0]], dom_l[avail_dim[1]], dom_r[avail_dim[1]] ], origin="lower" ,norm=LogNorm(vmin=plot_min, vmax=plot_max))
+        if not encountered_nans:
+            plt.imshow(frb,extent=[dom_l[avail_dim[0]], dom_r[avail_dim[0]], dom_l[avail_dim[1]], dom_r[avail_dim[1]] ], origin="lower" ,norm=LogNorm(vmin=plot_min, vmax=plot_max))
+        else:
+            plt.imshow(frb,extent=[dom_l[avail_dim[0]], dom_r[avail_dim[0]], dom_l[avail_dim[1]], dom_r[avail_dim[1]] ], origin="lower" ,norm=LogNorm())
     else:
-        plt.imshow(frb,extent=[dom_l[avail_dim[0]], dom_r[avail_dim[0]], dom_l[avail_dim[1]], dom_r[avail_dim[1]] ], origin="lower")
+        if not encountered_nans:
+            plt.imshow(frb,extent=[dom_l[avail_dim[0]], dom_r[avail_dim[0]], dom_l[avail_dim[1]], dom_r[avail_dim[1]] ], origin="lower", vmin=plot_min, vmax=plot_max)
+        else:
+            plt.imshow(frb,extent=[dom_l[avail_dim[0]], dom_r[avail_dim[0]], dom_l[avail_dim[1]], dom_r[avail_dim[1]] ], origin="lower")
     plt.title("Component: "+plot_field+" | t = %9.3f Myr"  %time)
 
     try:
@@ -260,7 +291,7 @@ if f_run == True:
 
     if (user_annot_line == True):
        prtinfo("Marking line on yt.plot at (0 0 0) : (500 500 0)")
-       yt_data_plot.annotate_line((0., 0., 0.), (500., 500.0, 0), plot_args={'color':'white',"lw":2.0} )#, coord_system='axis')
+       yt_data_plot.annotate_line((0., 0., 0.), (500., 500.0, 0), plot_args={'color':'white',"lw":2.0} )
     else:
        prtinfo("Not marking line on yt.plot (user_annot_line = %s)" %(user_annot_line))
 
@@ -334,7 +365,7 @@ if f_run == True:
 
             image_number=image_number+1
 #------------- saving just the spectrum
-            if (user_save_spectrum):
+            if (user_save_spec):
                extent = fig2.get_window_extent().transformed(s.dpi_scale_trans.inverted())
                s.savefig('results/'+filename_nam+'_'+plot_var+'_spectrum_%04d.pdf' % image_number, transparent ='True', bbox_inches=extent.expanded(1.4, 1.195),quality=95,dpi=150)
                prtinfo("  --->  Saved plot to: %s.\n\033[44mPress 'q' to quit and save yt.SlicePlot with marked coordinates." %str('results/'+filename_nam+'_'+plot_var+'_spectrum_%04d.pdf' %image_number))
