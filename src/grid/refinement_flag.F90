65df7e53ae043d065b383bce7ed674cd08e0b94b
Artur Gawryszczak <gawrysz@gmail.com>
2020-02-06 02:04:37 +0100

    [refinement_flag] added accessor functions for private map

--- src/grid/refinement_flag.F90
+++ src/grid/refinement_flag.F90
@@ -45,15 +45,29 @@ module refinement_flag
    end type SFC_candidate
 
    type :: ref_flag
-      logical :: refine   !> a request to refine, deprecated: use cg%refinemap whenever possible
+      logical :: refine   !> a request to refine, deprecated: use this%map whenever possible
       logical :: derefine !> a request to derefine
       type(SFC_candidate), allocatable, dimension(:) :: SFC_refine_list
       logical, private, allocatable, dimension(:,:,:) :: map  !> .true. when a cell triggers refinement criteria, .false. otherwise
    contains
-      procedure :: init      !> Initialize: (.false. , .false., allocate 0 elements)
-      procedure :: add       !> Appends one element to SFC_refine_list
-      procedure :: sanitize  !> Sanitize the refinement flags
-      procedure :: initmap   !> Allocate map
+      procedure          :: init           !> Initialize: (.false. , .false., allocate 0 elements)
+      procedure          :: add            !> Appends one element to SFC_refine_list
+      procedure          :: sanitize       !> Sanitize the refinement flags
+      procedure          :: initmap        !> Allocate map
+      procedure, private :: set_cell       !> Mark a cell for refinement
+!      procedure, private :: set_range      !> Mark a box of cells for refinement (list of indices)
+      procedure, private :: set_arrng      !> Mark a box of cells for refinement (se-type array of indices)
+      procedure, private :: set_all        !> Mark all cells for refinement
+      procedure, private :: set_mask       !> Mark cells according to mask
+      procedure, private :: get_cell       !> Read refinement status of a cell
+      procedure, private :: get_any_range  !> Read refinement status of a box of cells (list of indices)
+      procedure, private :: get_any_arrng  !> Read refinement status of a box of cells (se-type array of indices)
+      procedure, private :: get_any        !> Read refinement status of all cells
+      procedure, private :: clear_mask     !> Unmark cells according to mask
+      procedure, private :: clear_all      !> Unmark all cells
+      generic,   public  :: set   => set_cell, set_arrng, set_all, set_mask  !, set_range
+      generic,   public  :: get   => get_cell, get_any_range, get_any_arrng, get_any
+      generic,   public  :: clear => clear_mask, clear_all
    end type ref_flag
 
 contains
@@ -73,11 +87,11 @@ contains
       if (allocated(this%SFC_refine_list)) deallocate(this%SFC_refine_list)
       allocate(this%SFC_refine_list(0))
       if (.not. allocated(this%map)) call die("[refinement_flag:init] map not allocated")
-      this%map = .false.
+      call this%clear
 
    end subroutine init
 
-!> \brief allocate map
+!> \brief Allocate map
 
    subroutine initmap(this, ijkse)
 
@@ -93,6 +107,224 @@ contains
       allocate(this%map(ijkse(xdim, LO):ijkse(xdim, HI), ijkse(ydim, LO):ijkse(ydim, HI), ijkse(zdim, LO):ijkse(zdim, HI)))
 
    end subroutine initmap
+
+!> \brief  Read refinement status of a cell
+
+   logical function get_cell(this, i, j, k) result (refine)
+
+#ifdef DEBUG
+      use dataio_pub, only: die
+#endif /* DEBUG */
+
+      implicit none
+
+      class(ref_flag), intent(inout) :: this     !> object invoking this procedure
+      integer(kind=4), intent(in)    :: i, j, k  !> cell coordinates
+
+#ifdef DEBUG
+      if (any([i, j, k] < lbound(this%map)) .or. any([i, j, k] > ubound(this%map))) call die("[refinement_flag:set_cell] out of range")  ! this can be costly check
+#endif /* DEBUG */
+
+      refine = this%map(i, j, k)
+
+   end function get_cell
+
+!> \brief  Read refinement status of all cells
+
+   logical function get_any(this) result (refine)
+
+      implicit none
+
+      class(ref_flag), intent(inout) :: this  !> object invoking this procedure
+
+      refine = any(this%map)
+
+   end function get_any
+
+!> \brief  Read refinement status of a box of cells (list of indices)
+
+   logical function get_any_range(this, il, ih, jl, jh, kl, kh, mask) result (any_refine)
+
+#ifdef DEBUG
+      use dataio_pub, only: die
+#endif /* DEBUG */
+
+      implicit none
+
+      class(ref_flag), intent(inout) :: this     !> object invoking this procedure
+      integer(kind=4), intent(in)    :: il, jl, kl, ih, jh, kh  !> box coordinates
+      logical, dimension(il:ih, jl:jh, kl:kh), optional, intent(in) :: mask
+
+#ifdef DEBUG
+      if (any([il, jl, kl] < lbound(this%map)) .or. any([il, jl, kl] > ubound(this%map))) call die("[refinement_flag:get_any_range] out of range (l)")  ! this can be costly check
+      if (any([ih, jh, kh] < lbound(this%map)) .or. any([ih, jh, kh] > ubound(this%map))) call die("[refinement_flag:get_any_range] out of range (h)")  ! this can be costly check
+#endif /* DEBUG */
+
+      if (present(mask)) then
+         any_refine = any(this%map(il:ih, jl:jh, kl:kh) .and. mask)
+      else
+         any_refine = any(this%map(il:ih, jl:jh, kl:kh))
+      endif
+
+   end function get_any_range
+
+!> \brief  Read refinement status of a box of cells (se-type array of indices)
+
+   logical function get_any_arrng(this, se, mask) result (any_refine)
+
+      use constants,  only: xdim, ydim, zdim, LO, HI
+#ifdef DEBUG
+      use dataio_pub, only: die
+#endif /* DEBUG */
+
+      implicit none
+
+      class(ref_flag), intent(inout) :: this  !> object invoking this procedure
+      integer(kind=8), dimension(xdim:zdim, LO:HI), intent(in) :: se  !> box coordinates
+      logical, dimension(se(xdim,LO):se(xdim,HI), se(ydim,LO):se(ydim,HI), se(zdim,LO):se(zdim,HI)), optional, intent(in) :: mask
+
+#ifdef DEBUG
+      if (any(se < lbound(this%map)) .or. any(se > ubound(this%map))) call die("[refinement_flag:get_any_arrng] out of range")  ! this can be costly check
+#endif /* DEBUG */
+
+      if (present(mask)) then
+         any_refine = any(this%map(se(xdim,LO):se(xdim,HI), se(ydim,LO):se(ydim,HI), se(zdim,LO):se(zdim,HI)) .and. mask)
+      else
+         any_refine = any(this%map(se(xdim,LO):se(xdim,HI), se(ydim,LO):se(ydim,HI), se(zdim,LO):se(zdim,HI)))
+      endif
+
+   end function get_any_arrng
+
+!> \brief Mark a cell for refinement
+
+   subroutine set_cell(this, i, j, k)
+
+#ifdef DEBUG
+      use dataio_pub, only: die
+#endif /* DEBUG */
+
+      implicit none
+
+      class(ref_flag), intent(inout) :: this     !> object invoking this procedure
+      integer(kind=4), intent(in)    :: i, j, k  !> cell coordinates
+
+#ifdef DEBUG
+      if (any([i, j, k] < lbound(this%map)) .or. any([i, j, k] > ubound(this%map))) call die("[refinement_flag:set_cell] out of range")  ! this can be costly check
+#endif /* DEBUG */
+
+      this%map(i, j, k) = .true.
+
+   end subroutine set_cell
+
+!> \brief Mark a box of cells for refinement
+
+!!$   subroutine set_range(this, il, ih, jl, jh, kl, kh)
+!!$
+!!$#ifdef DEBUG
+!!$      use dataio_pub, only: die
+!!$#endif /* DEBUG */
+!!$
+!!$      implicit none
+!!$
+!!$      class(ref_flag), intent(inout) :: this                    !> object invoking this procedure
+!!$      integer(kind=4), intent(in)    :: il, jl, kl, ih, jh, kh  !> box coordinates
+!!$
+!!$#ifdef DEBUG
+!!$      if (any([il, jl, kl] < lbound(this%map)) .or. any([il, jl, kl] > ubound(this%map))) call die("[refinement_flag:set_range] out of range (l)")  ! this can be costly check
+!!$      if (any([ih, jh, kh] < lbound(this%map)) .or. any([ih, jh, kh] > ubound(this%map))) call die("[refinement_flag:set_range] out of range (h)")  ! this can be costly check
+!!$#endif /* DEBUG */
+!!$
+!!$      this%map(il:ih, jl:jh, kl:kh) = .true.
+!!$
+!!$   end subroutine set_range
+
+!> \brief Mark a box of cells for refinement (se-type array of indices)
+
+   subroutine set_arrng(this, se)
+
+      use constants,  only: xdim, ydim, zdim, LO, HI
+#ifdef DEBUG
+      use dataio_pub, only: die
+#endif /* DEBUG */
+
+      implicit none
+
+      class(ref_flag), intent(inout) :: this  !> object invoking this procedure
+      integer(kind=8), dimension(xdim:zdim, LO:HI), intent(in) :: se  !> box coordinates
+
+#ifdef DEBUG
+      if (any(se < lbound(this%map)) .or. any(se > ubound(this%map))) call die("[refinement_flag:set_arrng] out of range")  ! this can be costly check
+#endif /* DEBUG */
+
+      this%map(se(xdim,LO):se(xdim,HI), se(ydim,LO):se(ydim,HI), se(zdim,LO):se(zdim,HI)) = .true.
+
+   end subroutine set_arrng
+
+!> \brief Mark a box of cells for refinement
+
+   subroutine set_all(this, value)
+
+      implicit none
+
+      class(ref_flag),   intent(inout) :: this   !> object invoking this procedure
+      logical, optional, intent(in)    :: value  !> used to clear flags
+
+      if (present(value)) then
+         this%map = value
+      else
+         this%map = .true.
+      endif
+
+   end subroutine set_all
+
+!> \brief Mark cells according to mask
+
+   subroutine set_mask(this, mask)
+
+      use dataio_pub, only: die
+
+      implicit none
+
+      class(ref_flag),           intent(inout) :: this  !> object invoking this procedure
+      logical, dimension(:,:,:), intent(in)    :: mask  !> set flags according to this mask, preserve flags already set
+
+      if (any(shape(mask) /= shape(this%map))) call die("[refinement_flag::set_mask] wrong mask shape")
+
+      this%map = this%map .or. mask
+
+   end subroutine set_mask
+
+!> \brief Unmark cells according to mask
+
+   subroutine clear_mask(this, mask)
+
+      implicit none
+
+      class(ref_flag),                        intent(inout) :: this  !> object invoking this procedure
+      logical, dimension(:,:,:), allocatable, intent(in)    :: mask  !> set flags according to this mask, preserve flags already set
+      ! allocatable is required to get proper bounds, as they were allocated
+
+      if (any(lbound(mask) /= lbound(this%map)) .or. any(ubound(mask) /= ubound(this%map))) then
+         ! we expect a leafmap to be passed here, hence we don't calculate overlap (it would be easy, just more lenghty)
+         this%map     (lbound(mask, dim=1):ubound(mask, dim=1), lbound(mask, dim=2):ubound(mask, dim=2), lbound(mask, dim=3):ubound(mask, dim=3)) = &
+              this%map(lbound(mask, dim=1):ubound(mask, dim=1), lbound(mask, dim=2):ubound(mask, dim=2), lbound(mask, dim=3):ubound(mask, dim=3)) .and. mask
+      else
+         this%map = this%map .and. mask
+      endif
+
+   end subroutine clear_mask
+
+!> \brief Unmark cells according to mask
+
+   subroutine clear_all(this)
+
+      implicit none
+
+      class(ref_flag), intent(inout) :: this  !> object invoking this procedure
+
+      this%map = .false.
+
+   end subroutine clear_all
 
 !> \brief Sanitize the refinement flags
 
