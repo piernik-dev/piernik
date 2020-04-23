5400328f9008960d60e4d528d362fb35728ec17c
Artur Gawryszczak <gawrysz@gmail.com>
2020-04-23 13:38:02 +0200

    [ppp_plot.py] simplifications

--- python/ppp_plot.py
+++ python/ppp_plot.py
@@ -8,17 +8,18 @@ t_bias = 10  # I'd prefer to reduce the timers initially to 0, but sometimes sla
 
 
 class PPP_Node:
-    """A single event and links"""
+    """A single node on tree of events with all event enties and links"""
 
     parent = None
     proc = None
     label = ""
 
-    def __init__(self, label, time):
+    def __init__(self, label, time=None):
         self.label = label
         self.start = []
         self.stop = []
-        self._set_time(time)
+        if time is not None:
+            self._set_time(time)
         self.children = {}  # this flattens the tree a bit ut we can recontruct it from timings anyway, if needed
         self.parent = None
 
@@ -55,9 +56,9 @@ class PPP_Node:
         return (str(self.proc) if self.parent is None else self.parent.path()) + "/" + self.label
 
     def shortpath(self):
-        return ("" if self.parent is None else self.parent.shortpath() + "/") + self.label
+        return "" if self.parent is None else self.parent.shortpath() + "/" + self.label
 
-    def print(self, indent=1):  # need to filter through parent range
+    def print(self, indent=0):  # need to filter through parent range
         try:
             for _ in range(min(len(self.start), len(self.stop))):
                 print("  " * indent + "'" + self.label + "' %.6f %.6f" % (self.start[_], self.stop[_] - self.start[_]))
@@ -67,7 +68,7 @@ class PPP_Node:
             self.children[i].print(indent=indent + 1)
 
     def get_all_ev(self):
-        evl = [[self.shortpath(), self.start, self.stop, self.own_time, self.child_time]]
+        evl = [[self.shortpath(), self.start, self.stop, self.own_time, self.child_time]] if len(self.start) > 0 else []
         for i in self.children:
             evl += self.children[i].get_all_ev()
         return evl
@@ -94,33 +95,24 @@ class PPP_Tree:
 
     def __init__(self, name):
         self.thread = name
-        self._last = None
-        self.root = {}
+        self.root = PPP_Node("/")  # root node is special and does not have time
+        self.root.proc = self.thread
+        self._last = self.root
 
     def _add(self, label, time):
-        if self._last is None:
-            self.root[label] = PPP_Node(label, time)
-            self.root[label].proc = self.thread
-            self._last = self.root[label]
-        else:
-            self._last = self._last._add(label, time)
+        self._last = self._last._add(label, time)
 
     def print(self):
         print("Process: " + str(self.thread))
-        for i in self.root:
-            self.root[i].print()
-
-    def calculate_time(self):
-        for _ in self.root:
-            self.root[_].calculate_time()
+        self.root.print()
 
 
 class PPP:
-    """A collection of event trees from one or many Piernik processes"""
+    """A collection of event trees from one or many Piernik processes from a single run"""
 
     def __init__(self, name):
         self.name = name
-        self.trees = {}
+        self.trees = {}  # separate event tree for each process
 
     def print(self):
         print(self.name)
@@ -128,22 +120,17 @@ class PPP:
             self.trees[i].print()
 
     def _decode_text(self):
-        self.descr = ""
         self.nthr = 0
         self.bigbang = None
-        try:
-            file = open(self.name, 'r')
-            for line in file:
-                l = line.split()
-                if self.bigbang is None:
-                    self.bigbang = float(l[1]) - t_bias
-                self._add(int(l[0]), line[line.index(l[2]):].strip(), float(l[1]) + self.bigbang * (-1. if float(l[1]) > 0. else 1.))  # proc, label, time
-                if int(l[0]) >= self.nthr:
-                    self.nthr = int(l[0]) + 1
-            self.descr = "'%s' (%d thread%s)" % (self.name, self.nthr, "s" if self.nthr > 1 else "")
-        except IOError:
-            sys.stderr.write("Error: cannot open '" + self.name + "'\n")
-            exit(4)
+        file = open(self.name, 'r')
+        for line in file:
+            l = line.split()
+            if self.bigbang is None:
+                self.bigbang = float(l[1]) - t_bias
+            self._add(int(l[0]), line[line.index(l[2]):].strip(), float(l[1]) + self.bigbang * (-1. if float(l[1]) > 0. else 1.))  # proc, label, time
+            if int(l[0]) >= self.nthr:
+                self.nthr = int(l[0]) + 1
+        self.descr = "'%s' (%d thread%s)" % (self.name, self.nthr, "s" if self.nthr > 1 else "")
 
     def _add(self, proc, label, time):
         if proc not in self.trees:
@@ -153,40 +140,33 @@ class PPP:
     def calculate_time(self):
         self.total_time = 0.
         for _ in self.trees:
-            self.trees[_].calculate_time()
-            for r in self.trees[_].root:
-                self.total_time += self.trees[_].root[r].own_time
+            self.trees[_].root.calculate_time()
+            self.total_time += self.trees[_].root.child_time  # root does not have time
 
     def apply_exclusions(self):
         if args.exclude is not None:
             for p in self.trees:
-                for r in list(self.trees[p].root.keys()):
-                    if self.trees[p].root[r].label in args.exclude:
-                        del self.trees[p].root[r]
-                    else:
-                        self.trees[p].root[r].apply_exclusions()
+                self.trees[p].root.apply_exclusions()
 
 
 class PPPset:
     """A collection of event trees from one or many Piernik runs"""
 
-    def __init__(self):
-        self.evt = []
-
-    def decode(self, fnamelist):
-        """Let's focus on ASCII decoding for a while. Don't bother with HDF5 unltil we implement this type of PPP dump"""
-
+    def __init__(self, fnamelist):
+        """Let's focus on ASCII decoding for a while. Don't bother with HDF5 until we implement such type of PPP dump"""
+        self.run = {}
         for fname in fnamelist:
-            self.evt.append(PPP(fname))
-            self.evt[-1]._decode_text()
-            self.evt[-1].apply_exclusions()
-            self.evt[-1].calculate_time()
+            self.run[fname] = PPP(fname)
+        for _ in self.run:
+            self.run[_]._decode_text()
+            self.run[_].apply_exclusions()
+            self.run[_].calculate_time()
 
     def print(self, otype):
         self.out = ""
         if otype == "tree":
-            for _ in range(len(self.evt)):
-                self.evt[_].print()
+            for _ in self.run:
+                self.run[_].print()
         elif otype == "gnu":
             self.print_gnuplot()
             if args.output is None:
@@ -198,57 +178,52 @@ class PPPset:
         elif otype == "summary":
             # ToDo: add own time, %time and "called from"
             print("ARGS: ", args)
-            for f in range(len(self.evt)):
+            for f in self.run:
                 ed = {}
-                print("\n## File: '%s', %d threads, bigbang = %.7f" % (self.evt[f].name, self.evt[f].nthr, self.evt[f].bigbang))
-                total_time = 0.
-                for p in self.evt[f].trees:
-                    for r in self.evt[f].trees[p].root:
-                        for e in self.evt[f].trees[p].root[r].get_all_ev():
-                            e_base = e[0].split('/')[-1]
-                            if e_base not in ed:
-                                ed[e_base] = [0, 0., 0.]  # calls, cumul. own time, cumul. child. time
-                            ed[e_base][0] += len(e[1])
-                            ed[e_base][1] += e[3]
-                            ed[e_base][2] += e[4]
-                            total_time += e[3] - e[4]
+                print("\n## File: '%s', %d threads, bigbang = %.7f" % (self.run[f].name, self.run[f].nthr, self.run[f].bigbang))
+                for p in self.run[f].trees:
+                    for e in self.run[f].trees[p].root.get_all_ev():
+                        e_base = e[0].split('/')[-1]
+                        if e_base not in ed:
+                            ed[e_base] = [0, 0., 0.]  # calls, cumul. own time, cumul. child. time
+                        ed[e_base][0] += len(e[1])
+                        ed[e_base][1] += e[3]
+                        ed[e_base][2] += e[4]
                 ml = len(max(ed, key=len))
                 print("# %-*s %20s %8s %15s \033[97m%20s\033[0m %10s" % (ml - 2, "label", "avg. CPU time (s)", "%time", "avg. occurred", "avg. non-child", "% of total"))
                 print("# %-*s %20s %8s %15s \033[97m%20s\033[0m %10s" % (ml - 2, "", "(per thread)", "in child", "(per thread)", "time (s)", "time"))
                 skipped = [0, 0.]
                 for e in sorted(ed.items(), key=lambda x: x[1][1] - x[1][2], reverse=True):
-                    if (e[1][1] - e[1][2]) / total_time > args.cutsmall[0] / 100.:
-                        print("%-*s %20.6f %8s %15d%s \033[97m%20.6f\033[0m %10.2f" % (ml, e[0], e[1][1] / self.evt[f].nthr,
+                    if (e[1][1] - e[1][2]) / self.run[f].total_time > args.cutsmall[0] / 100.:
+                        print("%-*s %20.6f %8s %15d%s \033[97m%20.6f\033[0m %10.2f" % (ml, e[0], e[1][1] / self.run[f].nthr,
                                                                                        ("" if e[1][2] == 0. else "%8.2f" % ((100 * e[1][2] / e[1][1]) if e[1][1] > 0. else 0.)),
-                                                                                       e[1][0] / self.evt[f].nthr,
-                                                                                       "" if e[1][0] % self.evt[f].nthr == 0 else "+",
-                                                                                       (e[1][1] - e[1][2]) / self.evt[f].nthr, 100. * (e[1][1] - e[1][2]) / total_time))
+                                                                                       e[1][0] / self.run[f].nthr,
+                                                                                       "" if e[1][0] % self.run[f].nthr == 0 else "+",
+                                                                                       (e[1][1] - e[1][2]) / self.run[f].nthr, 100. * (e[1][1] - e[1][2]) / self.run[f].total_time))
                     else:
                         skipped[0] += 1
-                        skipped[1] += (e[1][1] - e[1][2]) / self.evt[f].nthr
+                        skipped[1] += (e[1][1] - e[1][2]) / self.run[f].nthr
                 if skipped[0] > 0:
-                    print("# (skipped %d timers that contributed %.6f seconds of non-child time = %.2f%% of total time)" % (skipped[0], skipped[1], 100. * self.evt[f].nthr * skipped[1] / total_time))
+                    print("# (skipped %d timers that contributed %.6f seconds of non-child time = %.2f%% of total time)" % (skipped[0], skipped[1], 100. * self.run[f].nthr * skipped[1] / self.run[f].total_time))
             # ToDo: merged summary with data presented side-by-side
 
     def print_gnuplot(self):
         self.descr = ""
         ev = {}
         peff = 0
-        for f in range(len(self.evt)):
-            for p in self.evt[f].trees:
-                evlist = []
-                for r in self.evt[f].trees[p].root:
-                    evlist += self.evt[f].trees[p].root[r].get_all_ev()
+        for f in self.run:
+            for p in self.run[f].trees:
+                evlist = self.run[f].trees[p].root.get_all_ev()
                 for e in evlist:
                     e_base = e[0].split('/')[-1].split()[0]
                     if e_base not in ev:
                         ev[e_base] = {}
                     if p + peff not in ev[e_base]:
                         ev[e_base][p + peff] = []
-                    if e[3] > args.cutsmall[0] / 100. * self.evt[f].total_time / self.evt[f].nthr:
+                    if e[3] > args.cutsmall[0] / 100. * self.run[f].total_time / self.run[f].nthr:
                         ev[e_base][p + peff].append(e)
-            peff = peff + self.evt[f].nthr + (1 if len(self.evt) > 1 else 0)  # spacing only when we have multiple files
-            self.descr = self.evt[f].descr + ("\\n" if len(self.descr) > 0 else "") + self.descr
+            peff = peff + self.run[f].nthr + (1 if len(self.run) > 1 else 0)  # spacing only when we have multiple files
+            self.descr = self.run[f].descr + ("\\n" if len(self.descr) > 0 else "") + self.descr
 
         ndel = []
         for e in ev:
@@ -276,7 +251,7 @@ class PPPset:
             self.out += "# __" + e + "__ '" + ftname + "'\n"
             for p in ev[e]:
                 for t in range(len(ev[e][p])):
-                    depth = ev[e][p][t][0].count("/")
+                    depth = ev[e][p][t][0].count("/") - 1
                     for _ in range(min(len(ev[e][p][t][i_s]), len(ev[e][p][t][i_e]))):
                         try:
                             if (ev[e][p][t][i_e][_] - ev[e][p][t][i_s][_]) > 0.:
@@ -332,12 +307,9 @@ pgroup.add_argument("-g", "--gnuplot", action="store_const", dest="otype", const
 pgroup.add_argument("-t", "--tree", action="store_const", dest="otype", const="tree", help="tree output")
 pgroup.add_argument("-s", "--summary", action="store_const", dest="otype", const="summary", help="short summary")
 
-
 args = parser.parse_args()
-
-evt = PPPset()
-evt.decode(args.filename)
-evt.print(args.otype)
+all_events = PPPset(args.filename)
+all_events.print(args.otype)
 
 # for j  in *.ppprofile.ascii ; do for i in `awk '{print $2}' $j |  sort |  uniq` ; do in=`grep "$i *  -" $j | wc -l` ; out=`grep "$i *  [1-9]" $j | wc -l` ; [ $in != $out ] && echo $j $i $in $out ; done |  column -t ; done
 
