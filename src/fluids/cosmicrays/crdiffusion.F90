ec43fba1fcc690c8d00196c0afd25baf242c169d
Artur Gawryszczak <gawrysz@gmail.com>
2020-04-27 00:26:41 +0200

    a sketch of solution

--- src/fluids/cosmicrays/crdiffusion.F90
+++ src/fluids/cosmicrays/crdiffusion.F90
@@ -72,8 +72,8 @@ contains
    subroutine all_wcr_boundaries
 
       use cg_leaves,        only: leaves
+      use cg_level_finest,  only: finest
       use cg_list,          only: cg_list_element
-      use cg_list_global,   only: all_cg
       use constants,        only: ndims, xdim, ydim, zdim, LO, HI, BND_PER, BND_MPI, BND_FC, BND_MPI_FC, I_TWO, I_THREE, wcr_n
       use dataio_pub,       only: die
       use domain,           only: dom
@@ -90,8 +90,10 @@ contains
 
       if (.not. has_cr) return
 
-      call all_cg%internal_boundaries_4d(wna%ind(wcr_n)) ! should be more selective (modified leaves?)
+      call finest%level%restrict_to_base_w_1var(wna%ind(wcr_n))
+      call leaves%leaf_arr4d_boundaries(wna%ind(wcr_n))
 
+      ! do the external boundaries
       cgl => leaves%first
       do while (associated(cgl))
          cg => cgl%cg
@@ -106,7 +108,6 @@ contains
                      case (BND_PER)
                      case (BND_MPI) !, BND_MPI_FC)
                      case (BND_FC, BND_MPI_FC)
-                        call die("[crdiffusion:all_wcr_boundaries] fine-coarse interfaces are not implemented here")
                      case default ! Set gradient == 0 on the external boundaries
                         r(d,:) = cg%ijkse(d,lh)
                         do i = 1, dom%nb
