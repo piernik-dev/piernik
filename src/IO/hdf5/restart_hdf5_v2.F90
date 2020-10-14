07c20de92e052c9ba2e2061fe488928c2ca38d9e
Artur Gawryszczak <gawrysz@gmail.com>
2020-10-14 13:29:24 +0200

    [IO] MPI_DOUBLE_INT imported inside #ifdef NBODY_1FILE

--- src/IO/hdf5/restart_hdf5_v2.F90
+++ src/IO/hdf5/restart_hdf5_v2.F90
@@ -208,14 +208,15 @@ contains
       use dataio_pub,       only: die, nproc_io, can_i_write
       use grid_cont,        only: grid_container
       use hdf5,             only: HID_T, HSIZE_T, H5T_NATIVE_DOUBLE, h5sclose_f, h5dwrite_f, h5sselect_none_f, h5screate_simple_f
-      use MPIF,             only: MPI_DOUBLE_PRECISION, MPI_STATUS_IGNORE, MPI_DOUBLE_INT, MPI_COMM_WORLD, MPI_Recv, MPI_Send
+      use MPIF,             only: MPI_DOUBLE_PRECISION, MPI_STATUS_IGNORE, MPI_COMM_WORLD, MPI_Recv, MPI_Send
       use mpisetup,         only: master, FIRST, proc, err_mpi, tag_ub, LAST
       use named_array_list, only: qna, wna
       use ppp,              only: ppp_main
 #ifdef NBODY_1FILE
-      use cg_particles_io, only: pdsets, nbody_datafields
-      use particle_utils,  only: count_all_particles
+      use cg_particles_io,  only: pdsets, nbody_datafields
       use data_hdf5,        only: gdf_translate
+      use MPIF,             only: MPI_DOUBLE_INT
+      use particle_utils,   only: count_all_particles
 #endif /* NBODY_1FILE */
 
       implicit none
