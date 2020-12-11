cd29718e4d67cd81d938bc38d18225b80bc939f7
mogrodnik <502260@doktorant.umk.pl>
2020-12-11 11:28:27 +0100

    [cresp_io_common] Add subroutine to explicitly create double precision 2-dim dataset
    (Can be later moved to helpers_hdft:create_dataset interface)

--- src/fluids/cosmicrays/cresp_io_common.F90
+++ src/fluids/cosmicrays/cresp_io_common.F90
@@ -73,4 +73,38 @@ module cresp_io_common
    end type map_header
 
    type(map_header), dimension(LO:HI)  :: hdr_io
+
+   contains
+
+!
+!> \brief Create double precision real dataset (rank-2 array) in the given place.
+!
+   subroutine create_dataset_real8_dim2(place, dname, ddata)   ! TODO might be moved to helpers_hdf5:create_dataset interface
+
+      use constants,     only: I_TWO
+      use hdf5,          only: h5dcreate_f, h5dclose_f, h5kind_to_type, h5sclose_f, h5screate_simple_f, &
+          &                    h5dwrite_f, HID_T, HSIZE_T, H5T_NATIVE_DOUBLE
+      use iso_c_binding, only: c_ptr, c_loc
+
+      implicit none
+
+      integer(HID_T),                intent(in) :: place !< object id where dataset will be created
+      character(len=*),              intent(in) :: dname !< name of dataset
+      real, pointer, dimension(:,:), intent(in) :: ddata !< data used to create dataset
+
+      integer(HID_T)                            :: dset, space!, mem_type
+      integer(kind=4)                           :: hdferr
+      integer(HSIZE_T), dimension(2)            :: dims
+      type(c_ptr)                               :: f_ptr
+
+      dims = shape(ddata)
+      call h5screate_simple_f(I_TWO, dims, space, hdferr)
+      call h5dcreate_f(place, dname, H5T_NATIVE_DOUBLE, space, dset, hdferr)
+      f_ptr = c_loc(ddata(1,1))
+      call h5dwrite_f(dset, H5T_NATIVE_DOUBLE, f_ptr, hdferr)
+      call h5dclose_f(dset,  hdferr)
+      call h5sclose_f(space, hdferr)
+
+   end subroutine create_dataset_real8_dim2
+
 end module cresp_io_common
