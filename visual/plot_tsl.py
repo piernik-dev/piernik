ca80443e933f217b13f78e41be233e9beb84eb3b
Dominik Wóltański <wolt@umk.pl>
2021-04-23 16:03:01 +0200

    [plot_tsl] print and enumerate header fields while no proper argument

--- visual/plot_tsl.py
+++ visual/plot_tsl.py
@@ -17,18 +17,31 @@ if len(args.files) < 1:
     parser.error("I need at least one tsl file")
 
 data = []
+nenough = True
+
+def print_header(fn, header):
+    print("There are following fields available in %s" % fn)
+    for ii, he in enumerate(header):
+        print(ii+1, he)
 
 for fn in args.files:
     f = open(fn, "r")
     tab = [line.strip() for line in f.readlines()]
     f.close()
     header = np.array(tab[0][1:].split())
+
     if args.f is None:
-        print("There are following fields available in %s" % fn)
-        print(header)
+        nenough = True
+    elif args.f[0] not in header:
+        print("Field %s not met in %s!" % (args.f[0], fn))
     else:
-        field = args.f[0]
-        fno = np.where(header == field)[0][0]
+        nenough = False
+    if nenough:
+        print_header(fn, header)
+        exit(0)
+
+    field = args.f[0]
+    fno = np.where(header == field)[0][0]
 
     tab_work = tab   # problems with map in python3+
     tab = np.array([[float(item) for item in line.split()]
