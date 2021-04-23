4637b9d66edb5ddce8f66acb22244de9e88e9167
Dominik Wóltański <wolt@umk.pl>
2021-04-23 17:02:35 +0200

    [plot_tsl] allow to plot many fields

--- visual/plot_tsl.py
+++ visual/plot_tsl.py
@@ -9,7 +9,7 @@ import argparse
 remove_comments = re.compile("(?!\#)", re.VERBOSE)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", nargs=1, default=None, help='variable to plot on vertical axis')
+parser.add_argument("-f", nargs='*', default=None, help='variable to plot on vertical axis')
 parser.add_argument("-x", nargs=1, default=['time'], help='variable to plot on horizontal axis')
 parser.add_argument("--xlim", nargs=2, default=None, help='scale span for horizontal axis')
 parser.add_argument("--ylim", nargs=2, default=None, help='scale span for vertical axis')
@@ -20,9 +20,6 @@ args = parser.parse_args()
 if len(args.files) < 1:
     parser.error("I need at least one tsl file")
 
-data = []
-nenough = True
-
 
 def print_header(fn, header):
     print("There are following fields available in %s" % fn)
@@ -30,26 +27,34 @@ def print_header(fn, header):
         print(ii+1, he)
 
 
+data = []
+field = []
+fno = []
+nenough = True
+
 for fn in args.files:
     f = open(fn, "r")
     tab = [line.strip() for line in f.readlines()]
     f.close()
     header = np.array(tab[0][1:].split())
 
-    if args.f is None:
-        nenough = True
-    elif args.f[0] not in header:
-        print("Field %s not met in %s!" % (args.f[0], fn))
-    elif args.x[0] not in header:
+    if args.f is not None:
+        for ff in args.f:
+            if ff not in header:
+                print("Field %s not met in %s!" % (ff, fn))
+            else:
+                field.append(ff)
+                fno.append(np.where(header == ff)[0][0])
+                nenough = False
+
+    if args.x[0] not in header:
         print("Field %s not met in %s!" % (args.x[0], fn))
-    else:
-        nenough = False
+        nenough = True
+
     if nenough:
         print_header(fn, header)
         exit(0)
 
-    field = args.f[0]
-    fno = np.where(header == field)[0][0]
     xtime = args.x[0]
     xno = np.where(header == xtime)[0][0]
 
@@ -63,14 +68,17 @@ ax = fig.add_subplot(111)
 if args.xlim is not None:
     plt.xlim(float(args.xlim[0]), float(args.xlim[1]))
 if args.ylim is not None:
+    print(args.ylim)
     plt.ylim(float(args.ylim[0]), float(args.ylim[1]))
 
 for i, fn in enumerate(data):
-    ax.plot(fn[:, xno], fn[:, fno], label=args.files[i])
+    for ii, ff in enumerate(fno):
+        ax.plot(fn[:, xno], fn[:, ff], label=field[ii]+' '+args.files[i])
 
 ax.legend()
-
-plt.ylabel(field)
+print(ax.get_ylim())
+print(field)
+plt.ylabel(' | '.join(field))
 plt.xlabel(xtime)
 plt.draw()
 if args.save is None:
