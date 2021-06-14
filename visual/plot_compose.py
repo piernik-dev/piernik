16311f64434e2c971d186ded0c45f90f77ff916a
Dominik Wóltański <wolt@umk.pl>
2021-06-14 15:23:53 +0200

    [plot_compose] add colorbar for particle mass histogram

--- visual/plot_compose.py
+++ visual/plot_compose.py
@@ -12,20 +12,22 @@ matplotlib.use('cairo')      # choose output format
 
 def draw_particles(ax, p1, p2, pm, nbins, min1, max1, min2, max2, drawd, pcolor, psize):
     if nbins > 1:
-        ax.hist2d(p1, p2, nbins, weights=pm, range=[[min1, max1], [min2, max2]], norm=matplotlib.colors.LogNorm(), cmap=pcolor)
+        ah = ax.hist2d(p1, p2, nbins, weights=pm, range=[[min1, max1], [min2, max2]], norm=matplotlib.colors.LogNorm(), cmap=pcolor)
         if not drawd:
             ax.set_facecolor('xkcd:black')
     else:
+        ah = []
         if psize <= 0:
             psize = matplotlib.rcParams['lines.markersize']**2
         ax.scatter(p1, p2, c=pcolor, marker=".", s=psize)
         ax.set_xlim(min1, max1)
         ax.set_ylim(min2, max2)
-    return ax
+    return ax, ah
 
 
 def plotcompose(pthfilen, var, output, options):
     umin, umax, cmap, pcolor, psize, sctype, cu, cx, cy, cz, drawd, drawp, nbins, uaxes = options
+    drawh = drawp and nbins > 1
     h5f = h5py.File(pthfilen, 'r')
     time = h5f.attrs['time'][0]
     utim = h5f['dataset_units']['time_unit'].attrs['unit']
@@ -33,6 +35,8 @@ def plotcompose(pthfilen, var, output, options):
     usc, ulen, uupd = rd.change_units(ulenf, uaxes)
     if drawd:
         uvar = h5f['dataset_units'][var].attrs['unit']
+    if drawh:
+        umass = h5f['dataset_units']['mass_unit'].attrs['unit']
     nx, ny, nz = h5f['simulation_parameters'].attrs['domain_dimensions']
     xmin, ymin, zmin = h5f['simulation_parameters'].attrs['domain_left_edge']
     xmax, ymax, zmax = h5f['simulation_parameters'].attrs['domain_right_edge']
@@ -75,7 +79,9 @@ def plotcompose(pthfilen, var, output, options):
         print('Plotted value range: ', vmin, vmax)
     fig = P.figure(1, figsize=(10, 10.5))
 
-    if drawd:
+    if drawd and drawh:
+        cbar_mode = 'none'
+    elif drawd or drawh:
         cbar_mode = 'single'
     else:
         cbar_mode = 'none'
@@ -86,7 +92,7 @@ def plotcompose(pthfilen, var, output, options):
     if drawd:
         a = ax.imshow(xz, origin="lower", extent=[xmin, xmax, zmin, zmax], vmin=vmin, vmax=vmax, interpolation='nearest', cmap=cmap)
     if drawp:
-        ax = draw_particles(ax, px, pz, pm, nbins, xmin, xmax, zmin, zmax, drawd, pcolor, psize)
+        ax, ah = draw_particles(ax, px, pz, pm, nbins, xmin, xmax, zmin, zmax, drawd, pcolor, psize)
     ax.set_xlabel("x [%s]" % pu.labelx()(ulen))
     ax.set_ylabel("z [%s]" % pu.labelx()(ulen))
 
@@ -94,7 +100,7 @@ def plotcompose(pthfilen, var, output, options):
     if drawd:
         a = ax.imshow(xy, origin="lower", extent=[ymin, ymax, xmin, xmax], vmin=vmin, vmax=vmax, interpolation='nearest', cmap=cmap)
     if drawp:
-        ax = draw_particles(ax, py, px, pm, nbins, ymin, ymax, xmin, xmax, drawd, pcolor, psize)
+        ax, ah = draw_particles(ax, py, px, pm, nbins, ymin, ymax, xmin, xmax, drawd, pcolor, psize)
     ax.set_ylabel("x [%s]" % pu.labelx()(ulen))
     ax.set_xlabel("y [%s]" % pu.labelx()(ulen))
     ax.set_title(timep)
@@ -103,15 +109,33 @@ def plotcompose(pthfilen, var, output, options):
     if drawd:
         a = ax.imshow(yz, origin="lower", extent=[ymin, ymax, zmin, zmax], vmin=vmin, vmax=vmax, interpolation='nearest', cmap=cmap)
     if drawp:
-        ax = draw_particles(ax, py, pz, pm, nbins, ymin, ymax, zmin, zmax, drawd, pcolor, psize)
+        ax, ah = draw_particles(ax, py, pz, pm, nbins, ymin, ymax, zmin, zmax, drawd, pcolor, psize)
     ax.set_xlabel("y [%s]" % pu.labelx()(ulen))
     ax.set_ylabel("z [%s]" % pu.labelx()(ulen))
 
+    if drawh:
+        if cbar_mode == 'none':
+            bar = grid[1]
+            cbarh = P.colorbar(ah[3], ax=bar, format='%.1e', drawedges=False, shrink=0.467, fraction=0.17, anchor=(0.0,0.96))
+        else:
+            bar = grid.cbar_axes[0]
+            bar.axis["right"].toggle(all=True)
+            cbarh = P.colorbar(ah[3], cax=bar, format='%.1e', drawedges=False)
+        cbarh.ax.set_ylabel('particle mass histogram' + " [%s]" % pu.labelx()(umass))
+        if cbar_mode == 'none':
+            cbarh.ax.yaxis.set_label_coords(-1.5, 0.5)
+
     if drawd:
-        bar = grid.cbar_axes[0]
-        bar.axis["right"].toggle(all=True)
-        cbar = P.colorbar(a, cax=bar, format='%.1e', drawedges=False)
+        if cbar_mode == 'none':
+            bar = grid[1]
+            cbar = P.colorbar(a, ax=bar, format='%.1e', drawedges=False, shrink=0.467, fraction=0.23, anchor=(0.0,0.96))
+        else:
+            bar = grid.cbar_axes[0]
+            bar.axis["right"].toggle(all=True)
+            cbar = P.colorbar(a, cax=bar, format='%.1e', drawedges=False)
         cbar.ax.set_ylabel(var + " [%s]" % pu.labelx()(uvar))
+        if cbar_mode == 'none':
+            cbar.ax.yaxis.set_label_coords(-1.5, 0.5)
 
     P.draw()
     P.savefig(output, facecolor='white')
