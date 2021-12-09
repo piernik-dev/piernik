0aa7f8a6e3b52680fce274f13ad63c18d87a9fdd
Artur Gawryszczak <gawrysz@gmail.com>
2021-12-09 11:56:33 +0100

    typos

--- src/base/global.F90
+++ src/base/global.F90
@@ -501,11 +501,11 @@ contains
       if (master) then
          select case (divB_0_method)
             case (DIVB_HDC)
-               call printinfo("    The div(B) constraint is maintaineded by Hyperbolic Cleaning (GLM).")
+               call printinfo("    The div(B) constraint is maintained by Hyperbolic Cleaning (GLM).")
             case (DIVB_CT)
-               call printinfo("    The div(B) constraint is maintaineded by Constrained Transport (2nd order).")
+               call printinfo("    The div(B) constraint is maintained by Constrained Transport (2nd order).")
             case default
-               call die("    The div(B) constraint is maintaineded by Uknown Something.")
+               call die("    The div(B) constraint is maintained by Uknown Something.")
          end select
 
          if (cc_mag) then
