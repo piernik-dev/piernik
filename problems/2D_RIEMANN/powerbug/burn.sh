84f7f861b2cb0b68b937ff90cb0ccbaa598d56a6
Artur Gawryszczak <gawrysz@gmail.com>
2022-11-08 23:46:17 +0100

    [powerbug/burn.sh] set up defaults for the burn, scan and ramspam modes

--- problems/2D_RIEMANN/powerbug/burn.sh
+++ problems/2D_RIEMANN/powerbug/burn.sh
@@ -2,8 +2,67 @@
 
 # Sample script that may help to determine parameters for stress-testing a computer with Piernik.
 
+MODE="ramspam"
+if [ $# -ge 1 ] ; then
+    MODE=$1
+    if [ "$MODE" == "help" ] ; then
+	echo "Usage: $0 [mode]"
+	echo "  where the mode is:"
+	echo "  burn    – for maximum CPU heating (may need adjustments for particular CPU)"
+	echo "  ramspam – for huge CPU heating and a lot of RAM usage"
+	echo "  scan    – tries few combinations of adjustable parameters to help to determine optimum block size for the burn mode"
+	echo "  help    – this help"
+	exit 0
+    fi
+fi
+
 # Count available CPU threads.
-NTHR=$( lscpu -p | grep -v '^#' | wc -l )
+NTHR=$( lscpu -p | grep -cv '^#' )
+VENDOR=$( grep vendor_id /proc/cpuinfo | uniq | awk '{print $3}' )
+
+k=1
+SL="Riemann"
+case $MODE in
+    "burn")
+	# Maximum power draw depends on CPU model and problem configuration:
+	# Ryzen 7 2700: bs=300 solver=RTVD
+	# i5-11600: bs=116 solver=RTVD
+	case $VENDOR in
+	    "GenuineIntel")
+		BSL=116
+		;;
+	    "AuthenticAMD")
+		BSL=300
+		;;
+	    *)
+		echo "Unknown vendor '$VENDOR'. Do a scan first, then make an update."
+		exit 1
+		;;
+	esac
+	;;
+    "ramspam")
+	BSL=256
+	# Use all the available memory.
+	# Assume an estimate for RES = 19 + 11.7 * k**2 kB
+	k=$( LC_ALL=C free | awk '/Mem/ {print int(sqrt(($NF/1024./'"$NTHR"'-19)/11.7))}' )
+	;;
+    "scan")
+	# Scan for most energy-consuming setup parameters.
+	# It seems that n_d has the biggest impact. Its optimal size may change with size and organization of the CPU cache.
+
+	BSL="92 116 148 188 236 300"
+	SL="Riemann RTVD"
+	;;
+    *)
+	echo "Unknown mode '$MODE'."
+	exit 2
+	;;
+esac
+
+DURATION=604800
+[ "$MODE" == "scan" ] && DURATION=30
+
+echo "Mode=$MODE Vendor=$VENDOR Nthr=$NTHR bs=$BSL solver=$SL k=$k duration=${DURATION}s"
 
 # If there is mprime  running for warm-up, stop it.
 MPRIME=1
@@ -11,66 +70,73 @@ killall -STOP mprime 2> /dev/null || MPRIME=0
 
 SHOW_TEMP=.__st__
 # A "daemon" for reporting temperatures. Adjust the grep filter to particular machine.
+case $VENDOR in
+    "GenuineIntel")
+	SENS='(Package)'
+	;;
+    "AuthenticAMD")
+	SENS='(Tctl|TSI0_TEMP)'
+	;;
+    *)
+	echo "Unknown vendor '$VENDOR'. Figure out sensors first, then make an update."
+	exit 3
+	# An alternative that can be used as a dirty fallback:
+	# sensors | grep ": .*°C" | sed 's/\(.*\): *\([^C]*\)°C.*/\1 \2/' | awk '{if ($NF > 0.) print}' | tr '\n' ' '
+	;;
+esac
 touch $SHOW_TEMP
 sleep 5 && while [ -e $SHOW_TEMP ] ; do
-    ( cat $SHOW_TEMP; sensors | grep -E '(Tctl|TSI0_TEMP)' ) | tr '\n' ' ' ; echo
-    #( cat $SHOW_TEMP; sensors | grep -E '(Package)' ) | tr '\n' ' ' ; echo
+    ( cat $SHOW_TEMP; sensors | grep -E $SENS ) | tr '\n' ' ' ; echo
     sleep 5
+    killall -CONT piernik 2> /dev/null || rm $SHOW_TEMP
 done &
 
 # Create separate directories for each run.
-for i in `seq $NTHR` ; do
-    [ -d $i ] && rm -r $i
-    mkdir $i
-    cp problem.par $i
-    ln -s ../piernik ${i}/piernik
+for i in $( seq "$NTHR" ) ; do
+    [ -d "$i" ] && rm -r "$i"
+    mkdir "$i"
+    cp problem.par "$i"
+    ln -s ../piernik "${i}/piernik"
 done
 
-# Scan for most energy-consuming setup parameters.
-# It seems that n_d has the biggest impact. Its optimal size may change with size and organization of the CPU cache.
-
-# Maximum power draw depends on CPU model and problem configuration:
-# Ryzen 7 2700: bs=300 solver=RTVD
-# i5-11600: bs=116 solver=RTVD
-
-for bs in 116 300 ; do
-    k=1
-    for s in Riemann RTVD ; do
-	nd=$(( $bs * $k ))
-	echo "flood: n_d= $nd  solver= $s" > $SHOW_TEMP
+for bs in $BSL ; do
+    for s in $SL ; do
+	nd=$(( "$bs * $k" ))
+	echo "${MODE}: n_d= $nd  solver= $s" > $SHOW_TEMP
 	# Run $NTHR independent single-threaded Piernik copies as this draws most electrical power.
 	# Parallel Piernik runs do spend some time on communication, so we choose flooding until we implement shared-memory :)
-	for d in `seq $NTHR` ; do
-	    cd $d
-	    rm *log 2> /dev/null
-	    ./piernik -n '&BASE_DOMAIN n_d = 2*'$nd', 1/ &AMR bsize = 2*'$bs', 1/ &NUMERICAL_SETUP solver_str="'$s'"' > /dev/null &
-	    cd - > /dev/null
+	for d in $( seq "$NTHR" ) ; do
+	    (
+		cd "$d" || exit 4
+		rm ./*log 2> /dev/null
+		./piernik -n '&BASE_DOMAIN n_d = 2*'"$nd"', 1/ &AMR bsize = 2*'"$bs"', 1/ &NUMERICAL_SETUP solver_str="'"$s"'"' | grep --line-buffered . > out &
+	    )
 	done
 
 	# Check if all instances are reaching the same values.
         OSTEP=0
 	while [ -e $SHOW_TEMP ] ; do
-	    [ -e 1/*log ] && \
-		STEP=$( tail -n 1 [1-9]*/*log | \
+	    [ -e 1/out ] && \
+		STEP=$( tail -n 1 [1-9]*/out | \
 			    grep nstep | \
-			    sort -n -k 5 | \
+			    sort -n -k 3 | \
 			    head -n 1 | \
-			    awk '{print $5}' )
-            [ ${STEP:-0} -gt 0 ] && \
-                if [ $STEP != $OSTEP ] ; then
+			    awk '{print $3}' )
+            [ "${STEP:-0}" -gt 0 ] && \
+                if [ "$STEP" != "$OSTEP" ] ; then
                     OSTEP=$STEP
-                    grep "nstep = *$STEP" [1-9]*/*log | \
+                    grep "nstep = *$STEP" [1-9]*/out | \
 			sort -n | \
 			column -t | \
 			awk '{if (NR==1) {dt=$9; t=$12; print} else if (dt != $9 || t != $12) print "Differs: ", $0}'
                 fi
-            sleep 3
+            sleep 10
         done &
 
         # ToDo also check if all $NTHR are alive and not stuck.
 
-	# Run each Piernik configuration for 60 seconds.
-	sleep 60 && killall piernik
+	# Run each Piernik configuration for limited amount of time.
+	sleep $DURATION && killall piernik
 	echo "kill $nd $k"
 	sleep 1 && killall -9 piernik
 	echo
