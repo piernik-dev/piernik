466714d3bada8a1276980c5b9dcedf5f8a202dd4
AntoineBaldacchino <a.baldacchino.j@gmail.com>
2023-03-22 15:17:41 +0100

    [cr_data] (WIP) re-add Be10 isotope, and add new isotope for Boron : B10 with appropriate mass, Z and cross section

--- src/fluids/cosmicrays/cr_data.F90
+++ src/fluids/cosmicrays/cr_data.F90
@@ -56,6 +56,8 @@ module cr_data
       enumerator :: icr_O16
       enumerator :: icr_Li7
       enumerator :: icr_Be9
+      enumerator :: icr_Be10
+      enumerator :: icr_B10
       enumerator :: icr_B11
 
       enumerator :: icr_LAST     !< should be used nowhere despite with nicr
@@ -77,6 +79,8 @@ module cr_data
    logical, dimension(PRES:PRIM)                           :: eH1                !< presence and grad_pcr essentiality of H1 isotope
    logical, dimension(PRES:PRIM)                           :: eLi7               !< presence and grad_pcr essentiality of Li7 isotope
    logical, dimension(PRES:PRIM)                           :: eBe9               !< presence and grad_pcr essentiality of Be9 isotope
+   logical, dimension(PRES:PRIM)                           :: eBe10              !< presence and grad_pcr essentiality of Be10 isotope
+   logical, dimension(PRES:PRIM)                           :: eB10               !< presence and grad_pcr essentiality of B10 isotope
    logical, dimension(PRES:PRIM)                           :: eB11               !< presence and grad_pcr essentiality of B11 isotope
    logical, dimension(PRES:PRIM)                           :: eC12               !< presence and grad_pcr essentiality of C12 isotope
    logical, dimension(PRES:PRIM)                           :: eN14               !< presence and grad_pcr essentiality of N14 isotope
@@ -104,6 +108,8 @@ module cr_data
    real, parameter :: m_H1   = 1.
    real, parameter :: m_Li7  = 7.
    real, parameter :: m_Be9  = 9.
+   real, parameter :: m_Be10 = 10.
+   real, parameter :: m_B10  = 10.
    real, parameter :: m_B11  = 11.
    real, parameter :: m_C12  = 12.
    real, parameter :: m_N14  = 14.
@@ -112,6 +118,8 @@ module cr_data
    real, parameter :: Z_H1   = 1.
    real, parameter :: Z_Li7  = 3.
    real, parameter :: Z_Be9  = 4.
+   real, parameter :: Z_Be10 = 4.
+   real, parameter :: Z_B10  = 5.
    real, parameter :: Z_B11  = 5.
    real, parameter :: Z_C12  = 6.
    real, parameter :: Z_N14  = 7.
@@ -121,7 +129,9 @@ module cr_data
 
    real, parameter :: sigma_C12_Li7  = 10.   !< mbarn
    real, parameter :: sigma_C12_Be9  = 6.   !< mbarn
-   real, parameter :: sigma_C12_B11  = 30.0 !3.5 !< mbarn
+   real, parameter :: sigma_C12_Be10 = 3.5   !< mbarn
+   real, parameter :: sigma_C12_B11  = 31.5 !3.5 !< mbarn
+   real, parameter :: sigma_C12_B10  = 17.3 !3.5 !< mbarn
 
    real, parameter :: sigma_N14_Li7  = 9.5 !< mbarn
 
@@ -133,7 +143,7 @@ module cr_data
 
 !<====Decay half live times from Garcia-Munoz 1987====>
 
-   real, parameter :: tau_B11 = 0.0 !< Myr
+   real, parameter :: tau_Be10 = 1.6 !< Myr
 
 !<Initial source abundances (in numer density) relative to hydrogen (compare e.g. Longair)>
 
@@ -176,14 +186,16 @@ contains
       ! Only protons (p+) are dynamically important, we can neglect grad_pcr from heavier nuclei
       ! because of their lower abundancies: n(alpha) ~ 0.1 n(p+), other elements less abundant by orders of magnitude
 #ifdef CRESP
-       eE    = [.true., .false., .true., .true.]
-       eH1   = [.true., .true., .false., .true.]
-       eLi7  = [.true., .false., .true., .false.]
-       eBe9  = [.true., .false., .true., .false.]
-       eB11  = [.true., .false., .true., .false.]
-       eC12  = [.true., .false., .true., .true.]
-       eN14  = [.true., .false., .true., .true.]
-       eO16  = [.true., .false., .true., .true.]
+       eE     = [.true., .false., .true., .true.]
+       eH1    = [.true., .true., .false., .true.]
+       eLi7   = [.true., .false., .true., .false.]
+       eBe9   = [.true., .false., .true., .false.]
+       eBe10  = [.true., .false., .true., .false.]
+       eB10   = [.true., .false., .true., .false.]
+       eB11   = [.true., .false., .true., .false.]
+       eC12   = [.true., .false., .true., .true.]
+       eN14   = [.true., .false., .true., .true.]
+       eO16   = [.true., .false., .true., .true.]
 #else /* !CRESP */
 
 #endif /* !CRESP */
@@ -207,15 +219,16 @@ contains
          close(nh%lun)
          call nh%compare_namelist() ! Do not use one-line if here!
 
-         lbuff(icr_E    VS icr_E   ) = eE
-         lbuff(icr_H1   VS icr_H1  ) = eH1
-         lbuff(icr_C12  VS icr_C12 ) = eC12
-         lbuff(icr_N14  VS icr_N14 ) = eN14
-         lbuff(icr_O16  VS icr_O16 ) = eO16
-         lbuff(icr_Li7  VS icr_Li7 ) = eLi7
-         lbuff(icr_Be9  VS icr_Be9 ) = eBe9
-         lbuff(icr_B11  VS icr_B11 ) = eB11
-
+         lbuff(icr_E    VS icr_E   )   = eE
+         lbuff(icr_H1   VS icr_H1  )   = eH1
+         lbuff(icr_C12  VS icr_C12 )   = eC12
+         lbuff(icr_N14  VS icr_N14 )   = eN14
+         lbuff(icr_O16  VS icr_O16 )   = eO16
+         lbuff(icr_Li7  VS icr_Li7 )   = eLi7
+         lbuff(icr_Be9  VS icr_Be9 )   = eBe9
+         lbuff(icr_Be10 VS icr_Be10 )  = eBe10
+         lbuff(icr_B10  VS icr_B10 )   = eB10
+         lbuff(icr_B11  VS icr_B11 )   = eB11
 
 
 
@@ -232,6 +245,8 @@ contains
          eO16  = lbuff(icr_O16  VS icr_O16 )
          eLi7  = lbuff(icr_Li7  VS icr_Li7 )
          eBe9  = lbuff(icr_Be9  VS icr_Be9 )
+         eBe10 = lbuff(icr_Be10 VS icr_Be10)
+         eB10  = lbuff(icr_B10  VS icr_B10 )
          eB11  = lbuff(icr_B11  VS icr_B11 )
 
 
@@ -239,9 +254,9 @@ contains
 
 #undef VS
 
-      eCRSP(1:nicr) = [eE(PRES), eH1(PRES), eC12(PRES), eN14(PRES), eO16(PRES), eLi7(PRES), eBe9(PRES), eB11(PRES)]
+      eCRSP(1:nicr) = [eE(PRES), eH1(PRES), eC12(PRES), eN14(PRES), eO16(PRES), eLi7(PRES), eBe9(PRES), eBe10(PRES), eB10(PRES), eB11(PRES)]
       ncrsp_auto = count(eCRSP, kind=4)
-      ePRIM(1:nicr) = [eE(PRIM), eH1(PRIM), eC12(PRIM), eN14(PRIM), eO16(PRIM), eLi7(PRIM), eBe9(PRIM), eB11(PRIM)]
+      ePRIM(1:nicr) = [eE(PRIM), eH1(PRIM), eC12(PRIM), eN14(PRIM), eO16(PRIM), eLi7(PRIM), eBe9(PRIM), eBe10(PRIM), eB10(PRIM), eB11(PRIM)]
       ncrsp_prim = count(ePRIM .and. eCRSP, kind=4)
       ncrsp_sec = ncrsp_auto - ncrsp_prim
 
@@ -310,12 +325,12 @@ contains
       logical,                   dimension(nicr) :: eCRSP_ess, eCRSP_spec, eCRSP_prim
       real,                      dimension(nicr) :: eCRSP_mass, eCRSP_Z
 
-      eCRSP_names(1:nicr) = ['e-  ', 'p+  ', 'C12 ', 'N14 ', 'O16 ','Li7 ','Be9 ', 'B11 ' ]
-      eCRSP_mass (1:nicr) = [me/mp,  m_H1,   m_C12,  m_N14, m_O16,  m_Li7 , m_Be9, m_B11 ]
-      eCRSP_Z    (1:nicr) = [   0.,  Z_H1,   Z_C12,  Z_N14, Z_O16,  Z_Li7 , Z_Be9, Z_B11 ]
-      eCRSP_ess  (1:nicr) = [eE(ESS) , eH1(ESS) , eC12(ESS) , eN14(ESS) , eO16(ESS) , eLi7(ESS) , eBe9(ESS) , eB11(ESS)  ]
-      eCRSP_spec (1:nicr) = [eE(SPEC), eH1(SPEC), eC12(SPEC), eN14(SPEC), eO16(SPEC), eLi7(SPEC), eBe9(SPEC), eB11(SPEC) ]
-      eCRSP_prim (1:nicr) = [eE(PRIM), eH1(PRIM), eC12(PRIM), eN14(PRIM), eO16(PRIM), eLi7(PRIM), eBe9(PRIM), eB11(PRIM) ]
+      eCRSP_names(1:nicr) = ['e-  ', 'p+  ', 'C12 ', 'N14 ', 'O16 ','Li7 ','Be9 ', 'Be10', 'B10 ', 'B11 ']
+      eCRSP_mass (1:nicr) = [me/mp,  m_H1,   m_C12,  m_N14, m_O16,  m_Li7 , m_Be9, m_Be10 m_B10, m_B11 ]
+      eCRSP_Z    (1:nicr) = [   0.,  Z_H1,   Z_C12,  Z_N14, Z_O16,  Z_Li7 , Z_Be9, Z_Be10, Z_B10, Z_B11 ]
+      eCRSP_ess  (1:nicr) = [eE(ESS) , eH1(ESS) , eC12(ESS) , eN14(ESS) , eO16(ESS) , eLi7(ESS) , eBe9(ESS) , eBe10(ESS)  , eB10(ESS) , eB11(ESS)  ]
+      eCRSP_spec (1:nicr) = [eE(SPEC), eH1(SPEC), eC12(SPEC), eN14(SPEC), eO16(SPEC), eLi7(SPEC), eBe9(SPEC), eBe10(SPEC) , eB10(SPEC), eB11(SPEC) ]
+      eCRSP_prim (1:nicr) = [eE(PRIM), eH1(PRIM), eC12(PRIM), eN14(PRIM), eO16(PRIM), eLi7(PRIM), eBe9(PRIM), eBe10(PRIM) , eB10(PRIM), eB11(PRIM) ]
 
       allocate(cr_names(ncrsp), cr_table(nicr), cr_index(nicr), cr_sigma(ncrsp,ncrsp), cr_tau(ncrsp), cr_primary(ncrsp_prim), cr_mass(ncrsp), cr_Z(ncrsp), cr_spectral(ncrsp), cr_gpess(ncrsp),cr_sigma_N(ncrsp), icr_spc(count(eCRSP_spec .and. eCRSP)), iarr_spc(ncrsp), rel_abound(ncrsp))
       cr_names(:)    = ''
@@ -448,7 +463,10 @@ contains
          cr_primary(cr_table(icr_C12)) = primary_C12
          if (eCRSP(icr_Li7 )) cr_sigma(cr_table(icr_C12), cr_table(icr_Li7 )) = sigma_C12_Li7
          if (eCRSP(icr_Be9 )) cr_sigma(cr_table(icr_C12), cr_table(icr_Be9 )) = sigma_C12_Be9
-         if (eCRSP(icr_B11)) cr_sigma(cr_table(icr_C12), cr_table(icr_B11)) = sigma_C12_B11
+         if (eCRSP(icr_Be10)) cr_sigma(cr_table(icr_C12), cr_table(icr_Be10)) = sigma_C12_Be10
+         if (eCRSP(icr_B10 )) cr_sigma(cr_table(icr_C12), cr_table(icr_B10 )) = sigma_C12_B10
+         if (eCRSP(icr_B11 )) cr_sigma(cr_table(icr_C12), cr_table(icr_B11 )) = sigma_C12_B11
+
       endif
       if (eCRSP(icr_N14)) then
          cr_primary(cr_table(icr_N14)) = primary_N14
@@ -461,7 +479,7 @@ contains
          if (eCRSP(icr_B11)) cr_sigma(cr_table(icr_O16), cr_table(icr_B11)) = sigma_O16_B11
       endif
       cr_sigma = cr_sigma * mbarn
-      if (eCRSP(icr_B11)) cr_tau(cr_table(icr_B11)) = tau_B11 * myr
+      if (eCRSP(icr_Be10)) cr_tau(cr_table(icr_Be10)) = tau_Be10 * myr
       !print *, 'sigmas : ', cr_sigma
       !print *, shape(cr_sigma)
       !stop
