248bdcd471f5e0d149d7d074163665b7a33b8a22
AntoineBaldacchino <a.baldacchino.j@gmail.com>
2023-07-03 17:52:41 +0200

    [cr_data] (WIP) by default, transrelativistic = false while transrelativistic energies are not correctly computed

--- src/fluids/cosmicrays/cr_data.F90
+++ src/fluids/cosmicrays/cr_data.F90
@@ -89,7 +89,7 @@ module cr_data
    logical, dimension(PRES:PRIM)                           :: eO16               !< presence and grad_pcr essentiality of O16 isotope
    logical,                                dimension(nicr) :: eCRSP              !< table of all isotopes presences
    logical,                                dimension(nicr) :: ePRIM              !< table of all PRIMARY presences
-   logical, parameter                                      :: transrelativistic = .true. !< Logical for transrelativistic limit of momentum in CRESP
+   logical, parameter                                      :: transrelativistic = .false. !< Logical for transrelativistic limit of momentum in CRESP
    integer, parameter                                      :: specieslen = 6     !< length of species names
    character(len=specieslen), allocatable, dimension(:)    :: cr_names           !< table of species names
    character(len=idlen), parameter                         :: p_bnd =  'fix'     !, 'bnd! impose momentum boundaries to be fixed along time, or able to move  ! BEWARE: src/fluids/cosmicrays/initcrspectrum.F90 also exports some variable named "p_bnd"
