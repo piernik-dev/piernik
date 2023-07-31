50b3c99adbc06f1deab4017623c842e8d87d632c
Artur Gawryszczak <gawrysz@gmail.com>
2023-07-31 23:40:54 +0200

    added a way to automagically mark cg_cost contributions to PPP

--- src/particles/particle_utils.F90
+++ src/particles/particle_utils.F90
@@ -153,7 +153,7 @@ contains
 #endif /* NBODY_CHECK_PID */
          if (phy .or. out) call cgl%cg%pset%add(pid, mass, pos, vel, acc, ener, in, phy, out, tform1, tdyn1)
          cgfound = cgfound .or. (phy .or. out)
-         call cgl%cg%costs%stop(I_PARTICLE)
+         call cgl%cg%costs%stop(I_PARTICLE, ppp = .false.)
          cgl => cgl%nxt
       enddo
       if (present(success)) success = cgfound
