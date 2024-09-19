af07540a467de97e86d14653a5fb514932163c25
Artur Gawryszczak <gawrysz@gmail.com>
2024-09-19 20:36:54 +0200

    [dot] converted to the new req% type

--- src/grid/dot.F90
+++ src/grid/dot.F90
@@ -39,7 +39,6 @@
 module dot
 
    use decomposition, only: cuboid
-   use req_array,     only: req_arr
 
    implicit none
 
@@ -80,8 +79,6 @@ module dot
    integer(kind=8), parameter :: huge_SFC = huge(1_8)
    integer, dimension(:), allocatable :: pp  ! an array used in find_grid
 
-   type(req_arr) :: req
-
 contains
 
 !> \brief Deallocate everything
@@ -95,8 +92,6 @@ contains
       if (allocated(this%gse)) deallocate(this%gse) ! this%gse(:)%c should be deallocated automagically
       if (allocated(this%SFC_id_range)) deallocate(this%SFC_id_range)
       if (allocated(pp)) deallocate(pp)
-
-      call req%cleanup
 
    end subroutine cleanup
 
@@ -277,10 +272,10 @@ contains
 
       use allreduce, only: piernik_MPI_Allreduce
       use constants, only: ndims, LO, HI, pLAND, I_ONE
-      use MPIF,      only: MPI_INTEGER, MPI_REQUEST_NULL, MPI_COMM_WORLD
+      use MPIF,      only: MPI_INTEGER, MPI_COMM_WORLD
       use MPIFUN,    only: MPI_Irecv, MPI_Isend, MPI_Comm_dup, MPI_Comm_free
-      use mpisetup,  only: proc, req, err_mpi, LAST, inflate_req, slave
-      use ppp_mpi,   only: piernik_Waitall
+      use mpisetup,  only: proc, err_mpi, LAST, slave
+      use ppp_mpi,   only: req_ppp
 #ifdef MPIF08
       use MPIF,      only: MPI_Comm
 #endif /* MPIF08 */
@@ -293,13 +288,14 @@ contains
       integer(kind=4), parameter :: sh_tag = 7
       integer(kind=4), parameter :: nr = 2
       integer :: i
+      type(req_ppp) :: req
 #ifdef MPIF08
       type(MPI_Comm)  :: dot_comm
 #else /* !MPIF08 */
       integer(kind=4) :: dot_comm
 #endif /* !MPIF08 */
 
-      call inflate_req(nr)
+      call req%init(nr)
       this%is_blocky = .true.
       shape = 0
       shape1 = 0
@@ -316,11 +312,10 @@ contains
       endif
 
       call MPI_Comm_dup(MPI_COMM_WORLD, dot_comm, err_mpi)
-      req = MPI_REQUEST_NULL
-      if (slave)     call MPI_Irecv(shape1, size(shape1, kind=4), MPI_INTEGER, proc-I_ONE, sh_tag, dot_comm, req(1 ), err_mpi)
-      if (proc<LAST) call MPI_Isend(shape,  size(shape, kind=4),  MPI_INTEGER, proc+I_ONE, sh_tag, dot_comm, req(nr), err_mpi)
+      if (slave)     call MPI_Irecv(shape1, size(shape1, kind=4), MPI_INTEGER, proc-I_ONE, sh_tag, dot_comm, req%nxt(), err_mpi)
+      if (proc<LAST) call MPI_Isend(shape,  size(shape, kind=4),  MPI_INTEGER, proc+I_ONE, sh_tag, dot_comm, req%nxt(), err_mpi)
 
-      call piernik_Waitall(nr, "dot:chk_blocky")
+      call req%waitall("dot:chk_blocky")
       call MPI_Comm_free(dot_comm, err_mpi)
 
       if (any(shape /= 0) .and. any(shape1 /= 0)) then
