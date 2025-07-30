54e60b6ff6d74d07f0aa7214dc06ee18a0f6979f
Vinod Pisharody <vinodpisharody7@gmail.com>
2025-07-30 10:42:29 +0200

    Fixed source term here

--- src/scheme/unsplit_schemes/unsplit_source.F90
+++ src/scheme/unsplit_schemes/unsplit_source.F90
@@ -26,14 +26,14 @@
 !
 #include "piernik.h"
 
-module unsplit_source 
+module unsplit_source
 
 ! pulled by ANY
 
    implicit none
 
    private
-   public  :: apply_source 
+   public  :: apply_source
 
 contains
 
@@ -41,74 +41,63 @@ contains
 
     subroutine apply_source(cg,istep)
         use grid_cont,          only: grid_container
-        use named_array_list,   only: wna, qna
-        use constants,          only: pdims, ORTHO1, ORTHO2, LO, HI, magh_n, uh_n, rk_coef, cs_i2_n, first_stage, last_stage, xdim, ydim, zdim
-        use global,             only: dt, integration_order, nstep
+        use named_array_list,   only: wna
+        use constants,          only: pdims, ORTHO1, ORTHO2, LO, HI, uh_n, rk_coef, first_stage, xdim, zdim
+        use global,             only: dt, integration_order
         use domain,             only: dom
-        use fluidindex,         only: flind, iarr_all_dn, iarr_all_mx, iarr_all_swp, iarr_mag_swp
+        use fluidindex,         only: flind, iarr_all_dn, iarr_all_mx, iarr_all_swp
         use sources,            only: internal_sources, care_for_positives
+        use diagnostics,        only: my_allocate, my_deallocate
+#ifdef MAGNETIC
+        use constants,          only: magh_n
+        use fluidindex,         only: iarr_mag_swp
+#endif /* MAGNETIC */
 
         implicit none
 
         type(grid_container), pointer,     intent(in) :: cg
         integer,                           intent(in) :: istep
 
-        integer                                                     :: ddim, i1, i2, uhi, bhi
+        integer(kind=4)                                             :: uhi, ddim
+        integer                                                     :: i1, i2
         real, dimension(:,:),allocatable                            :: u
         real, dimension(:,:), pointer                               :: pu,pb
         real, allocatable, target                                   :: vx(:,:)
-        real, dimension(1, 1)                                       :: b_ugly ! ugly
-        real, dimension(:,:),allocatable                            :: b
         real, dimension(:,:),allocatable                            :: u1
-
-        uhi = wna%ind(uh_n)
+#ifdef MAGNETIC
+        real, dimension(:,:),allocatable                            :: b
+        integer                                                     :: bhi
         bhi = wna%ind(magh_n)
-
+#else /* !MAGNETIC */
+         real, dimension(1, 1)                                       :: b_ugly ! ugly
+         b_ugly = 0.0
+#endif /* !MAGNETIC */
+        uhi = wna%ind(uh_n)
         do ddim=xdim,zdim
-
-            if (.not. allocated(u)) then
-                allocate(u(cg%n_(ddim), size(cg%u,1)))
-            else
-                deallocate(u)
-                allocate(u(cg%n_(ddim), size(cg%u,1)))
-
-            endif
-            if (.not. allocated(b)) then
-                allocate(b(cg%n_(ddim), size(cg%b,1)))
-            else
-                deallocate(b)
-                allocate(b(cg%n_(ddim), size(cg%b,1)))
-
-            endif
-            if (.not. allocated(vx)) then
-                allocate(vx(size(u,1), flind%fluids))
-            else
-                deallocate(vx)
-                allocate(vx(size(u,1), flind%fluids))
-            endif
-            if (.not. allocated(u1)) then
-                allocate(u1(size(u, 1),size(u, 2)))
-            else
-                deallocate(u1)
-                allocate(u1(size(u, 1),size(u, 2)))
-            endif
             if (.not. dom%has_dir(ddim)) cycle
+            call my_allocate(u,[cg%n_(ddim), size(cg%u,1,kind=4)])
+            call my_allocate(vx,[size(u,1,kind=4), flind%fluids])
+            call my_allocate(u1,[size(u, 1,kind=4),size(u, 2,kind=4)])
+#ifdef MAGNETIC
+            call my_allocate(b,[cg%n_(ddim), size(cg%b,1,kind=4)])
+#endif /* MAGNETIC */
             do i2 = cg%ijkse(pdims(ddim, ORTHO2), LO), cg%ijkse(pdims(ddim, ORTHO2), HI)
-                do i1 = cg%ijkse(pdims(ddim, ORTHO1), LO), cg%ijkse(pdims(ddim, ORTHO1), HI)  
-
-                    pu => cg%w(uhi)%get_sweep(ddim,i1,i2)
-                    pb => cg%w(bhi)%get_sweep(ddim,i1,i2)
+                do i1 = cg%ijkse(pdims(ddim, ORTHO1), LO), cg%ijkse(pdims(ddim, ORTHO1), HI)
+                    pu => cg%w(wna%fi)%get_sweep(ddim,i1,i2)
+#ifdef MAGNETIC
+                    pb => cg%w(wna%bi)%get_sweep(ddim,i1,i2)
+#endif /* MAGNETIC */
                     if (istep == first_stage(integration_order) .or. integration_order < 2 ) then
-                            pu => cg%w(wna%fi)%get_sweep(ddim,i1,i2)
-                            pb => cg%w(wna%bi)%get_sweep(ddim,i1,i2)
+                            pu => cg%w(uhi)%get_sweep(ddim,i1,i2)
+#ifdef MAGNETIC
+                            pb => cg%w(bhi)%get_sweep(ddim,i1,i2)
+#endif /* MAGNETIC */
                     endif
-                    
-
                     u(:, iarr_all_swp(ddim,:)) = transpose(pu(:,:))
+#ifdef MAGNETIC
                     b(:, iarr_mag_swp(ddim,:)) = transpose(pb(:,:))
-
+#endif /* MAGNETIC */
                     u1 = u
-
                     vx = u(:, iarr_all_mx) / u(:, iarr_all_dn) ! this may also be useful for gravitational acceleration
 #ifdef MAGNETIC
                     call internal_sources(size(u, 1, kind=4), u, u1, b, cg, istep, ddim, i1, i2, rk_coef(istep) * dt, vx)
@@ -120,11 +109,12 @@ contains
                     call care_for_positives(size(u, 1, kind=4), u1, b_ugly, cg, ddim, i1, i2)
 #endif /* MAGNETIC */
                     pu(:,:) = transpose(u1(:, iarr_all_swp(ddim,:)))
-                end do        
-            end do
-
-            deallocate(vx,u1,u,b)
-            
-        end do
+                enddo
+            enddo
+            call my_deallocate(vx); call my_deallocate(u1); call my_deallocate(u)
+#ifdef MAGNETIC
+            call my_deallocate(b)
+#endif /* MAGNETIC */
+        enddo
     end subroutine apply_source
-end module unsplit_source 
+end module unsplit_source
